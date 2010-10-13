3cb6e56738be35a3080fac0fc529a86a5b74408f
AndreyMaz <none@none>
2010-10-13 11:43:46 +0000

    DO NOT DOWNLOAD. DOWNLOAD LATEST STABLE FROM "DOWNLOADS" TAB Further changeset 57899 changes
    --HG--
    extra : convert_revision : svn%3A712752d5-f020-ad4b-8268-b7cbb332fca5/trunk%401153

--- NopCommerceStore/Documents/Upgrade.sql
+++ NopCommerceStore/Documents/Upgrade.sql
@@ -577,3 +577,63 @@ BEGIN
 	ALTER TABLE [dbo].[Nop_PaymentMethod] DROP COLUMN HidePaymentInfoForZeroOrders
 END
 GO
+
+IF EXISTS (
+		SELECT *
+		FROM dbo.sysobjects
+		WHERE id = OBJECT_ID(N'[dbo].[Nop_PaymentMethodLoadAll]') AND OBJECTPROPERTY(id,N'IsProcedure') = 1)
+DROP PROCEDURE [dbo].[Nop_PaymentMethodLoadAll]
+GO
+
+CREATE PROCEDURE [dbo].[Nop_PaymentMethodLoadAll]
+(
+	@ShowHidden bit = 0,
+	@FilterByCountryID int = NULL
+)
+AS
+BEGIN
+	SET NOCOUNT ON
+	IF(@FilterByCountryID IS NOT NULL AND @FilterByCountryID != 0)
+		BEGIN
+			SELECT  
+				pm.PaymentMethodId,
+				pm.Name,
+				pm.VisibleName,
+				pm.Description,
+				pm.ConfigureTemplatePath,
+				pm.UserTemplatePath,
+				pm.ClassName,
+				pm.SystemKeyword,
+				pm.IsActive,
+				pm.DisplayOrder
+		    FROM 
+				[Nop_PaymentMethod] pm
+		    WHERE 
+                pm.PaymentMethodID NOT IN 
+				(
+				    SELECT 
+						pmc.PaymentMethodID
+				    FROM 
+						[Nop_PaymentMethod_RestrictedCountries] pmc
+				    WHERE 
+						pmc.CountryID = @FilterByCountryID AND 
+						pm.PaymentMethodID = pmc.PaymentMethodID
+				)
+				AND
+				(IsActive = 1 or @ShowHidden = 1)
+		   ORDER BY 
+				pm.DisplayOrder
+		END
+	ELSE
+		BEGIN
+			SELECT 
+				*
+			FROM 
+				[Nop_PaymentMethod]
+			WHERE 
+				(IsActive = 1 or @ShowHidden = 1)
+			ORDER BY 
+				DisplayOrder
+		END
+END
+GO
