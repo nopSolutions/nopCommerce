fe1ffb56b618d6ea14b1a7aa246919b6a00aeea4
AndreyMaz <none@none>
2010-11-16 15:50:05 +0000

    DO NOT DOWNLOAD. DOWNLOAD LATEST STABLE FROM "DOWNLOADS" TAB Further performance optimization
    --HG--
    extra : convert_revision : svn%3A712752d5-f020-ad4b-8268-b7cbb332fca5/trunk%401255

--- NopCommerceStore/Documents/Upgrade.sql
+++ NopCommerceStore/Documents/Upgrade.sql
@@ -1395,3 +1395,34 @@ IF EXISTS (
 DROP PROCEDURE [dbo].[Nop_ProductTagLoadAll]
 GO
 
+IF EXISTS (
+		SELECT *
+		FROM dbo.sysobjects
+		WHERE id = OBJECT_ID(N'[dbo].[Nop_OrderProductVariantReport]') AND OBJECTPROPERTY(id,N'IsProcedure') = 1)
+DROP PROCEDURE [dbo].[Nop_OrderProductVariantReport]
+GO
+CREATE PROCEDURE [dbo].[Nop_OrderProductVariantReport]
+(
+	@StartTime datetime = NULL,
+	@EndTime datetime = NULL,
+	@OrderStatusID int,
+	@PaymentStatusID int,
+	@BillingCountryID int
+)
+AS
+BEGIN
+	SET NOCOUNT ON
+
+	SELECT DISTINCT opv.ProductVariantId, isnull(sum(opv.PriceExclTax), 0) as PriceExclTax, isnull(sum(opv.Quantity), 0) as Quantity
+	FROM Nop_OrderProductVariant opv 
+	INNER JOIN [Nop_Order] o ON o.OrderId = opv.OrderID
+	WHERE
+		(@StartTime is NULL or @StartTime <= o.CreatedOn) and
+		(@EndTime is NULL or @EndTime >= o.CreatedOn) and 
+		(@OrderStatusID IS NULL or @OrderStatusID=0 or o.OrderStatusID = @OrderStatusID) and
+		(@PaymentStatusID IS NULL or @PaymentStatusID=0 or o.PaymentStatusID = @PaymentStatusID) and
+		(@BillingCountryID IS NULL or @BillingCountryID=0 or o.BillingCountryID = @BillingCountryID) and
+		(o.Deleted=0)
+	GROUP BY opv.ProductVariantId
+	ORDER BY PriceExclTax desc
+END
