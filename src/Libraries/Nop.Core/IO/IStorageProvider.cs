b28ca79b58e72c9156c1984d8690deef45aa842d
andreymaz <none@none>
2010-12-20 15:17:57 +0300

    Added FileSystemStorageProvider

---
+++ src/Libraries/Nop.Core/IO/IStorageProvider.cs
@@ -0,0 +1,32 @@
+﻿//------------------------------------------------------------------------------
+// The contents of this file are subject to the nopCommerce Public License Version 1.0 ("License"); you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at  http://www.nopCommerce.com/License.aspx. 
+// 
+// Software distributed under the License is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. 
+// See the License for the specific language governing rights and limitations under the License.
+// 
+// The Original Code is nopCommerce.
+// The Initial Developer of the Original Code is NopSolutions.
+// All Rights Reserved.
+// 
+// Contributor(s): OrchardProject_______. 
+//------------------------------------------------------------------------------
+
+using System.Collections.Generic;
+
+namespace Nop.Core.IO
+{
+    public interface IStorageProvider
+    {
+        string GetPublicUrl(string path);
+        IStorageFile GetFile(string path);
+        IEnumerable<IStorageFile> ListFiles(string path);
+        IEnumerable<IStorageFolder> ListFolders(string path);
+        void CreateFolder(string path);
+        void DeleteFolder(string path);
+        void RenameFolder(string path, string newPath);
+        void DeleteFile(string path);
+        void RenameFile(string path, string newPath);
+        IStorageFile CreateFile(string path);
+    }
+}
