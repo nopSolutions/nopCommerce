5808bee22c7d909e8515a0945f52a06796647391
andreymaz <none@none>
2011-01-04 15:55:59 +0300

    Further customer session changes

---
+++ src/Tests/Nop.Data.Tests/CustomerSessionTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Nop.Tests;
+using Nop.Core.Domain;
+
+namespace Nop.Data.Tests
+{
+    [TestFixture]
+    public class CustomerSessionTests : PersistenceTest
+    {
+        [Test]
+        public void Can_save_and_load_customerSession()
+        {
+            var customerSession = new CustomerSession
+            {
+                CustomerSessionGuid = Guid.NewGuid(),
+                IsExpired = true,
+                LastAccessedUtc= new DateTime(2010, 01, 01),
+            };
+
+            var fromDb = SaveAndLoadEntity(customerSession);
+            fromDb.IsExpired.ShouldEqual(true);
+            fromDb.LastAccessedUtc.ShouldEqual(new DateTime(2010, 01, 01));
+        }
+
+        [Test]
+        public void Can_save_and_load_customerSession_with_customer()
+        {
+            var customerSession = new CustomerSession
+            {
+                CustomerSessionGuid = Guid.NewGuid(),
+                IsExpired = true,
+                LastAccessedUtc = new DateTime(2010, 01, 01),
+                Customer = new Customer()
+                {
+                    CustomerGuid = Guid.NewGuid(),
+                    Email = "admin@yourStore.com",
+                    Username = "admin@yourStore.com",
+                    PasswordHash = "hash1",
+                    SaltKey = "SaltKey1",
+                    AdminComment = "some comment here",
+                    Active = true,
+                    Deleted = false,
+                    RegistrationDateUtc = new DateTime(2010, 01, 01)
+                }
+            };
+
+            var fromDb = SaveAndLoadEntity(customerSession);
+            fromDb.IsExpired.ShouldEqual(true);
+
+            fromDb.Customer.ShouldNotBeNull();
+            fromDb.Customer.Email.ShouldEqual("admin@yourStore.com");
+        }
+    }
+}
