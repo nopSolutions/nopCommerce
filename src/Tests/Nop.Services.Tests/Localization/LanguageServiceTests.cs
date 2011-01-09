71ac2292ab7f50b3fb5a0be75facf236ed26cead
andreymaz <none@none>
2011-01-09 16:50:55 +0300

    Minor service tests changes

--- src/Tests/Nop.Services.Tests/Localization/LanguageServiceTests.cs
+++ src/Tests/Nop.Services.Tests/Localization/LanguageServiceTests.cs
@@ -5,30 +5,26 @@ using System.Text;
 using Nop.Core.Caching;
 using Nop.Core.Domain;
 using Nop.Data;
+using Nop.Services.Security;
 using Nop.Tests;
 using NUnit.Framework;
 using Nop.Core;
 using Nop.Services.Localization;
 using Nop.Core.Domain.Localization;
+using Rhino.Mocks;
 
 namespace Nop.Services.Tests.Localization
 {
     [TestFixture]
-    public class LanguageServiceTests : ServiceTest
+    public class LanguageServiceTests
     {
+        IRepository<Language> _languageRepo;
         ILanguageService _languageService;
 
         [SetUp]
         public void SetUp()
         {
-            var repo1 = new EfRepository<Language>(context);
-            var cacheManager = new NopNullCache();
-            IWorkContext workContext = null;
-            this._languageService = new LanguageService(workContext, cacheManager, repo1);
-        }
-
-        protected void RegisterTestData()
-        {
+            _languageRepo = MockRepository.GenerateMock<IRepository<Language>>();
             var lang1 = new Language
             {
                 Name = "English",
@@ -37,56 +33,28 @@ namespace Nop.Services.Tests.Localization
                 Published = true,
                 DisplayOrder = 1
             };
+            var lang2 = new Language
+            {
+                Name = "Russian",
+                LanguageCulture = "ru-Ru",
+                FlagImageFileName = "ru.png",
+                Published = true,
+                DisplayOrder = 2
+            };
 
-            this._languageService.InsertLanguage(lang1);
+            _languageRepo.Expect(x => x.Table).Return(new List<Language>() { lang1, lang2 }.AsQueryable());
+
+            var cacheManager = new NopNullCache();
+            IWorkContext workContext = null;
+            _languageService = new LanguageService(workContext, cacheManager, _languageRepo);
         }
 
         [Test]
         public void Can_get_all_languages()
         {
-            RegisterTestData();
-
             var languages = _languageService.GetAllLanguages(true);
             languages.ShouldNotBeNull();
             (languages.Count > 0).ShouldBeTrue();
-        }
-
-        [Test]
-        public void Can_insert_and_load_language()
-        {
-            var lang = new Language
-            {
-                Name = "English",
-                LanguageCulture = "en-Us",
-                FlagImageFileName = "us.png",
-                Published = true,
-                DisplayOrder = 1
-            };
-            _languageService.InsertLanguage(lang);
-            
-            var from = _languageService.GetLanguageById(lang.Id);
-            from.Name.ShouldEqual("English");
-            from.LanguageCulture.ShouldEqual("en-Us");
-            from.FlagImageFileName.ShouldEqual("us.png");
-            from.Published.ShouldEqual(true);
-            from.DisplayOrder.ShouldEqual(1);
-        }
-
-        [Test]
-        public void Can_load_and_delete_language()
-        {
-            RegisterTestData();
-
-            //load the first language
-            var language = _languageService.GetLanguageById(1);
-            language.ShouldNotBeNull();
-
-            //delete language
-            _languageService.DeleteLanguage(language);
-
-            //load th languages
-            var from = _languageService.GetLanguageById(language.Id);
-            from.ShouldBeNull();
         }
     }
 }
