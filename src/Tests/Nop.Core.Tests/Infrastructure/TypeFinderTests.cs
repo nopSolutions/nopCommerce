9f4fad4c7a3adcd8d26286abe0487e2c51bf119d
andreymaz <none@none>
2011-02-01 14:44:29 +0300

    Added TypeFinder tests

---
+++ src/Tests/Nop.Core.Tests/Infrastructure/TypeFinderTests.cs
@@ -0,0 +1,47 @@
+﻿//------------------------------------------------------------------------------
+// The contents of this file are subject to the nopCommerce Public License Version 1.0 ("License"); you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at  http://www.nopCommerce.com/License.aspx. 
+// 
+// Software distributed under the License is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. 
+// See the License for the specific language governing rights and limitations under the License.
+// 
+// The Original Code is nopCommerce.
+// The Initial Developer of the Original Code is NopSolutions.
+// All Rights Reserved.
+// 
+// Contributor(s): _______. 
+//------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Nop.Core.Infrastructure;
+using NUnit.Framework;
+using Nop.Tests;
+using Nop.Core.Domain.Security;
+
+namespace Nop.Core.Tests.Infrastructure
+{
+    [TestFixture]
+    public class TypeFinderTests
+    {
+        [Test]
+        public void TypeFinder_Benchmark_Findings()
+        {
+            var finder = new TypeFinder();
+
+            var type = finder.FindClassesOfType<ISomeInterface>();
+            type.Count().ShouldEqual(1);
+            typeof(ISomeInterface).IsAssignableFrom(type.FirstOrDefault()).ShouldBeTrue();
+        }
+
+        public interface ISomeInterface
+        {
+        }
+
+        public class SomeClass : ISomeInterface
+        {
+        }
+    }
+}
