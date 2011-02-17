f74f444aca9bffe3ae6b3cc9cc2fd4fbdc072e2b
andreymaz <none@none>
2011-02-17 18:12:56 +0300

    Removed license notices

--- src/Libraries/Nop.Services/Orders/OrderTotalCalculationService.cs
+++ src/Libraries/Nop.Services/Orders/OrderTotalCalculationService.cs
@@ -1,35 +1,20 @@
-//------------------------------------------------------------------------------
-// The contents of this file are subject to the nopCommerce Public License Version 1.0 ("License"); you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at  http://www.nopCommerce.com/License.aspx. 
-// 
-// Software distributed under the License is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. 
-// See the License for the specific language governing rights and limitations under the License.
-// 
-// The Original Code is nopCommerce.
-// The Initial Developer of the Original Code is NopSolutions.
-// All Rights Reserved.
-// 
-// Contributor(s): _______. 
-//------------------------------------------------------------------------------
+
 
 using System;
 using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
-using System.Text;
+using Nop.Core;
+using Nop.Core.Domain.Common;
 using Nop.Core.Domain.Customers;
 using Nop.Core.Domain.Discounts;
 using Nop.Core.Domain.Orders;
-using Nop.Core;
+using Nop.Core.Domain.Shipping;
 using Nop.Core.Domain.Tax;
 using Nop.Services.Catalog;
 using Nop.Services.Customers;
-using Nop.Services.Tax;
 using Nop.Services.Discounts;
-using Nop.Services.Shipping;
 using Nop.Services.Payments;
-using Nop.Core.Domain.Shipping;
-using Nop.Core.Domain.Common;
+using Nop.Services.Shipping;
+using Nop.Services.Tax;
 
 namespace Nop.Services.Orders
 {
--- src/Libraries/Nop.Services/Tax/TaxService.cs
+++ src/Libraries/Nop.Services/Tax/TaxService.cs
@@ -1,18 +1,5 @@
-    //------------------------------------------------------------------------------
-// The contents of this file are subject to the nopCommerce Public License Version 1.0 ("License"); you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at  http://www.nopCommerce.com/License.aspx. 
-// 
-// Software distributed under the License is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. 
-// See the License for the specific language governing rights and limitations under the License.
-// 
-// The Original Code is nopCommerce.
-// The Initial Developer of the Original Code is NopSolutions.
-// All Rights Reserved.
-// 
-// Contributor(s): Stephen Kennedy - VAT support, _______. 
-//------------------------------------------------------------------------------
-
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using Nop.Core;
@@ -22,14 +9,8 @@ using Nop.Core.Domain.Customers;
 using Nop.Core.Domain.Directory;
 using Nop.Core.Domain.Orders;
 using Nop.Core.Domain.Tax;
-using Nop.Services.Common;
-using System.Collections.Generic;
-using System.Reflection;
-using System.Web;
-using System.IO;
-using System.Collections.ObjectModel;
-using System.Web.Compilation;
 using Nop.Core.Infrastructure;
+using Nop.Services.Common;
 
 namespace Nop.Services.Tax
 {
