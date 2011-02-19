4dd4e92051d17412116bdc54b4c01034b64d8680
theonlylawislove <none@none>
2011-02-19 13:34:29 -0500

    Added an InitializerModule that initializes the engine and also disposes of the ILifetimeScope that autofac's mvc implementation uses.

---
+++ src/Libraries/Nop.Core/Web/InitializerModule.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Web;
+using Autofac;
+using Nop.Core.Infrastructure;
+
+namespace Nop.Core.Web
+{
+    public class InitializerModule : IHttpModule
+    {
+        public void Init(HttpApplication context)
+        {
+            EventBroker.Instance.Attach(context);
+
+            EventBroker.Instance.EndRequest += ContextEndRequest;
+
+            Context.Initialize(false);
+        }
+
+        private static void ContextEndRequest(object sender, EventArgs e)
+        {
+            var scope = (ILifetimeScope)HttpContext.Current.Items[typeof(ILifetimeScope)];
+            if (scope != null)
+                scope.Dispose();
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
