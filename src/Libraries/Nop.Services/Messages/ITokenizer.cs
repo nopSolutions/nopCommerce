431b4b1a4cbb064fa82c0f33fe320b03c8dcefe6
Dmitry Bulavin <dmitry.bulavin@gmail.com>
2011-02-28 17:10:31 +0300

    Placeholder files for remaining messaging services

---
+++ src/Libraries/Nop.Services/Messages/ITokenizer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Nop.Services.Messages
+{
+    public partial interface ITokenizer
+    {
+    }
+}
