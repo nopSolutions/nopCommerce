5cfc297713815fb8498c90228dc6bd08937dd291
theonlylawislove <none@none>
2011-03-10 13:27:33 -0500

    Forgot a file.

---
+++ src/Presentation/Nop.Web.Framework/BaseNopModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+
+namespace Nop.Web.Framework
+{
+    public class BaseNopModel
+    {
+        public virtual void BindModel(ControllerContext controllerContext, ModelBindingContext bindingContext)
+        {
+        }
+    }
+    public abstract class BaseNopEntityModel<TEntity> : BaseNopModel
+    {
+        public int Id {get;set;}
+    }
+}
