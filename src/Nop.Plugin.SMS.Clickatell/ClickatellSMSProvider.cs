dcb8c7a557a78cd74df716cb82cce316629766fb
Dmitry Bulavin <dmitry.bulavin@gmail.com>
2011-03-14 15:43:41 +0300

    Added ISMSProvider plugins

---
+++ src/Nop.Plugin.SMS.Clickatell/ClickatellSMSProvider.cs
@@ -0,0 +1,142 @@
+﻿using System;
+
+using Nop.Core;
+using Nop.Core.Domain.Logging;
+using Nop.Plugin.SMS.Clickatell.Clickatell;
+using Nop.Services.Configuration;
+using Nop.Services.Logging;
+using Nop.Services.Messages;
+using System.ServiceModel;
+
+namespace Nop.Plugin.SMS.Clickatell
+{
+    /// <summary>
+    /// Represents the Clickatell SMS provider
+    /// </summary>
+    public class ClickatellSMSProvider : ISMSProvider
+    {
+        /// <summary>
+        /// Gets the friendly name
+        /// </summary>
+        public string FriendlyName
+        {
+            get { return "Clickatell SMS Provider"; }
+        }
+
+        /// <summary>
+        /// Gets the system name
+        /// </summary>
+        public string SystemName
+        {
+            get { return "Mobile.SMS.Clickatell"; }
+        }
+
+        /// <summary>
+        /// Sends SMS
+        /// </summary>
+        /// <param name="text">Text</param>
+        public bool SendSMS(string text)
+        {
+            try
+            {
+                using (var svc = new PushServerWSPortTypeClient(new BasicHttpBinding(), new EndpointAddress("http://api.clickatell.com/soap/webservice_vs.php")))
+                {
+                    string authRsp = svc.auth(Int32.Parse(ClickatellAPIId), ClickatellUsername, ClickatellPassword);
+
+                    if (!authRsp.ToUpperInvariant().StartsWith("OK"))
+                    {
+                        throw new NopException(authRsp);
+                    }
+
+                    string ssid = authRsp.Substring(4);
+                    string[] sndRsp = svc.sendmsg(ssid,
+                        Int32.Parse(ClickatellAPIId), ClickatellUsername,
+                        ClickatellPassword, new string[1] { ClickatellPhoneNumber },
+                        String.Empty, text, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                        String.Empty, 0, String.Empty, String.Empty, String.Empty, 0);
+
+                    if (!sndRsp[0].ToUpperInvariant().StartsWith("ID"))
+                    {
+                        throw new NopException(sndRsp[0]);
+                    }
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.InsertLog(LogLevel.Error, ex.Message, ex);
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Gets or sets the setting service
+        /// </summary>
+        public ILogger Logger { get; set; }
+
+        /// <summary>
+        /// Gets or sets the setting service
+        /// </summary>
+        public ISettingService SettingService { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Clickatell phone number
+        /// </summary>
+        public string ClickatellPhoneNumber
+        {
+            get
+            {
+                return SettingService.GetSettingByKey<string>("Mobile.SMS.Clickatell.PhoneNumber");
+            }
+            set
+            {
+                SettingService.SetSetting<string>("Mobile.SMS.Clickatell.PhoneNumber", value);
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the Clickatell API ID
+        /// </summary>
+        public string ClickatellAPIId
+        {
+            get
+            {
+                return SettingService.GetSettingByKey<string>("Mobile.SMS.Clickatell.APIID");
+            }
+            set
+            {
+                SettingService.SetSetting<string>("Mobile.SMS.Clickatell.APIID", value);
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the Clickatell username
+        /// </summary>
+        public string ClickatellUsername
+        {
+            get
+            {
+                return SettingService.GetSettingByKey<string>("Mobile.SMS.Clickatell.Username");
+            }
+            set
+            {
+                SettingService.SetSetting<string>("Mobile.SMS.Clickatell.Username", value);
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the Clickatell password
+        /// </summary>
+        public string ClickatellPassword
+        {
+            get
+            {
+                return SettingService.GetSettingByKey<string>("Mobile.SMS.Clickatell.Password");
+            }
+            set
+            {
+                SettingService.SetSetting<string>("Mobile.SMS.Clickatell.Password", value);
+            }
+        }
+    }
+}
---
+++ src/Nop.Plugin.SMS.Verizon/VerizonSMSProvider.cs
@@ -0,0 +1,104 @@
+﻿using System;
+
+using Nop.Core.Domain.Logging;
+using Nop.Core.Domain.Messages;
+using Nop.Services.Configuration;
+using Nop.Services.Logging;
+using Nop.Services.Messages;
+
+namespace Nop.Plugin.SMS.Verizon
+{
+    /// <summary>
+    /// Represents the Verizon SMS provider
+    /// </summary>
+    public class VerizonSMSProvider : ISMSProvider
+    {
+        /// <summary>
+        /// Gets the friendly name
+        /// </summary>
+        public string FriendlyName
+        {
+            get { return "Verizon SMS Provider"; }
+        }
+
+        /// <summary>
+        /// Gets the system name
+        /// </summary>
+        public string SystemName
+        {
+            get { return "Mobile.SMS.Verizon"; }
+        }
+
+        /// <summary>
+        /// Sends SMS
+        /// </summary>
+        /// <param name="text">SMS text</param>
+        /// <returns>Result</returns>
+        public bool SendSMS(string text)
+        {
+            try
+            {
+                var emailAccount = EmailAccountService.DefaultEmailAccount;
+
+                var queuedEmail = new QueuedEmail()
+                {
+                    Priority = 5,
+                    From = emailAccount.Email,
+                    FromName = emailAccount.DisplayName,
+                    To = VerizonEmail,
+                    ToName = string.Empty,
+                    //TODO: implement getting store name
+                    //Subject = SettingService.GetSettingByKey<string>("Common.StoreNamw"),
+                    Body = text,
+                    CreatedOn = DateTime.UtcNow,
+                    EmailAccountId = emailAccount.Id
+                };
+
+                QueuedEmailService.InsertQueuedEmail(queuedEmail);
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Logger.InsertLog(LogLevel.Error, ex.Message, ex);
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the setting service
+        /// </summary>
+        public ISettingService SettingService { get; set; }
+
+        /// <summary>
+        /// Gets or sets the queued email service
+        /// </summary>
+        public IQueuedEmailService QueuedEmailService { get; set; }
+
+        /// <summary>
+        /// Gets or sets the email account service
+        /// </summary>
+        public IEmailAccountService EmailAccountService { get; set; }
+
+        /// <summary>
+        /// Gets or sets the setting service
+        /// </summary>
+        public ILogger Logger { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Verizon email
+        /// </summary>
+        public string VerizonEmail
+        {
+            get
+            {
+                return SettingService.GetSettingByKey<string>("Mobile.SMS.Verizon.Email");
+            }
+            set
+            {
+                SettingService.SetSetting<string>("Mobile.SMS.Verizon.Email", value);
+            }
+        }
+
+    }
+}
