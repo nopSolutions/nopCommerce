79874081b4e39e489b4493f0523e50e8ca58db57
andreymaz <none@none>
2011-04-13 16:35:53 +0400

    Added Nop.Web for 'Nop.Web' project

---
+++ src/Presentation/Nop.Web/Controllers/BaseNopController.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Nop.Web.Controllers
+{
+    public class BaseNopController : Controller
+    {
+    }
+}
