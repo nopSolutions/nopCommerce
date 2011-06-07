f35936d1237d5d17846550155e69b53810633714
andreymaz <none@none>
2011-06-07 22:22:31 +0400

    Admin UI. Notifications

---
+++ src/Presentation/Nop.Web.Framework/UI/NotifyType.cs
@@ -0,0 +1,8 @@
+﻿namespace Nop.Web.Framework.UI
+{
+    public enum NotifyType
+    {
+        Success,
+        Error
+    }
+}
--- src/Presentation/Nop.Web/Administration/Controllers/BaseNopController.cs
+++ src/Presentation/Nop.Web/Administration/Controllers/BaseNopController.cs
@@ -7,6 +7,7 @@ using Nop.Core;
 using Nop.Core.Infrastructure;
 using Nop.Services.Localization;
 using Nop.Web.Framework.Localization;
+using Nop.Web.Framework.UI;
 
 namespace Nop.Admin.Controllers
 {
@@ -43,5 +44,36 @@ namespace Nop.Admin.Controllers
             //return new HttpUnauthorizedResult();
             return RedirectToAction("AccessDenied", "Security", new { pageUrl = this.Request.RawUrl });
         }
+
+
+
+
+
+
+        protected void SuccessNotification(string message, bool persistForTheNextRequest = true)
+        {
+            AddNotification(NotifyType.Success, message, persistForTheNextRequest);
+        }
+        protected void ErrorNotification(string message, bool persistForTheNextRequest = true)
+        {
+            AddNotification(NotifyType.Error, message, persistForTheNextRequest);
+        }
+        protected void AddNotification(NotifyType type, string message, bool persistForTheNextRequest)
+        {
+            string dataKey = string.Format("nop.notifications.{0}", type);
+            if (persistForTheNextRequest)
+            {
+                if (TempData[dataKey] == null)
+                    TempData[dataKey] = new List<string>();
+                ((List<string>)TempData[dataKey]).Add(message);
+            }
+            else
+            {
+                if (ViewData[dataKey] == null)
+                    ViewData[dataKey] = new List<string>();
+                ((List<string>)ViewData[dataKey]).Add(message);
+            }
+        }
+
     }
 }
