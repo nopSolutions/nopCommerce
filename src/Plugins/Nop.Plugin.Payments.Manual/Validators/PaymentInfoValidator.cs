062e8c1f0ac1a5ce41f07a8d052e1bb05931f82e
andreymaz <none@none>
2011-06-10 13:55:11 +0400

    Credit card validation

--- src/Plugins/Nop.Plugin.Payments.Manual/Validators/PaymentInfoValidator.cs
+++ src/Plugins/Nop.Plugin.Payments.Manual/Validators/PaymentInfoValidator.cs
@@ -1,6 +1,7 @@
 ﻿using FluentValidation;
 using Nop.Services.Localization;
 using Nop.Plugin.Payments.Manual.Models;
+using Nop.Web.Framework.Validators;
 
 namespace Nop.Plugin.Payments.Manual.Validators
 {
@@ -8,13 +9,15 @@ namespace Nop.Plugin.Payments.Manual.Validators
     {
         public PaymentInfoValidator(ILocalizationService localizationService)
         {
-            //TODO implement and add custom CC validator and use it here
             //useful links:
             //http://fluentvalidation.codeplex.com/wikipage?title=Custom&referringTitle=Documentation&ANCHOR#CustomValidator
             //http://benjii.me/2010/11/credit-card-validator-attribute-for-asp-net-mvc-3/
+
+            //RuleFor(x => x.CardNumber).NotEmpty().WithMessage(localizationService.GetResource("Payment.CardNumber.Required"));
+            //RuleFor(x => x.CardCode).NotEmpty().WithMessage(localizationService.GetResource("Payment.CardCode.Required"));
+
             RuleFor(x => x.CardholderName).NotEmpty().WithMessage(localizationService.GetResource("Payment.CardholderName.Required"));
-            RuleFor(x => x.CardNumber).NotEmpty().WithMessage(localizationService.GetResource("Payment.CardNumber.Required"));
-            RuleFor(x => x.CardCode).NotEmpty().WithMessage(localizationService.GetResource("Payment.CardCode.Required"));
-            //TODO ensure CardCode is a 3-4 digit integer
+            RuleFor(x => x.CardNumber).IsCreditCard().WithMessage(localizationService.GetResource("Payment.CardNumber.Wrong"));
+            RuleFor(x => x.CardCode).Matches(@"^[0-9]{3,4}$").WithMessage(localizationService.GetResource("Payment.CardCode.Wrong"));
         }}
 }
---
+++ src/Presentation/Nop.Web.Framework/Validators/ValidatorExtensions .cs
@@ -0,0 +1,15 @@
+
+
+using System.Collections.Generic;
+using FluentValidation;
+
+namespace Nop.Web.Framework.Validators
+{
+    public static class MyValidatorExtensions
+    {
+        public static IRuleBuilderOptions<T, string> IsCreditCard<T>(this IRuleBuilder<T, string> ruleBuilder)
+        {
+            return ruleBuilder.SetValidator(new CreditCardPropertyValidator());
+        }
+    }
+}
