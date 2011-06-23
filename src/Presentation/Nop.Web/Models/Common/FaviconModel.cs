bcecd04eb6444a2f13b014bf027af201579c9021
andreymaz <none@none>
2011-06-23 10:26:56 +0400

    Forgot a file

---
+++ src/Presentation/Nop.Web/Models/Common/FaviconModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nop.Web.Framework;
+using Nop.Web.Framework.Mvc;
+
+namespace Nop.Web.Models.Common
+{
+    public class FaviconModel : BaseNopModel
+    {
+        public bool Uploaded { get; set; }
+        public string FaviconUrl { get; set; }
+    }
+}
