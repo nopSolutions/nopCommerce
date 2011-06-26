896bc1bf2fb3f6612bde620c28fd9f9f1004bfd7
andreymaz <none@none>
2011-06-26 21:46:28 +0400

    Validate max length of news comments and product reviews

--- src/Presentation/Nop.Web/Validators/Catalog/ProductReviewsValidator.cs
+++ src/Presentation/Nop.Web/Validators/Catalog/ProductReviewsValidator.cs
@@ -9,6 +9,8 @@ namespace Nop.Web.Validators.Catalog
         public ProductReviewsValidator(ILocalizationService localizationService)
         {
             RuleFor(x => x.AddProductReview.Title).NotEmpty().WithMessage(localizationService.GetResource("Reviews.Fields.Title.Required")).When(x => x.AddProductReview != null);
+            RuleFor(x => x.AddProductReview.Title).Length(1, 200).WithMessage(string.Format(localizationService.GetResource("Reviews.Fields.Title.MaxLengthValidation"), 200)).When(x => x.AddProductReview != null);
             RuleFor(x => x.AddProductReview.ReviewText).NotEmpty().WithMessage(localizationService.GetResource("Reviews.Fields.ReviewText.Required")).When(x => x.AddProductReview != null);
-        }}
+        }
+    }
 }
