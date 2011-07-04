4a97a7b6acbb82ea292b83dc9b0081697b19a4ef
andreymaz <none@none>
2011-07-04 15:11:40 +0400

    Minor changes

--- src/Tests/Nop.Web.MVC.Tests/Admin/Controllers/CategoryControllerTests.cs
+++ src/Tests/Nop.Web.MVC.Tests/Admin/Controllers/CategoryControllerTests.cs
@@ -6,15 +6,17 @@ namespace Nop.Web.MVC.Tests.Admin.Controllers
     public class CategoryControllerTests : Nop.Tests.TestsBase
     {
         //TODO also test MVC views.
-        //Find more inoe here: http://blog.davidebbo.com/2011/06/unit-test-your-mvc-views-using-razor.html
+        //Find more info here: http://blog.davidebbo.com/2011/06/unit-test-your-mvc-views-using-razor.html
 
 
         //TODO ASP.NET MVC Outbound Url tests with NSubstitute.
-        //Find more inoe here: http://blogs.planetcloud.co.uk/mygreatdiscovery/post/ASPNET-MVC-Outbound-Url-tests-with-NSubstitute.aspx
+        //Find more info here: http://blogs.planetcloud.co.uk/mygreatdiscovery/post/ASPNET-MVC-Outbound-Url-tests-with-NSubstitute.aspx
         public override void SetUp()
         {
             base.SetUp();
         }
 
+        //TODO test URLs.
+        //Find more info here: http://dotnetslackers.com/articles/aspnet/Built-in-Unit-Test-for-ASP-NET-MVC-3-in-Visual-Studio-2010-Part-2.aspx
     }
 }
