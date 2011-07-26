430f6ca3c19939d0b54611ba781db0705959dc81
andreymaz <none@none>
2011-07-26 16:13:53 +0400

    Got started on upgrade script (1.90=>2.00).

---
+++ upgradescripts/1.90-2.00/Readme.txt
@@ -0,0 +1,6 @@
+﻿Steps:
+1. Backup your existing database
+2. Install nopCommerce 2.00 to your existing database (1.90)
+3. Backup your existing database (again)
+4. Execute upgrade.sql script over your database
+5. That's all!
---
+++ upgradescripts/1.90-2.00/upgrade.sql
@@ -0,0 +1,160 @@
+﻿--upgrade scripts from nopCommerce 1.90 to nopCommerce 2.00
+
+--temporary table for identifiers
+CREATE TABLE #IDs
+	(
+		[OriginalId] int NOT NULL,
+		[NewId] int NOT NULL,
+		[EntityName] nvarchar(100) NOT NULL
+	)
+GO
+
+--move customer roles
+DECLARE @OriginalCustomerRoleId int
+DECLARE cur_originalcustomerrole CURSOR FOR
+SELECT CustomerRoleId
+FROM [Nop_CustomerRole]
+ORDER BY CustomerRoleId
+OPEN cur_originalcustomerrole
+FETCH NEXT FROM cur_originalcustomerrole INTO @OriginalCustomerRoleId
+WHILE @@FETCH_STATUS = 0
+BEGIN	
+	PRINT 'moving customer role id ' + cast(@OriginalCustomerRoleId as nvarchar(10))
+	INSERT INTO [CustomerRole] ([Name], [FreeShipping], [TaxExempt], [Active], [IsSystemRole])
+	SELECT [Name], [FreeShipping], [TaxExempt], [Active], 0
+	FROM [Nop_CustomerRole]
+	WHERE CustomerRoleId = @OriginalCustomerRoleId and Deleted=0
+
+	--new ID
+	DECLARE @NewCustomerRoleId int
+	SET @NewCustomerRoleId = @@IDENTITY
+
+	--TODO should we delete old built-in customer roles? Solution: Just add a note to upgrade readme file
+	INSERT INTO #IDs  ([OriginalId], [NewId], [EntityName])
+	VALUES (@OriginalCustomerRoleId, @NewCustomerRoleId, N'CustomerRole')
+	--fetch next identifier
+	FETCH NEXT FROM cur_originalcustomerrole INTO @OriginalCustomerRoleId
+END
+CLOSE cur_originalcustomerrole
+DEALLOCATE cur_originalcustomerrole
+GO
+
+
+--move customers
+--delete default admin record from Customer table
+DELETE FROM [Customer]
+WHERE IsSystemAccount=0
+GO
+
+DECLARE @OriginalCustomerId int
+DECLARE cur_originalcustomer CURSOR FOR
+SELECT CustomerId
+FROM [Nop_Customer]
+ORDER BY CustomerId
+OPEN cur_originalcustomer
+FETCH NEXT FROM cur_originalcustomer INTO @OriginalCustomerId
+WHILE @@FETCH_STATUS = 0
+BEGIN	
+	PRINT 'moving customer id ' + cast(@OriginalCustomerId as nvarchar(10))
+	--TODO insert AffiliateId
+	INSERT INTO [Customer] ([CustomerGuid], [Username], [Email], [Password], [PasswordFormatId], [PasswordSalt], [AdminComment], [TaxDisplayTypeId], [IsTaxExempt], [VatNumberStatusId], [UseRewardPointsDuringCheckout], [TimeZoneId], [Active], [Deleted], [IsSystemAccount], [CreatedOnUtc], [LastActivityDateUtc])
+	SELECT [CustomerGuid], [Username], [Email], [PasswordHash], 1 /*hashed*/, [SaltKey], [AdminComment], 0 /*IncludingTax now*/, [IsTaxExempt], 10 /*Empty now*/, 0, [TimeZoneId], [Active], [Deleted], 0, [RegistrationDate], [RegistrationDate]
+	FROM [Nop_Customer]
+	WHERE CustomerId = @OriginalCustomerId
+
+	--new ID
+	DECLARE @NewCustomerId int
+	SET @NewCustomerId = @@IDENTITY
+
+	INSERT INTO #IDs  ([OriginalId], [NewId], [EntityName])
+	VALUES (@OriginalCustomerId, @NewCustomerId, N'Customer')
+
+	--move customer attributes (Gender, Firstname, Lastname, Company, PasswordRecoveryToken, AccountActivationToken)
+	INSERT INTO [CustomerAttribute] ([CustomerId], [Key], [Value])
+	SELECT @NewCustomerId, [key], [value]
+	FROM [Nop_CustomerAttribute]
+	WHERE (CustomerID = @OriginalCustomerId) and 
+	([key] = N'Gender' or [key] = N'FirstName' or [key] = N'LastName' or [key] = N'Company' or [key] = N'PasswordRecoveryToken' or [key] = N'AccountActivationToken') and
+	([value] is not null)
+	--move 'ForumPostCount' customer attribute
+	DECLARE @ForumPostCount int
+	SELECT @ForumPostCount = [TotalForumPosts]
+	FROM [Nop_Customer]
+	WHERE CustomerId = @OriginalCustomerId 
+	IF (@ForumPostCount > 0)
+	BEGIN
+		INSERT INTO [CustomerAttribute] ([CustomerId], [Key], [Value])
+		VALUES (@NewCustomerId, 'ForumPostCount', cast(@ForumPostCount as nvarchar(100)))
+	END
+	--move 'Signature' customer attribute
+	DECLARE @Signature nvarchar(1000)
+	SELECT @Signature = [Signature]
+	FROM [Nop_Customer]
+	WHERE CustomerId = @OriginalCustomerId 
+	IF (len(@Signature) > 0)
+	BEGIN
+		INSERT INTO [CustomerAttribute] ([CustomerId], [Key], [Value])
+		VALUES (@NewCustomerId, 'Signature', @Signature)
+	END
+	--TODO move other customer attributes (AvatarPictureId, LocationCountryId)
+
+	--map customer to customer roles (new system roles)
+	DECLARE @IsAdmin bit
+	DECLARE @IsGuest bit
+	DECLARE @IsRegistered bit
+	DECLARE @IsForumModerator bit
+	SELECT @IsAdmin = IsAdmin, @IsGuest = IsGuest, @IsRegistered = ~IsGuest, @IsForumModerator = IsForumModerator
+	FROM [Nop_Customer]
+	WHERE CustomerId = @OriginalCustomerId
+	DECLARE @AdminCustomerRoleId int
+	DECLARE @GuestCustomerRoleId int
+	DECLARE @RegisteredCustomerRoleId int
+	DECLARE @ForumModeratorCustomerRoleId int
+	SELECT @AdminCustomerRoleId = Id
+	FROM [CustomerRole]
+	WHERE IsSystemRole=1 and [SystemName] = N'Administrators'
+	SELECT @GuestCustomerRoleId = Id
+	FROM [CustomerRole]
+	WHERE IsSystemRole=1 and [SystemName] = N'Guests'
+	SELECT @RegisteredCustomerRoleId = Id
+	FROM [CustomerRole]
+	WHERE IsSystemRole=1 and [SystemName] = N'Registered'
+	SELECT @ForumModeratorCustomerRoleId = Id
+	FROM [CustomerRole]
+	WHERE IsSystemRole=1 and [SystemName] = N'ForumModerators'
+	IF (@IsAdmin = 1)
+	BEGIN
+		INSERT INTO [Customer_CustomerRole_Mapping] ([CustomerRole_Id], [Customer_Id])
+		VALUES (@AdminCustomerRoleId, @NewCustomerId)
+	END	
+	IF (@IsGuest = 1)
+	BEGIN
+		INSERT INTO Customer_CustomerRole_Mapping ([CustomerRole_Id], [Customer_Id])
+		VALUES (@GuestCustomerRoleId, @NewCustomerId)
+	END	
+	IF (@IsRegistered = 1)
+	BEGIN
+		INSERT INTO Customer_CustomerRole_Mapping ([CustomerRole_Id], [Customer_Id])
+		VALUES (@RegisteredCustomerRoleId, @NewCustomerId)
+	END	
+	IF (@IsForumModerator = 1)
+	BEGIN
+		INSERT INTO Customer_CustomerRole_Mapping ([CustomerRole_Id], [Customer_Id])
+		VALUES (@ForumModeratorCustomerRoleId, @NewCustomerId)
+	END
+	--map customer to customer roles(old roles)
+	INSERT INTO [Customer_CustomerRole_Mapping] ([CustomerRole_Id],[Customer_Id])
+	SELECT (SELECT [NewId] FROM #IDs WHERE [EntityName]=N'CustomerRole' and [OriginalId]=original_ccrm.CustomerRoleId ), @NewCustomerId
+	FROM [Nop_Customer_CustomerRole_Mapping] original_ccrm
+	WHERE original_ccrm.CustomerID = @OriginalCustomerId
+
+	--fetch next identifier
+	FETCH NEXT FROM cur_originalcustomer INTO @OriginalCustomerId
+END
+CLOSE cur_originalcustomer
+DEALLOCATE cur_originalcustomer
+GO
+
+--drop temporary table
+DROP TABLE #IDs
+GO
