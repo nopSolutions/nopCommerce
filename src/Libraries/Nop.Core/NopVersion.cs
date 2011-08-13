a46dd2d9454528ab7c7be68caf34be58216a7080
andreymaz <none@none>
2011-08-13 13:24:26 +0400

    Forgot to add one file

---
+++ src/Libraries/Nop.Core/NopVersion.cs
@@ -0,0 +1,17 @@
+﻿
+namespace Nop.Core
+{
+    public static class NopVersion
+    {
+        /// <summary>
+        /// Gets or sets the store version
+        /// </summary>
+        public static string CurrentVersion 
+        {
+            get
+            {
+                return "2.10";
+            }
+        }
+    }
+}
