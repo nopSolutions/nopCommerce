7089bcae64b4d726c248f38bec04711677e91370
andreymaz <none@none>
2011-09-25 12:33:18 +0400

    Rich editor. Added image browser.

--- src/Presentation/Nop.Web.Framework/Controllers/AdminAuthorizeAttribute.cs
+++ src/Presentation/Nop.Web.Framework/Controllers/AdminAuthorizeAttribute.cs
@@ -1,8 +1,6 @@
 ﻿using System.Web;
 using System.Web.Mvc;
-using Nop.Core;
 using Nop.Core.Infrastructure;
-using Nop.Services.Customers;
 using Nop.Services.Security;
 
 namespace Nop.Web.Framework.Controllers
@@ -14,13 +12,6 @@ namespace Nop.Web.Framework.Controllers
             var permissionService = EngineContext.Current.Resolve<IPermissionService>();
             bool result = permissionService.Authorize(StandardPermissionProvider.AccessAdminPanel);
             return result;
-
-
-            //previous implementation
-            //var workContext = EngineContext.Current.Resolve<IWorkContext>();
-            //var user = workContext.CurrentCustomer;
-            //bool result = user != null && user.IsAdmin();
-            //return result;
         }
     }
 }
---
+++ src/Presentation/Nop.Web/Administration/Controllers/ImageBrowserController.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using Nop.Web.Framework.Controllers;
+using Telerik.Web.Mvc.UI;
+
+//Original code can be found here http://demos.telerik.com/aspnet-mvc/razor/editor/imagetool
+namespace Nop.Admin.Controllers
+{
+    [AdminAuthorize]
+    public class ImageBrowserController : EditorFileBrowserController
+    {
+        private const string UploadedImagesFolder = "~/content/images/uploaded/";
+
+        /// <summary>
+        /// Gets the base paths from which content will be served.
+        /// </summary>
+        public override string[] ContentPaths
+        {
+            get
+            {
+                var path = Server.MapPath(UploadedImagesFolder);
+                if (!Directory.Exists(path))
+                    Directory.CreateDirectory(path);
+                return new[] { UploadedImagesFolder };
+            }
+        }
+    }
+}
