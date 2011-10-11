25515db5be743a366df97248fc151194597dfc0d
andreymaz <none@none>
2011-10-11 09:23:05 +0400

    Entity Framework. Throw more detailed exception instead of generic 'Validation failed for one or more entities while saving changes'.

--- src/Libraries/Nop.Data/EfRepository.cs
+++ src/Libraries/Nop.Data/EfRepository.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Data.Entity;
+using System.Diagnostics;
 using System.Linq;
 using Nop.Core;
 using Nop.Core.Data;
+using System.Data.Entity.Validation;
 
 namespace Nop.Data
 {
@@ -18,47 +20,97 @@ namespace Nop.Data
         /// Ctor
         /// </summary>
         /// <param name="context">Object context</param>
-        public EfRepository(IDbContext context) {
+        public EfRepository(IDbContext context)
+        {
             this._context = context;
             this._entities = context.Set<T>();
         }
-        
-        public T GetById(object id) {
+
+        public T GetById(object id)
+        {
             return this._entities.Find(id);
         }
 
         public void Insert(T entity)
         {
-            if (entity == null)
-                throw new ArgumentNullException("entity");
+            try
+            {
+                if (entity == null)
+                    throw new ArgumentNullException("entity");
 
-            this._entities.Add(entity);
+                this._entities.Add(entity);
 
-            this._context.SaveChanges();
+                this._context.SaveChanges();
+            }
+            catch (DbEntityValidationException dbEx)
+            {
+                var msg = string.Empty;
+
+                foreach (var validationErrors in dbEx.EntityValidationErrors)
+                    foreach (var validationError in validationErrors.ValidationErrors)
+                        msg += string.Format("Property: {0} Error: {1}", validationError.PropertyName, validationError.ErrorMessage) + Environment.NewLine;
+
+                var fail = new Exception(msg, dbEx);
+                Debug.WriteLine(fail.Message, fail);
+
+                throw fail;
+            }
         }
 
         public void Update(T entity)
         {
-            if (entity == null)
-                throw new ArgumentNullException("entity");
+            try
+            {
+                if (entity == null)
+                    throw new ArgumentNullException("entity");
 
-            //if (!this._context.IsAttached(entity))
-            //    this._entities.Attach(entity);
+                //if (!this._context.IsAttached(entity))
+                //    this._entities.Attach(entity);
 
-            this._context.SaveChanges();
+                this._context.SaveChanges();
+            }
+            catch (DbEntityValidationException dbEx)
+            {
+                var msg = string.Empty;
+
+                foreach (var validationErrors in dbEx.EntityValidationErrors)
+                    foreach (var validationError in validationErrors.ValidationErrors)
+                        msg += Environment.NewLine + string.Format("Property: {0} Error: {1}", validationError.PropertyName, validationError.ErrorMessage);
+
+                var fail = new Exception(msg, dbEx);
+                Debug.WriteLine(fail.Message, fail);
+
+                throw fail;
+            }
         }
 
         public void Delete(T entity)
         {
-            if (entity == null)
-                throw new ArgumentNullException("entity");
+            try
+            {
+                if (entity == null)
+                    throw new ArgumentNullException("entity");
 
-            //if (!this._context.IsAttached(entity))
-            //    this._entities.Attach(entity);
+                //if (!this._context.IsAttached(entity))
+                //    this._entities.Attach(entity);
 
-            this._entities.Remove(entity);
+                this._entities.Remove(entity);
 
-            this._context.SaveChanges();
+                this._context.SaveChanges();
+            }
+            catch (DbEntityValidationException dbEx)
+            {
+                var msg = string.Empty;
+
+                foreach (var validationErrors in dbEx.EntityValidationErrors)
+                    foreach (var validationError in validationErrors.ValidationErrors)
+                        msg += Environment.NewLine + string.Format("Property: {0} Error: {1}", validationError.PropertyName, validationError.ErrorMessage);
+
+                var fail = new Exception(msg, dbEx);
+                Debug.WriteLine(fail.Message, fail);
+
+                throw fail;
+            }
         }
 
         public virtual IQueryable<T> Table
