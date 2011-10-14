a748891ef08ea8d500f1d16fd02d7eaa182e1c55
andreymaz <none@none>
2011-10-14 11:40:22 +0400

    Created placeholders for the new upgrade scripts (version 2.20 to 2.30)

---
+++ upgradescripts/2.20-2.30(upcoming)/upgrade.additional.sql
@@ -0,0 +1,4 @@
+﻿--additional upgrade scripts from nopCommerce 2.20 to nopCommerce 2.30
+--Execure it if your database supports stored procedures and functions. Note that SQL Compact doesn't support them.
+
+
