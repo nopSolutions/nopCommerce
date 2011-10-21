3bba2611b30ebe2faf4c68d9c218c8605a87682a
andreymaz <none@none>
2011-10-21 15:17:09 +0400

    Added 'Clear cache' background task (as per 1.90). Disabled by default.

---
+++ src/Libraries/Nop.Services/Caching/ClearCacheTask.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics;
+using System.Xml;
+using Nop.Core.Caching;
+using Nop.Core.Tasks;
+
+namespace Nop.Services.Caching
+{
+    /// <summary>
+    /// Clear cache schedueled task implementation
+    /// </summary>
+    public partial class ClearCacheTask : ITask
+    {
+        /// <summary>
+        /// Executes a task
+        /// </summary>
+        /// <param name="node">Xml node that represents a task description</param>
+        public void Execute(XmlNode node)
+        {
+            var cacheManager = new MemoryCacheManager();
+            cacheManager.Clear();
+        }
+    }
+}
