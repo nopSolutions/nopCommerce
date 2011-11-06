29b1a1eb5095d5f790b51f8a502e8370d651a0ad
andreymaz <none@none>
2011-11-06 21:59:28 +0400

    Issue fixed. Cached model validators don't display messages in correct language (when you two or more published languages)

--- src/Presentation/Nop.Web.Framework/NopValidatorFactory.cs
+++ src/Presentation/Nop.Web.Framework/NopValidatorFactory.cs
@@ -8,8 +8,7 @@ namespace Nop.Web.Framework
 {
     public class NopValidatorFactory : AttributedValidatorFactory
     {
-        private readonly InstanceCache _cache = new InstanceCache();
-
+        //private readonly InstanceCache _cache = new InstanceCache();
         public override IValidator GetValidator(Type type)
         {
             if (type != null)
@@ -17,9 +16,11 @@ namespace Nop.Web.Framework
                 var attribute = (ValidatorAttribute)Attribute.GetCustomAttribute(type, typeof(ValidatorAttribute));
                 if ((attribute != null) && (attribute.ValidatorType != null))
                 {
-                    var instance = _cache.GetOrCreateInstance(attribute.ValidatorType,
-                                               x =>
-                                               EngineContext.Current.ContainerManager.ResolveUnregistered(x));
+                    //validators can depend on some customer specific settings (such as working language)
+                    //that's why we do not cache validators
+                    //var instance = _cache.GetOrCreateInstance(attribute.ValidatorType,
+                    //                           x => EngineContext.Current.ContainerManager.ResolveUnregistered(x));
+                    var instance = EngineContext.Current.ContainerManager.ResolveUnregistered(attribute.ValidatorType);
                     return instance as IValidator;
                 }
             }
