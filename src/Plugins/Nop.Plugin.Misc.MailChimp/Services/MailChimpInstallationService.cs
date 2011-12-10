865ef437380d1d79c27e24abd4226ae1dbfef08f
sseverns <none@none>
2011-12-09 18:36:25 -0600

    Added task installation.

--- src/Plugins/Nop.Plugin.Misc.MailChimp/Services/MailChimpInstallationService.cs
+++ src/Plugins/Nop.Plugin.Misc.MailChimp/Services/MailChimpInstallationService.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.Linq;
 using Nop.Core.Domain.Localization;
+using Nop.Core.Domain.Tasks;
 using Nop.Plugin.Misc.MailChimp.Data;
 using Nop.Services.Localization;
+using Nop.Services.Tasks;
 
 namespace Nop.Plugin.Misc.MailChimp.Services {
     public class MailChimpInstallationService {
@@ -11,10 +14,12 @@ namespace Nop.Plugin.Misc.MailChimp.Services {
 
         private readonly ILocalizationService _localizationService;
         private readonly MailChimpObjectContext _mailChimpObjectContext;
+        private readonly IScheduleTaskService _scheduleTaskService;
 
-        public MailChimpInstallationService(MailChimpObjectContext mailChimpObjectContext, ILocalizationService localizationService) {
+        public MailChimpInstallationService(MailChimpObjectContext mailChimpObjectContext, ILocalizationService localizationService, IScheduleTaskService scheduleTaskService) {
             _mailChimpObjectContext = mailChimpObjectContext;
             _localizationService = localizationService;
+            _scheduleTaskService = scheduleTaskService;
         }
 
         /// <summary>
@@ -26,8 +31,35 @@ namespace Nop.Plugin.Misc.MailChimp.Services {
             UpdateOrInstallStringResource(DEFAULT_LIST_NAME, "Default MailChimp List");
             UpdateOrInstallStringResource(AUTO_SYNC_NAME, "Use AutoSync Task");
 
+            //Install sync task
+            InstallSyncTask();
+
             //Install the database tables
             _mailChimpObjectContext.Install();
+        }
+
+        /// <summary>
+        /// Installs the sync task.
+        /// </summary>
+        private void InstallSyncTask() {
+            //Check the database for the task
+            ScheduleTask task = FindScheduledTask();
+
+            if (task == null) {
+                task = new ScheduleTask {
+                        Name = "MailChimp sync",
+                        //each 60 minutes
+                        Seconds = 3600,
+                        Type = "Nop.Plugin.Misc.MailChimp.MailChimpSynchronizationTask, Nop.Plugin.Misc.MailChimp",
+                        Enabled = false,
+                        StopOnError = false,
+                };
+                _scheduleTaskService.InsertTask(task);
+            }
+        }
+
+        private ScheduleTask FindScheduledTask() {
+            return _scheduleTaskService.GetAllTasks().Where(x => x.Type.Equals("Nop.Plugin.Misc.MailChimp.MailChimpSyncTask, Nop.Plugin.Misc.MailChimp", StringComparison.InvariantCultureIgnoreCase)).FirstOrDefault();
         }
 
         /// <summary>
@@ -55,6 +87,10 @@ namespace Nop.Plugin.Misc.MailChimp.Services {
             DeleteStringResource(API_KEY_NAME);
             DeleteStringResource(DEFAULT_LIST_NAME);
             DeleteStringResource(AUTO_SYNC_NAME);
+
+            //Remove scheduled task
+            ScheduleTask task = FindScheduledTask();
+            _scheduleTaskService.DeleteTask(task);
 
             //Uninstall the database tables
             _mailChimpObjectContext.Uninstall();
