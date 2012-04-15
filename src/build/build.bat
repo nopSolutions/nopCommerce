068f61660e4d3d1b7943b74b6333ebb3ae5d3860
andreymaz <none@none>
2012-04-15 11:42:41 +0400

    The build script (deploy.bat) should not generate and deploy PDB files

--- src/build/build.bat
+++ src/build/build.bat
@@ -1,3 +1,3 @@
 @IF NOT EXIST %windir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe @ECHO COULDN'T FIND MSBUILD: %windir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe (Is .NET 4 installed?)
 
-%windir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe nop.proj /maxcpucount /l:FileLogger,Microsoft.Build.Engine;logfile=log.log %*
+%windir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe nop.proj /p:DebugSymbols=false /p:DebugType=None /maxcpucount /l:FileLogger,Microsoft.Build.Engine;logfile=log.log %*
