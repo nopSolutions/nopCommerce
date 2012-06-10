262caf2d367d239c406bd845028e2d26a44ef81a
andreymaz <none@none>
2012-06-10 13:48:21 +0400

    More pluggable infrastructure. Added a new event ("AdminTabStripCreated") which allows you to add custom tabs to admin area pages. For example, if you want to add a new "Custom product info tab" tab to the product details page, then follow the next steps: 1. Create a new class which implements IConsumer<AdminTabStripCreated> interface. Or implement it right in your plugin controller so you can use "RenderPartialViewToString" contoller extension method 2. Implement it the following way: public void HandleEvent(AdminTabStripCreated eventMessage)         {             //we know that tabstrip name on the product details page has "product-edit" name             if (eventMessage.TabStripName == "product-edit")             {                 eventMessage.ItemFactory                     .Add()                     .Text("Custom product info tab")                     .Content("put your content here");                 //hint: use "RenderPartialViewToString" contoller extension method to put content here             }         }

---
+++ src/Presentation/Nop.Web.Framework/Events/AdminTabStripCreated.cs
@@ -0,0 +1,20 @@
+﻿
+using Telerik.Web.Mvc.UI;
+
+namespace Nop.Web.Framework.Events
+{
+    /// <summary>
+    /// Admin tabstrip created event
+    /// </summary>
+    public class AdminTabStripCreated
+    {
+        public AdminTabStripCreated(TabStripItemFactory itemFactory, string tabStripName)
+        {
+            this.ItemFactory = itemFactory;
+            this.TabStripName = tabStripName;
+        }
+
+        public TabStripItemFactory ItemFactory { get; private set; }
+        public string TabStripName { get; private set; }
+    }
+}
