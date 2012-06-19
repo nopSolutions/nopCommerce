57e933f261ceb7dd705537ab61784b1c51126290
andreymaz <none@none>
2012-06-19 20:28:55 +0400

    HTML encoding issue fixed when using checkout attributes with "multiline textbox" type

--- src/Libraries/Nop.Services/Orders/CheckoutAttributeFormatter.cs
+++ src/Libraries/Nop.Services/Orders/CheckoutAttributeFormatter.cs
@@ -95,7 +95,7 @@ namespace Nop.Services.Orders
                             //encode (if required)
                             if (htmlEncode)
                                 attributeName = HttpUtility.HtmlEncode(attributeName);
-                            caAttribute = string.Format("{0}: {1}", attributeName, HtmlHelper.FormatText(valueStr, false, true, true, false, false, false));
+                            caAttribute = string.Format("{0}: {1}", attributeName, HtmlHelper.FormatText(valueStr, false, true, false, false, false, false));
                             //we never encode multiline textbox input
                         }
                         else if (ca.AttributeControlType == AttributeControlType.FileUpload)
