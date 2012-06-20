2e5dc18757875696f1b40b2df4c26adc5b9fdbc2
andreymaz <none@none>
2012-06-20 11:05:10 +0400

    Further changeset on changeset 4c8ff307fd5b ("Localized installation page"). Forgot to add a file

---
+++ src/Presentation/Nop.Web/Infrastructure/Installation/IInstallationLocalizationService.cs
@@ -0,0 +1,19 @@
+﻿
+using System.Collections.Generic;
+
+namespace Nop.Web.Infrastructure.Installation
+{
+    /// <summary>
+    /// Localization service for installation process
+    /// </summary>
+    public partial interface IInstallationLocalizationService
+    {
+        string GetResource(string resourceName);
+
+        InstallationLanguage GetCurrentLanguage();
+
+        void SaveCurrentLanguage(string languageCode);
+
+        IList<InstallationLanguage> GetAvailableLanguages();
+    }
+}
