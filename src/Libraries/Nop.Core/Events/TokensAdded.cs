a149c20fee42e9e89cbc5303c2239825a6d7c040
nicolas <none@none>
2012-06-22 12:46:19 +0200

    Add a new event 'TokensAdded' to allow plugins to manage their own custom tokens. By doing this, plugins can include dynamic content in emails

---
+++ src/Libraries/Nop.Core/Events/TokensAdded.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+namespace Nop.Core.Events
+{
+    /// <summary>
+    /// A container for tokens that are added.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class TokensAdded<T, U> where T : BaseEntity
+    {
+        private readonly T _entity;
+        private readonly IList<U> _tokens;
+
+        public TokensAdded(T entity, IList<U> tokens)
+        {
+            _entity = entity;
+            _tokens = tokens;
+        }
+
+        public T Entity { get { return _entity; } }
+        public IList<U> Tokens { get { return _tokens; } }
+    }
+}
