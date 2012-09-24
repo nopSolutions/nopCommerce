69f5c5f5d9ab3018b6fdf956c906e4a0e017698a
andreymaz <none@none>
2012-09-24 16:38:57 +0400

    Issue fixed. Password field could not contain HTML dangerous chars when logging in (an exception was thrown). Now it allows HTML.

--- src/Presentation/Nop.Web/Models/Customer/LoginModel.cs
+++ src/Presentation/Nop.Web/Models/Customer/LoginModel.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
 using Nop.Web.Framework;
 using Nop.Web.Framework.Mvc;
 
@@ -9,14 +10,17 @@ namespace Nop.Web.Models.Customer
         public bool CheckoutAsGuest { get; set; }
 
         [NopResourceDisplayName("Account.Login.Fields.Email")]
+        [AllowHtml]
         public string Email { get; set; }
 
         public bool UsernamesEnabled { get; set; }
         [NopResourceDisplayName("Account.Login.Fields.UserName")]
+        [AllowHtml]
         public string Username { get; set; }
 
         [DataType(DataType.Password)]
         [NopResourceDisplayName("Account.Login.Fields.Password")]
+        [AllowHtml]
         public string Password { get; set; }
 
         [NopResourceDisplayName("Account.Login.Fields.RememberMe")]
