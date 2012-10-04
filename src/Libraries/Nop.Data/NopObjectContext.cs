782b722920cfc82b21f92f70ec22d84c2ba345d8
andreymaz <none@none>
2012-10-04 10:08:17 +0400

    Source code refactoring (NopObjectContext)

--- src/Libraries/Nop.Data/NopObjectContext.cs
+++ src/Libraries/Nop.Data/NopObjectContext.cs
@@ -26,8 +26,10 @@ namespace Nop.Data
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
             //dynamically load all configuration
-            System.Type configType = typeof(LanguageMap);   //any of your configuration classes here
-            var typesToRegister = Assembly.GetAssembly(configType).GetTypes()
+            //System.Type configType = typeof(LanguageMap);   //any of your configuration classes here
+            //var typesToRegister = Assembly.GetAssembly(configType).GetTypes()
+
+            var typesToRegister = Assembly.GetExecutingAssembly().GetTypes()
             .Where(type => !String.IsNullOrEmpty(type.Namespace))
             .Where(type => type.BaseType != null && type.BaseType.IsGenericType && type.BaseType.GetGenericTypeDefinition() == typeof(EntityTypeConfiguration<>));
             foreach (var type in typesToRegister)
