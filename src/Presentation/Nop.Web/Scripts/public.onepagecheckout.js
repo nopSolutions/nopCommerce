d47814d90f92a80c52b2fed7a432f4b28ca67a1f
andreymaz <none@none>
2012-11-02 12:51:05 +0400

    public.onepagecheckout.js file refactoring

--- src/Presentation/Nop.Web/Scripts/public.onepagecheckout.js
+++ src/Presentation/Nop.Web/Scripts/public.onepagecheckout.js
@@ -126,9 +126,9 @@ var Billing = {
     },
 
     resetSelectedAddress: function () {
-        var selectElement = $('#billing-address-select')
+        var selectElement = $('#billing-address-select');
         if (selectElement) {
-            selectElement.value = '';
+            selectElement.val('');
         }
     },
 
@@ -187,9 +187,9 @@ var Shipping = {
     },
 
     resetSelectedAddress: function () {
-        var selectElement = $('#shipping-address-select')
+        var selectElement = $('#shipping-address-select');
         if (selectElement) {
-            selectElement.value = '';
+            selectElement.val('');
         }
     },
 
