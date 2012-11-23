7204615e917bef42e93be1b44238849b4c62ee3f
andreymaz <none@none>
2012-11-23 13:56:41 +0400

    Added a property to BaseNopModel class. This way developers can extend any model with new properties via an ActionFilter when they need to do so.

--- src/Presentation/Nop.Web.Framework/Mvc/BaseNopModel.cs
+++ src/Presentation/Nop.Web.Framework/Mvc/BaseNopModel.cs
@@ -1,13 +1,30 @@
-﻿using System.Web.Mvc;
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
 
 namespace Nop.Web.Framework.Mvc
 {
+    /// <summary>
+    /// Base nopCommerce model
+    /// </summary>
     public partial class BaseNopModel
     {
+        public BaseNopModel()
+        {
+            this.CustomProperties = new Dictionary<string, object>();
+        }
         public virtual void BindModel(ControllerContext controllerContext, ModelBindingContext bindingContext)
         {
         }
+
+        /// <summary>
+        /// Use this property to store any custom value for your models. 
+        /// </summary>
+        public Dictionary<string, object> CustomProperties { get; set; }
     }
+
+    /// <summary>
+    /// Base nopCommerce entity model
+    /// </summary>
     public partial class BaseNopEntityModel : BaseNopModel
     {
         public virtual int Id { get; set; }
