ec1ac54b0dbff6fc81d9a660b14d1fa12111a3df
andreymaz <none@none>
2013-01-29 18:37:10 +0400

    Implemented a new discount requirement rule plugin ("Restricted to store") which allows a store owner to map discounts to a store.
    --HG--
    branch : multi-store

---
+++ src/Plugins/Nop.Plugin.DiscountRules.Store/Models/RequirementModel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using Nop.Web.Framework;
+
+namespace Nop.Plugin.DiscountRules.Store.Models
+{
+    public class RequirementModel
+    {
+        public RequirementModel()
+        {
+            AvailableStores = new List<SelectListItem>();
+        }
+
+        [NopResourceDisplayName("Plugins.DiscountRules.Store.Fields.Store")]
+        public int StoreId { get; set; }
+
+        public int DiscountId { get; set; }
+
+        public int RequirementId { get; set; }
+
+        public IList<SelectListItem> AvailableStores { get; set; }
+    }
+}
