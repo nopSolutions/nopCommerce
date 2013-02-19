1642c109819e7facf5f7c2456f06a0c4f53d34fb
andreymaz <none@none>
2013-02-19 14:23:56 +0400

    Source code refactoring. Removed code which is not used.

--- src/Libraries/Nop.Core/Infrastructure/DependencyManagement/ContainerConfigurer.cs
+++ src/Libraries/Nop.Core/Infrastructure/DependencyManagement/ContainerConfigurer.cs
@@ -10,17 +10,6 @@ namespace Nop.Core.Infrastructure.DependencyManagement
     /// </summary>
     public class ContainerConfigurer
     {
-        /// <summary>
-        /// Known configuration keys used to configure services.
-        /// </summary>
-        public static class ConfigurationKeys
-        {
-            /// <summary>Key used to configure services intended for medium trust.</summary>
-            public const string MediumTrust = "MediumTrust";
-            /// <summary>Key used to configure services intended for full trust.</summary>
-            public const string FullTrust = "FullTrust";
-        }
-
         public virtual void Configure(IEngine engine, ContainerManager containerManager, EventBroker broker, NopConfig configuration)
         {
             //other dependencies
@@ -47,24 +36,6 @@ namespace Nop.Core.Infrastructure.DependencyManagement
 
             //event broker
             containerManager.AddComponentInstance(broker);
-
-            //service registration
-            containerManager.AddComponent<DependencyAttributeRegistrator>("nop.serviceRegistrator");
-            var registrator = containerManager.Resolve<DependencyAttributeRegistrator>();
-            var services = registrator.FindServices();
-            var configurations = GetComponentConfigurations(configuration);
-            services = registrator.FilterServices(services, configurations);
-            registrator.RegisterServices(services);
-        }
-
-        protected virtual string[] GetComponentConfigurations(NopConfig configuration)
-        {
-            var configurations = new List<string>();
-            string trustConfiguration = (CommonHelper.GetTrustLevel() > System.Web.AspNetHostingPermissionLevel.Medium)
-                ? ConfigurationKeys.FullTrust
-                : ConfigurationKeys.MediumTrust;
-            configurations.Add(trustConfiguration);
-            return configurations.ToArray();
         }
     }
 }
