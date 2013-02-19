8af733a924a19723c4c17628c668e2a089f842c2
andreymaz <none@none>
2013-02-19 14:37:10 +0400

    Updated 51degrees.mobi assembly to the latest version

---
+++ src/packages/51Degrees.mobi.2.1.15.1/content/Mobile/Default.aspx.cs.pp
@@ -0,0 +1,42 @@
+﻿/* *********************************************************************
+ * The contents of this file are subject to the Mozilla Public License 
+ * Version 1.1 (the "License"); you may not use this file except in 
+ * compliance with the License. You may obtain a copy of the License at 
+ * http://www.mozilla.org/MPL/
+ * 
+ * Software distributed under the License is distributed on an "AS IS" 
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing rights and 
+ * limitations under the License.
+ *
+ * The Original Code is named .NET Mobile API, first released under 
+ * this licence on 11th March 2009.
+ * 
+ * The Initial Developer of the Original Code is owned by 
+ * 51 Degrees Mobile Experts Limited. Portions created by 51 Degrees 
+ * Mobile Experts Limited are Copyright (C) 2009 - 2010. All Rights Reserved.
+ * 
+ * Contributor(s):
+ *     James Rosewell <james@51degrees.mobi>
+ *	   Thomas Holmes <tom@51degrees.mobi>
+ * 
+ * ********************************************************************* */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Collections;
+
+namespace $rootnamespace$.Mobile
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
