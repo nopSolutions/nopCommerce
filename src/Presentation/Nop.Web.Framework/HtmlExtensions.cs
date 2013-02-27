0a56c4f2fbdbba2f4a323d0f906665143e853435
andreymaz <none@none>
2013-02-27 13:32:12 +0400

    Created a new HTML extension that renders a text as a label and adds a string suffix to the text of the label. It will be very useful if there is a need for a colon after the text, just like in the _CreateOrUpdateAddress view. Thus there will be no need to change the locale resource and the colons will be inside the label element which is much more better for styling.
    Contributed by www.nop-templates.com

--- src/Presentation/Nop.Web.Framework/HtmlExtensions.cs
+++ src/Presentation/Nop.Web.Framework/HtmlExtensions.cs
@@ -335,6 +335,39 @@ namespace Nop.Web.Framework
             return helper.Action("WidgetsByZone", "Widget", new { widgetZone = widgetZone });
         }
 
+        /// <summary>
+        /// Renders the standard label with a specified suffix added to label text
+        /// </summary>
+        /// <typeparam name="TModel">Model</typeparam>
+        /// <typeparam name="TValue">Value</typeparam>
+        /// <param name="html">HTML helper</param>
+        /// <param name="expression">Expression</param>
+        /// <param name="htmlAttributes">HTML attributes</param>
+        /// <param name="suffix">Suffix</param>
+        /// <returns>Label</returns>
+        public static MvcHtmlString LabelFor<TModel, TValue>(this HtmlHelper<TModel> html, Expression<Func<TModel, TValue>> expression, object htmlAttributes, string suffix)
+        {
+            string htmlFieldName = ExpressionHelper.GetExpressionText((LambdaExpression)expression);
+            var metadata = ModelMetadata.FromLambdaExpression<TModel, TValue>(expression, html.ViewData);
+            string resolvedLabelText = metadata.DisplayName ?? (metadata.PropertyName ?? htmlFieldName.Split(new char[] { '.' }).Last<string>());
+            if (string.IsNullOrEmpty(resolvedLabelText))
+            {
+                return MvcHtmlString.Empty;
+            }
+            var tag = new TagBuilder("label");
+            tag.Attributes.Add("for", TagBuilder.CreateSanitizedId(html.ViewContext.ViewData.TemplateInfo.GetFullHtmlFieldId(htmlFieldName)));
+            if (!String.IsNullOrEmpty(suffix))
+            {
+                resolvedLabelText = String.Concat(resolvedLabelText, suffix);
+            }
+            tag.SetInnerText(resolvedLabelText);
+
+            var dictionary = ((IDictionary<string, object>)HtmlHelper.AnonymousObjectToHtmlAttributes(htmlAttributes));
+            tag.MergeAttributes<string, object>(dictionary, true);
+
+            return MvcHtmlString.Create(tag.ToString(TagRenderMode.Normal));
+        }
+
         #endregion
     }
 }
