1f71a5ec434ff1057e0c986a713ad8b122e9a80c
andreymaz <none@none>
2013-03-15 16:24:08 +0400

    More source code comments

--- src/Libraries/Nop.Services/Events/ISubscriptionService.cs
+++ src/Libraries/Nop.Services/Events/ISubscriptionService.cs
@@ -2,8 +2,16 @@
 
 namespace Nop.Services.Events
 {
+    /// <summary>
+    /// Event subscrption service
+    /// </summary>
     public interface ISubscriptionService
     {
+        /// <summary>
+        /// Get subscriptions
+        /// </summary>
+        /// <typeparam name="T">Type</typeparam>
+        /// <returns>Event consumers</returns>
         IList<IConsumer<T>> GetSubscriptions<T>();
     }
 }
--- src/Libraries/Nop.Services/Events/SubscriptionService.cs
+++ src/Libraries/Nop.Services/Events/SubscriptionService.cs
@@ -3,8 +3,16 @@ using Nop.Core.Infrastructure;
 
 namespace Nop.Services.Events
 {
+    /// <summary>
+    /// Event subscrption service
+    /// </summary>
     public class SubscriptionService : ISubscriptionService
     {
+        /// <summary>
+        /// Get subscriptions
+        /// </summary>
+        /// <typeparam name="T">Type</typeparam>
+        /// <returns>Event consumers</returns>
         public IList<IConsumer<T>> GetSubscriptions<T>()
         {
             return EngineContext.Current.ResolveAll<IConsumer<T>>();
