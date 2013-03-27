3bc7e63d035e742b4d2eb0d939f8b94a8f3eada9
andreymaz <none@none>
2013-03-27 20:17:51 +0400

    Added unit tests for MemoryCacheManager

---
+++ src/Tests/Nop.Core.Tests/Caching/MemoryCacheManagerTests.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using Nop.Core.Caching;
+using Nop.Tests;
+
+namespace Nop.Core.Tests.Caching
+{
+    [TestFixture]
+    public class MemoryCacheManagerTests
+    {
+        [Test]
+        public void Can_set_and_get_object_from_cache()
+        {
+            var cacheManager = new MemoryCacheManager();
+            cacheManager.Set("some_key_1", 3, int.MaxValue);
+
+            cacheManager.Get<int>("some_key_1").ShouldEqual(3);
+        }
+
+        [Test]
+        public void Can_validate_whetherobject_is_cached()
+        {
+            var cacheManager = new MemoryCacheManager();
+            cacheManager.Set("some_key_1", 3, int.MaxValue);
+            cacheManager.Set("some_key_2", 4, int.MaxValue);
+
+            cacheManager.IsSet("some_key_1").ShouldEqual(true);
+            cacheManager.IsSet("some_key_3").ShouldEqual(false);
+        }
+
+        [Test]
+        public void Can_clear_cache()
+        {
+            var cacheManager = new MemoryCacheManager();
+            cacheManager.Set("some_key_1", 3, int.MaxValue);
+
+            cacheManager.Clear();
+
+            cacheManager.IsSet("some_key_1").ShouldEqual(false);
+        }
+    }
+}
