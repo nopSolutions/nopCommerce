ecb95eb440cce9a8da8139aa63b164c3045611da
andreymaz <none@none>
2013-06-18 23:00:06 +0400

    Minor refactoring
    --HG--
    branch : new-product-logic

--- src/Presentation/Nop.Web/Scripts/public.common.js
+++ src/Presentation/Nop.Web/Scripts/public.common.js
@@ -3,16 +3,6 @@
 */
 
 
-function getE(name) {
-    //Obsolete since nopCommerce 2.60. But still here for backwards compatibility (in case of some plugin developers used it in their plugins or customized solutions)
-    if (document.getElementById)
-        var elem = document.getElementById(name);
-    else if (document.all)
-        var elem = document.all[name];
-    else if (document.layers)
-        var elem = document.layers[name];
-    return elem;
-}
 
 function OpenWindow(query, w, h, scroll) {
     var l = (screen.width - w) / 2;
---
+++ src/Presentation/Nop.Web/Scripts/public.common.mobile.js
@@ -0,0 +1,21 @@
+﻿/*
+** nopCommerce custom js functions for mobile devices
+*/
+
+
+function setLocation(url) {
+    window.location.href = url;
+}
+
+function displayStandardAlertNotification(message) {
+    var alertText = '';
+    if ((typeof message) == 'string') {
+        alertText = message;
+    } else {
+        for (var i = 0; i < message.length; i++) {
+            alertText = alertText + '\\n' + message[i];
+        }
+    }
+
+    alert(alertText);
+}
