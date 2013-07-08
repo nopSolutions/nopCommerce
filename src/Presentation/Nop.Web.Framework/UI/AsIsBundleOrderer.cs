c630c1ca115ce36e21ec0a3c62d8eee7d29c6ea5
andreymaz <none@none>
2013-07-08 13:01:13 +0400

    Updated third-aprty assemblies to the latest version

--- src/Presentation/Nop.Web.Framework/UI/AsIsBundleOrderer.cs
+++ src/Presentation/Nop.Web.Framework/UI/AsIsBundleOrderer.cs
@@ -1,12 +1,11 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
 using System.Web.Optimization;
 
 namespace Nop.Web.Framework.UI
 {
     public partial class AsIsBundleOrderer : IBundleOrderer
     {
-        public virtual IEnumerable<System.IO.FileInfo> OrderFiles(BundleContext context, IEnumerable<System.IO.FileInfo> files)
+        public virtual IEnumerable<BundleFile> OrderFiles(BundleContext context, IEnumerable<BundleFile> files)
         {
             return files;
         }
