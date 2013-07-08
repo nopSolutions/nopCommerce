23be68a9b551980d2b9f9ee47345182103b84423
andreymaz <none@none>
2013-07-08 17:38:51 +0400

    Added a note about noise words to "full-text settings" page

--- upgradescripts/3.00-the next version/upgrade.sql
+++ upgradescripts/3.00-the next version/upgrade.sql
@@ -1400,6 +1400,9 @@ set @resources='
   <LocaleResource Name="Admin.Common.Select">
     <Value>Select</Value>
   </LocaleResource>
+  <LocaleResource Name="Admin.Configuration.Settings.GeneralCommon.FullTextSettings.NoiseWords">
+    <Value>To prevent a full-text index from becoming bloated, Microsoft SQL Server has a mechanism that discards commonly occurring words that do not help the search. These words are called noise words, or stop words. Noise words are listed in the locale specific noise word files. For example, in the English locale, words such as "a," "and," "is," and "the" are in the English noise word file and are left out of the full-text index since they are empirically known to be useless to a search. Please contact your SQL Server administrator to get more information about it.</Value>
+  </LocaleResource>
 </Language>
 '
 
