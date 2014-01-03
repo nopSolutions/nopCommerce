3d3e26347bcbaaf4c19ec41a6d257b50bf2c3123
andreymaz <none@none>
2014-01-03 14:51:41 +0400

    The products list page (admin area). There's no need to return full description on this page. It improves performance and ensure that no "Error during serialization or deserialization using the JSON JavaScriptSerializer" excepion is thrown.

--- src/Presentation/Nop.Web/Administration/Controllers/ProductController.cs
+++ src/Presentation/Nop.Web/Administration/Controllers/ProductController.cs
@@ -717,6 +717,13 @@ namespace Nop.Admin.Controllers
             gridModel.Data = products.Select(x =>
             {
                 var productModel = x.ToModel();
+                //little hack here:
+                //ensure that product full descriptions are not returned
+                //otherwise, we can get the following error if products have too long descriptions:
+                //"Error during serialization or deserialization using the JSON JavaScriptSerializer. The length of the string exceeds the value set on the maxJsonLength property. "
+                //also it improves performance
+                productModel.FullDescription = "";
+
                 if (_adminAreaSettings.DisplayProductPictures)
                 {
                     var defaultProductPicture = _pictureService.GetPicturesByProductId(x.Id, 1).FirstOrDefault();
