5330029b1df290e0ccf8723ad702f3ef982488dd
andreymaz <none@none>
2014-02-07 18:01:20 +0400

    Trim (remove extra spaces) address fields. Otherwise, it can cause issues when using PayPal.

--- src/Presentation/Nop.Web/Extensions/MappingExtensions.cs
+++ src/Presentation/Nop.Web/Extensions/MappingExtensions.cs
@@ -191,19 +191,42 @@ namespace Nop.Web.Extensions
             model.FaxEnabled = addressSettings.FaxEnabled;
             model.FaxRequired = addressSettings.FaxRequired;
         }
-        public static Address ToEntity(this AddressModel model)
+        public static Address ToEntity(this AddressModel model, bool trimFields = true)
         {
             if (model == null)
                 return null;
 
             var entity = new Address();
-            return ToEntity(model, entity);
+            return ToEntity(model, entity, trimFields);
         }
-        public static Address ToEntity(this AddressModel model, Address destination)
+        public static Address ToEntity(this AddressModel model, Address destination, bool trimFields = true)
         {
             if (model == null)
                 return destination;
 
+            if (trimFields)
+            {
+                if (model.FirstName != null)
+                    model.FirstName = model.FirstName.Trim();
+                if (model.LastName != null)
+                    model.LastName = model.LastName.Trim();
+                if (model.Email != null)
+                    model.Email = model.Email.Trim();
+                if (model.Company != null)
+                    model.Company = model.Company.Trim();
+                if (model.City != null)
+                    model.City = model.City.Trim();
+                if (model.Address1 != null)
+                    model.Address1 = model.Address1.Trim();
+                if (model.Address2 != null)
+                    model.Address2 = model.Address2.Trim();
+                if (model.ZipPostalCode != null)
+                    model.ZipPostalCode = model.ZipPostalCode.Trim();
+                if (model.PhoneNumber != null)
+                    model.PhoneNumber = model.PhoneNumber.Trim();
+                if (model.FaxNumber != null)
+                    model.FaxNumber = model.FaxNumber.Trim();
+            }
             destination.Id = model.Id;
             destination.FirstName = model.FirstName;
             destination.LastName = model.LastName;
