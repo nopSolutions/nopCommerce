af895742b4fe3f655f0c1f3f6c79b3f695c29451
andreymaz <none@none>
2014-03-06 19:12:02 +0400

    Source code refactoring

--- src/Presentation/Nop.Web.Framework/Mvc/NopModelBinder.cs
+++ src/Presentation/Nop.Web.Framework/Mvc/NopModelBinder.cs
@@ -7,19 +7,13 @@ namespace Nop.Web.Framework.Mvc
         public override object BindModel(ControllerContext controllerContext, ModelBindingContext bindingContext)
         {
             var model = base.BindModel(controllerContext, bindingContext);
-            if (model is BaseNopModel) ((BaseNopModel) model).BindModel(controllerContext, bindingContext);
+            if (model is BaseNopModel)
+            {
+                ((BaseNopModel)model).BindModel(controllerContext, bindingContext);
+            }
             return model;
         }
-        protected override object GetPropertyValue(ControllerContext controllerContext, ModelBindingContext bindingContext, System.ComponentModel.PropertyDescriptor propertyDescriptor, IModelBinder propertyBinder)
-        {
-            var value = base.GetPropertyValue(controllerContext, bindingContext, propertyDescriptor, propertyBinder);
-            return value;
-        }
 
-        protected override void BindProperty(ControllerContext controllerContext, ModelBindingContext bindingContext, System.ComponentModel.PropertyDescriptor propertyDescriptor)
-        {
-            base.BindProperty(controllerContext, bindingContext, propertyDescriptor);
-        }
         protected override System.ComponentModel.PropertyDescriptorCollection GetModelProperties(ControllerContext controllerContext, ModelBindingContext bindingContext)
         {
             bindingContext.PropertyFilter = new System.Predicate<string>(pred);
