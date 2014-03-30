30ed9e30e06c322f0d1a74886370852627052a82
andreymaz <none@none>
2014-03-30 11:22:33 +0400

    Prepared fast SQL installation scripts for Windows Azure package (web plaform installer)

--- src/Libraries/Nop.Services/Installation/CodeFirstInstallationService.cs
+++ src/Libraries/Nop.Services/Installation/CodeFirstInstallationService.cs
@@ -1,10 +1,7 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Xml;
 using Nop.Core;
 using Nop.Core.Data;
 using Nop.Core.Domain;
