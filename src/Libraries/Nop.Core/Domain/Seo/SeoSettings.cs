53b4c40a9c0ca31a36f26aa3c4d2ce0f4f879fbe
andreymaz <none@none>
2014-05-30 10:16:40 +0400

    More source code comments

--- src/Libraries/Nop.Core/Domain/Seo/SeoSettings.cs
+++ src/Libraries/Nop.Core/Domain/Seo/SeoSettings.cs
@@ -8,34 +8,54 @@ namespace Nop.Core.Domain.Seo
     /// </summary>
     public class SeoSettings : ISettings
     {
+        /// <summary>
+        /// Page title separator
+        /// </summary>
         public string PageTitleSeparator { get; set; }
+        /// <summary>
+        /// Page itle SEO adjustment
+        /// </summary>
         public PageTitleSeoAdjustment PageTitleSeoAdjustment { get; set; }
+        /// <summary>
+        /// Default title
+        /// </summary>
         public string DefaultTitle { get; set; }
+        /// <summary>
+        /// Default META keywords
+        /// </summary>
         public string DefaultMetaKeywords { get; set; }
+        /// <summary>
+        /// Default META description
+        /// </summary>
         public string DefaultMetaDescription { get; set; }
-
         /// <summary>
         /// A value indicating whether product META descriptions will be generated automatically (if not entered)
         /// </summary>
         public bool GenerateProductMetaDescription { get; set; }
-
+        /// <summary>
+        /// A value indicating whether we should conver non-wetern chars to western ones
+        /// </summary>
         public bool ConvertNonWesternChars { get; set; }
+        /// <summary>
+        /// A value indicating whether unicode chars are allowed
+        /// </summary>
         public bool AllowUnicodeCharsInUrls { get; set; }
-
+        /// <summary>
+        /// A value indicating whether canonical URL tags should be used
+        /// </summary>
         public bool CanonicalUrlsEnabled { get; set; }
-
+        /// <summary>
+        /// WWW requires (with or without WWW)
+        /// </summary>
         public WwwRequirement WwwRequirement { get; set; }
-
         /// <summary>
         /// A value indicating whether JS file bundling and minification is enabled
         /// </summary>
         public bool EnableJsBundling { get; set; }
-
         /// <summary>
         /// A value indicating whether CSS file bundling and minification is enabled
         /// </summary>
         public bool EnableCssBundling { get; set; }
-
         /// <summary>
         /// Slugs (sename) reserved for some other needs
         /// </summary>
