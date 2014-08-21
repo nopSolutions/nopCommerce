4b13537240a616f1832474d8e59908a750ef1e9b
AndreyMaz <none@none>
2014-08-21 16:36:03 +0400

    Source code refactoring. Renamed ProductVariantExtensions to ProductExtensions.

---
+++ src/Libraries/Nop.Core/Domain/Catalog/ProductExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Nop.Core.Domain.Catalog
+{
+    public static class ProductExtensions
+    {
+        public static int[] ParseRequiredProductIds(this Product product)
+        {
+            if (product == null)
+                throw new ArgumentNullException("product");
+
+            if (String.IsNullOrEmpty(product.RequiredProductIds))
+                return new int[0];
+
+            var ids = new List<int>();
+
+            foreach (var idStr in product.RequiredProductIds
+                .Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(x => x.Trim()))
+            {
+                int id = 0;
+                if (int.TryParse(idStr, out id))
+                    ids.Add(id);
+            }
+
+            return ids.ToArray();
+        }
+    }
+}
