0657c2ab3ef4a5c823b7ee8ede4f14686244cd14
AndreyMaz <none@none>
2014-09-01 13:52:44 +0400

    Source code refactoring. Renamed AdminMenuPlugin.cs file name to IAdminMenuPlugin.cs as it should be.

---
+++ src/Presentation/Nop.Web.Framework/Web/IAdminMenuPlugin.cs
@@ -0,0 +1,20 @@
+﻿using Nop.Core.Plugins;
+using Nop.Web.Framework.Menu;
+
+namespace Nop.Web.Framework.Web
+{
+    public interface IAdminMenuPlugin : IPlugin
+    {
+        /// <summary>
+        /// Authenticate a user (can he see this plugin menu item?)
+        /// </summary>
+        /// <returns></returns>
+        bool Authenticate();
+
+        /// <summary>
+        /// Build menu item
+        /// </summary>
+        /// <returns>Site map item</returns>
+        SiteMapNode BuildMenuItem();
+    }
+}
