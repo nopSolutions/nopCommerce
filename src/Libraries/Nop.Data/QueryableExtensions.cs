899d8a050082ea6e200aef1e4f654c39570304ef
AndreyMaz <none@none>
2014-10-10 12:43:27 +0400

    Added "IncludeProperties" method to repositories. Now developers can include a list of properties to load with a single SQL request.

---
+++ src/Libraries/Nop.Data/QueryableExtensions.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Nop.Data 
+{
+    /// <summary>
+    /// Queryable extensions
+    /// </summary>
+    public static class QueryableExtensions
+    {
+        /// <summary>
+        /// Include
+        /// </summary>
+        /// <typeparam name="T">Type</typeparam>
+        /// <param name="queryable">Queryable</param>
+        /// <param name="includeProperties">A list of properties to include</param>
+        /// <returns>New queryable</returns>
+        public static IQueryable<T> IncludeProperties<T>(this IQueryable<T> queryable,
+            params Expression<Func<T, object>>[] includeProperties)
+        {
+            if (queryable == null)
+                throw new ArgumentNullException("queryable");
+
+            foreach (Expression<Func<T, object>> includeProperty in includeProperties)
+                queryable = queryable.Include(includeProperty);
+
+            return queryable;
+        }
+
+    }
+}
