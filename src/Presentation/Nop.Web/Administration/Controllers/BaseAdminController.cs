dbc5810213b332550e0088163243456c913e8acb
AndreyMaz <none@none>
2014-11-24 17:38:21 +0300

    Further changes on recent changeset related to payment plugin description localization. Removed duplicated code.

--- src/Presentation/Nop.Web/Administration/Controllers/BaseAdminController.cs
+++ src/Presentation/Nop.Web/Administration/Controllers/BaseAdminController.cs
@@ -39,37 +39,6 @@ namespace Nop.Admin.Controllers
         }
         
         /// <summary>
-        /// Add locales for localizable entities
-        /// </summary>
-        /// <typeparam name="TLocalizedModelLocal">Localizable model</typeparam>
-        /// <param name="languageService">Language service</param>
-        /// <param name="locales">Locales</param>
-        protected virtual void AddLocales<TLocalizedModelLocal>(ILanguageService languageService, IList<TLocalizedModelLocal> locales) where TLocalizedModelLocal : ILocalizedModelLocal
-        {
-            AddLocales(languageService, locales, null);
-        }
-        /// <summary>
-        /// Add locales for localizable entities
-        /// </summary>
-        /// <typeparam name="TLocalizedModelLocal">Localizable model</typeparam>
-        /// <param name="languageService">Language service</param>
-        /// <param name="locales">Locales</param>
-        /// <param name="configure">Configure action</param>
-        protected virtual void AddLocales<TLocalizedModelLocal>(ILanguageService languageService, IList<TLocalizedModelLocal> locales, Action<TLocalizedModelLocal, int> configure) where TLocalizedModelLocal : ILocalizedModelLocal
-        {
-            foreach (var language in languageService.GetAllLanguages(true))
-            {
-                var locale = Activator.CreateInstance<TLocalizedModelLocal>();
-                locale.LanguageId = language.Id;
-                if (configure != null)
-                {
-                    configure.Invoke(locale, locale.LanguageId);
-                }
-                locales.Add(locale);
-            }
-        }
-
-        /// <summary>
         /// Access denied view
         /// </summary>
         /// <returns>Access denied view</returns>
