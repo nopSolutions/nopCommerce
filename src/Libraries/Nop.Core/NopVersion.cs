9bebd33480636fecac9000fb83c2b935b5393dea
AndreyMaz <none@none>
2014-12-08 21:09:39 +0300

    Set version number to 3.60 (the next one). This version should not be used in a production environment. We do not guarantee that any of the functionality or code found in our DCS (distributed control system) will be made available in our official releases.

--- src/Libraries/Nop.Core/NopVersion.cs
+++ src/Libraries/Nop.Core/NopVersion.cs
@@ -10,7 +10,7 @@ namespace Nop.Core
         {
             get
             {
-                return "3.50";
+                return "3.60";
             }
         }
     }
--- src/Presentation/Nop.Web.Framework/Properties/AssemblyInfo.cs
+++ src/Presentation/Nop.Web.Framework/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.5.0.0")]
-[assembly: AssemblyFileVersion("3.5.0.0")]
+[assembly: AssemblyVersion("3.6.0.0")]
+[assembly: AssemblyFileVersion("3.6.0.0")]
