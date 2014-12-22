c040890a59a23032191c355f628a0f27cd5f88ea
AndreyMaz <none@none>
2014-12-22 19:37:13 +0300

    Source code refactoring. Refactoring of SitemapGenerator class.

--- src/Libraries/Nop.Services/Seo/SitemapGenerator.cs
+++ src/Libraries/Nop.Services/Seo/SitemapGenerator.cs
@@ -1,6 +1,9 @@
 using System;
+using System.IO;
 using System.Linq;
+using System.Text;
 using System.Web.Mvc;
+using System.Xml;
 using Nop.Core;
 using Nop.Core.Domain.Blogs;
 using Nop.Core.Domain.Catalog;
@@ -15,8 +18,10 @@ namespace Nop.Services.Seo
     /// <summary>
     /// Represents a sitemap generator
     /// </summary>
-    public partial class SitemapGenerator : BaseSitemapGenerator, ISitemapGenerator
+    public partial class SitemapGenerator : ISitemapGenerator
     {
+        #region Fields
+
         private readonly IStoreContext _storeContext;
         private readonly ICategoryService _categoryService;
         private readonly IProductService _productService;
@@ -26,6 +31,13 @@ namespace Nop.Services.Seo
         private readonly BlogSettings _blogSettings;
         private readonly NewsSettings _newsSettings;
         private readonly ForumSettings _forumSettings;
+
+        private const string DateFormat = @"yyyy-MM-dd";
+        private XmlTextWriter _writer;
+
+        #endregion
+
+        #region Ctor
 
         public SitemapGenerator(IStoreContext storeContext,
             ICategoryService categoryService,
@@ -48,12 +60,32 @@ namespace Nop.Services.Seo
             this._forumSettings = forumSettings;
         }
 
+        #endregion
+
+        #region Utilities
+
+        /// <summary>
+        /// Writes the url location to the writer.
+        /// </summary>
+        /// <param name="url">Url of indexed location (don't put root url information in).</param>
+        /// <param name="updateFrequency">Update frequency - always, hourly, daily, weekly, yearly, never.</param>
+        /// <param name="lastUpdated">Date last updated.</param>
+        protected virtual void WriteUrlLocation(string url, UpdateFrequency updateFrequency, DateTime lastUpdated)
+        {
+            _writer.WriteStartElement("url");
+            string loc = XmlHelper.XmlEncode(url);
+            _writer.WriteElementString("loc", loc);
+            _writer.WriteElementString("changefreq", updateFrequency.ToString().ToLowerInvariant());
+            _writer.WriteElementString("lastmod", lastUpdated.ToString(DateFormat));
+            _writer.WriteEndElement();
+        }
+
         /// <summary>
         /// Method that is overridden, that handles creation of child urls.
         /// Use the method WriteUrlLocation() within this method.
         /// </summary>
         /// <param name="urlHelper">URL helper</param>
-        protected override void GenerateUrlNodes(UrlHelper urlHelper)
+        protected virtual void GenerateUrlNodes(UrlHelper urlHelper)
         {
             //home page
             var homePageUrl = urlHelper.RouteUrl("HomePage", null, "http");
@@ -101,7 +133,7 @@ namespace Nop.Services.Seo
             WriteTopics(urlHelper);
         }
 
-        private void WriteCategories(UrlHelper urlHelper, int parentCategoryId)
+        protected virtual void WriteCategories(UrlHelper urlHelper, int parentCategoryId)
         {
             var categories = _categoryService.GetAllCategoriesByParentCategoryId(parentCategoryId);
             foreach (var category in categories)
@@ -113,7 +145,7 @@ namespace Nop.Services.Seo
             }
         }
 
-        private void WriteManufacturers(UrlHelper urlHelper)
+        protected virtual void WriteManufacturers(UrlHelper urlHelper)
         {
             var manufacturers = _manufacturerService.GetAllManufacturers();
             foreach (var manufacturer in manufacturers)
@@ -123,7 +155,7 @@ namespace Nop.Services.Seo
             }
         }
 
-        private void WriteProducts(UrlHelper urlHelper)
+        protected virtual void WriteProducts(UrlHelper urlHelper)
         {
             var products = _productService.SearchProducts(
                 storeId: _storeContext.CurrentStore.Id,
@@ -136,7 +168,7 @@ namespace Nop.Services.Seo
             }
         }
 
-        private void WriteTopics(UrlHelper urlHelper)
+        protected virtual void WriteTopics(UrlHelper urlHelper)
         {
             var topics = _topicService.GetAllTopics(_storeContext.CurrentStore.Id)
                 .Where(t => t.IncludeInSitemap)
@@ -147,5 +179,48 @@ namespace Nop.Services.Seo
                 WriteUrlLocation(url, UpdateFrequency.Weekly, DateTime.UtcNow);
             }
         }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// This will build an xml sitemap for better index with search engines.
+        /// See http://en.wikipedia.org/wiki/Sitemaps for more information.
+        /// </summary>
+        /// <param name="urlHelper">URL helper</param>
+        /// <returns>Sitemap.xml as string</returns>
+        public virtual string Generate(UrlHelper urlHelper)
+        {
+            using (var stream = new MemoryStream())
+            {
+                Generate(urlHelper, stream);
+                return Encoding.UTF8.GetString(stream.ToArray());
+            }
+        }
+
+        /// <summary>
+        /// This will build an xml sitemap for better index with search engines.
+        /// See http://en.wikipedia.org/wiki/Sitemaps for more information.
+        /// </summary>
+        /// <param name="urlHelper">URL helper</param>
+        /// <param name="stream">Stream of sitemap.</param>
+        public virtual void Generate(UrlHelper urlHelper, Stream stream)
+        {
+            _writer = new XmlTextWriter(stream, Encoding.UTF8);
+            _writer.Formatting = Formatting.Indented;
+            _writer.WriteStartDocument();
+            _writer.WriteStartElement("urlset");
+            _writer.WriteAttributeString("xmlns", "http://www.sitemaps.org/schemas/sitemap/0.9");
+            _writer.WriteAttributeString("xmlns:xsi", "http://www.w3.org/2001/XMLSchema-instance");
+            _writer.WriteAttributeString("xsi:schemaLocation", "http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd");
+
+            GenerateUrlNodes(urlHelper);
+
+            _writer.WriteEndElement();
+            _writer.Close();
+        }
+
+        #endregion
     }
 }
