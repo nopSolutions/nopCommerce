222d7a8a49d27d0d900dc27510f836dfbde47da4
AndreyMaz <none@none>
2015-01-10 15:05:46 +0300

    #12487 Specification attributes could not be edited when custom HTML chars were used.

--- src/Presentation/Nop.Web/Administration/Models/Catalog/ProductSpecificationAttributeModel.cs
+++ src/Presentation/Nop.Web/Administration/Models/Catalog/ProductSpecificationAttributeModel.cs
@@ -1,13 +1,17 @@
-﻿using Nop.Web.Framework.Mvc;
+﻿using System.Web.Mvc;
+using Nop.Web.Framework.Mvc;
 
 namespace Nop.Admin.Models.Catalog
 {
     public partial class ProductSpecificationAttributeModel : BaseNopEntityModel
     {
+        [AllowHtml]
         public string AttributeTypeName { get; set; }
 
+        [AllowHtml]
         public string AttributeName { get; set; }
 
+        [AllowHtml]
         public string ValueRaw { get; set; }
 
         public bool AllowFiltering { get; set; }
