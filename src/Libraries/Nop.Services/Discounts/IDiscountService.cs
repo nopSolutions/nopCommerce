ec28c69958182c017bf93950b62ab7287adb14e2
AndreyMaz <none@none>
2015-02-27 19:11:21 +0300

    Source code comment typo fixed

--- src/Libraries/Nop.Services/Discounts/IDiscountService.cs
+++ src/Libraries/Nop.Services/Discounts/IDiscountService.cs
@@ -27,7 +27,7 @@ namespace Nop.Services.Discounts
         /// Gets all discounts
         /// </summary>
         /// <param name="discountType">Discount type; null to load all discount</param>
-        /// <param name="couponCode">Coupon code to find (exactl match)</param>
+        /// <param name="couponCode">Coupon code to find (exact match)</param>
         /// <param name="showHidden">A value indicating whether to show hidden records</param>
         /// <returns>Discount collection</returns>
         IList<Discount> GetAllDiscounts(DiscountType? discountType, string couponCode = "", bool showHidden = false);
