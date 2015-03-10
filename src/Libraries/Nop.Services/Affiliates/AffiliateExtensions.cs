148231a819d11c775781b5e7de52496d76ec8142
AndreyMaz <none@none>
2015-03-10 19:36:36 +0300

    #12561 When entering "friendly URL name" for affliate we should ensure that it's not reserved by other affliate entity

--- src/Libraries/Nop.Services/Affiliates/AffiliateExtensions.cs
+++ src/Libraries/Nop.Services/Affiliates/AffiliateExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Nop.Core;
 using Nop.Core.Domain.Affiliates;
+using Nop.Core.Infrastructure;
 using Nop.Services.Seo;
 
 namespace Nop.Services.Affiliates
@@ -39,14 +40,41 @@ namespace Nop.Services.Affiliates
         /// <returns>Valid friendly name</returns>
         public static string ValidateFriendlyUrlName(this Affiliate affiliate, string friendlyUrlName)
         {
+            if (affiliate == null)
+                throw new ArgumentNullException("affiliate");
+
             //ensure we have only valid chars
             friendlyUrlName = SeoExtensions.GetSeName(friendlyUrlName);
+
 
             //max length
             //For long URLs we can get the following error:
             //"the specified path, file name, or both are too long. The fully qualified file name must be less than 260 characters, and the directory name must be less than 248 characters"
             //that's why we limit it to 200 here (consider a store URL + probably added {0}-{1} below)
             friendlyUrlName = CommonHelper.EnsureMaximumLength(friendlyUrlName, 200);
+
+
+            //ensure this name is not reserved yet
+            //empty? nothing to check
+            if (String.IsNullOrEmpty(friendlyUrlName))
+                return friendlyUrlName;
+            //check whether such friendly URL name already exists (and that is not the current affiliate)
+            int i = 2;
+            var tempName = friendlyUrlName;
+            while (true)
+            {
+                var affiliateService = EngineContext.Current.Resolve<IAffiliateService>();
+                var affiliateByFriendlyUrlName = affiliateService.GetAffiliateByFriendlyUrlName(tempName);
+
+                bool reserved = affiliateByFriendlyUrlName != null && affiliateByFriendlyUrlName.Id != affiliate.Id;
+                if (!reserved)
+                    break;
+
+                tempName = string.Format("{0}-{1}", friendlyUrlName, i);
+                i++;
+            }
+            friendlyUrlName = tempName;
+
 
             return friendlyUrlName;
         }
