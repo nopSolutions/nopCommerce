bef134308560d8278f223fc14d685f08f88a7b15
AndreyMaz <none@none>
2015-04-30 08:18:27 +0300

    More source code comments added

--- src/Libraries/Nop.Services/Customers/CustomerRegistrationRequest.cs
+++ src/Libraries/Nop.Services/Customers/CustomerRegistrationRequest.cs
@@ -2,15 +2,45 @@
 
 namespace Nop.Services.Customers
 {
+    /// <summary>
+    /// Customer registration request
+    /// </summary>
     public class CustomerRegistrationRequest
     {
+        /// <summary>
+        /// Customer
+        /// </summary>
         public Customer Customer { get; set; }
+        /// <summary>
+        /// Email
+        /// </summary>
         public string Email { get; set; }
+        /// <summary>
+        /// Username
+        /// </summary>
         public string Username { get; set; }
+        /// <summary>
+        /// Password
+        /// </summary>
         public string Password { get; set; }
+        /// <summary>
+        /// Password format
+        /// </summary>
         public PasswordFormat PasswordFormat { get; set; }
+        /// <summary>
+        /// Is approved
+        /// </summary>
         public bool IsApproved { get; set; }
 
+        /// <summary>
+        /// Ctor
+        /// </summary>
+        /// <param name="customer">Customer</param>
+        /// <param name="email">Email</param>
+        /// <param name="username">Username</param>
+        /// <param name="password">Password</param>
+        /// <param name="passwordFormat">Password fprmat</param>
+        /// <param name="isApproved">Is approved</param>
         public CustomerRegistrationRequest(Customer customer, string email, string username,
             string password, 
             PasswordFormat passwordFormat,
@@ -23,16 +53,5 @@ namespace Nop.Services.Customers
             this.PasswordFormat = passwordFormat;
             this.IsApproved = isApproved;
         }
-
-        //public bool IsValid  
-        //{
-        //    get 
-        //    {
-        //        return (!CommonHelper.AreNullOrEmpty(
-        //                    this.Email,
-        //                    this.Password
-        //                    ));
-        //    }
-        //}
     }
 }
