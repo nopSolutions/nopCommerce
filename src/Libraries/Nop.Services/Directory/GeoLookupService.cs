e10039263f8f230778c46cab10f157b3fc95195a
AndreyMaz <none@none>
2015-05-28 10:11:12 +0300

    Updated GeoLite library to the latest version.

--- src/Libraries/Nop.Services/Directory/GeoLookupService.cs
+++ src/Libraries/Nop.Services/Directory/GeoLookupService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿//This product includes GeoLite2 data created by MaxMind, available from http://www.maxmind.com
+
+using System;
 using MaxMind.GeoIP2;
 using MaxMind.GeoIP2.Exceptions;
 using MaxMind.GeoIP2.Responses;
