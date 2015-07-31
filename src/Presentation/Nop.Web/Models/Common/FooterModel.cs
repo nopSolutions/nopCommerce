2e74f1c762783fbe95dd6df40df1edec1a11c262
AndreyMaz <none@none>
2015-07-31 12:03:09 +0300

    Deletes several unused properties (FooterModel class).

--- src/Presentation/Nop.Web/Models/Common/FooterModel.cs
+++ src/Presentation/Nop.Web/Models/Common/FooterModel.cs
@@ -18,14 +18,11 @@ namespace Nop.Web.Models.Common
         public string GooglePlusLink { get; set; }
         public bool WishlistEnabled { get; set; }
         public bool ShoppingCartEnabled { get; set; }
-        public bool HideAddresses { get; set; }
-        public bool HideOrders { get; set; }
         public bool SitemapEnabled { get; set; }
         public bool NewsEnabled { get; set; }
         public bool BlogEnabled { get; set; }
         public bool CompareProductsEnabled { get; set; }
         public bool ForumEnabled { get; set; }
-        public bool AllowPrivateMessages { get; set; }
         public bool RecentlyViewedProductsEnabled { get; set; }
         public bool RecentlyAddedProductsEnabled { get; set; }
         public bool AllowCustomersToApplyForVendorAccount { get; set; }
