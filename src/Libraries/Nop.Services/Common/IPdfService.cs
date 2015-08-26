7518fafa7fc9bfda9cfe65ce53e7deb37544a87d
AndreyMaz <none@none>
2015-08-26 15:37:17 +0300

    Source code typo fixed

--- src/Libraries/Nop.Services/Common/IPdfService.cs
+++ src/Libraries/Nop.Services/Common/IPdfService.cs
@@ -16,7 +16,7 @@ namespace Nop.Services.Common
         /// </summary>
         /// <param name="order">Order</param>
         /// <param name="languageId">Language identifier; 0 to use a language used when placing an order</param>
-        /// <returns>A path of generates file</returns>
+        /// <returns>A path of generated file</returns>
         string PrintOrderToPdf(Order order, int languageId);
 
         /// <summary>
