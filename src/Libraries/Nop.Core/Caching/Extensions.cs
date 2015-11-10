264a659235bfae0ec3b0a0e6c25eb4331af713a7
AndreyMaz <andrey.maz@gmail.com>
2015-11-10 11:33:08 +0300

    #12577 Significant performance optimization of caching implementation (for high-loaded sites). We should not use "lock" in CachingExtensions ("get" method).

--- src/Libraries/Nop.Core/Caching/Extensions.cs
+++ src/Libraries/Nop.Core/Caching/Extensions.cs
@@ -8,11 +8,6 @@ namespace Nop.Core.Caching
     public static class CacheExtensions
     {
         /// <summary>
-        /// Variable (lock) to support thread-safe
-        /// </summary>
-        private static readonly object _syncObject = new object();
-
-        /// <summary>
         /// Get a cached item. If it's not in the cache yet, then load and cache it
         /// </summary>
         /// <typeparam name="T">Type</typeparam>
@@ -34,20 +29,17 @@ namespace Nop.Core.Caching
         /// <param name="cacheTime">Cache time in minutes (0 - do not cache)</param>
         /// <param name="acquire">Function to load item if it's not in the cache yet</param>
         /// <returns>Cached item</returns>
-        public static T Get<T>(this ICacheManager cacheManager, string key, int cacheTime, Func<T> acquire) 
+        public static T Get<T>(this ICacheManager cacheManager, string key, int cacheTime, Func<T> acquire)
         {
-            lock (_syncObject)
+            if (cacheManager.IsSet(key))
             {
-                if (cacheManager.IsSet(key))
-                {
-                    return cacheManager.Get<T>(key);
-                }
-
-                var result = acquire();
-                if (cacheTime > 0)
-                    cacheManager.Set(key, result, cacheTime);
-                return result;
+                return cacheManager.Get<T>(key);
             }
+
+            var result = acquire();
+            if (cacheTime > 0)
+                cacheManager.Set(key, result, cacheTime);
+            return result;
         }
     }
 }
