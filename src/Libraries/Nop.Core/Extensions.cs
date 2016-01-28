6ee40bf1ac55c26c723f635f61a1c8f9cfddbe3f
skoshelev <sergey.k@nopcommerce.com>
2016-01-28 12:55:21 +0300

    Placed RemoveByPattern method to more appropriate class

--- src/Libraries/Nop.Core/Extensions.cs
+++ src/Libraries/Nop.Core/Extensions.cs
@@ -1,23 +1,10 @@
-﻿
-
-using Nop.Core.Caching;
-using System.Collections.Generic;
-using System.Text.RegularExpressions;
-using System.Linq;
-namespace Nop.Core
+﻿namespace Nop.Core
 {
     public static class Extensions
     {
         public static bool IsNullOrDefault<T>(this T? value) where T : struct
         {
             return default(T).Equals(value.GetValueOrDefault());
-        }
-
-        public static void RemoveByPattern(this ICacheManager obj, string pattern, IEnumerable<string> keys)
-        {
-            var regex = new Regex(pattern, RegexOptions.Singleline | RegexOptions.Compiled | RegexOptions.IgnoreCase);
-            foreach (var key in keys.Where(p => regex.IsMatch(p)))
-                obj.Remove(key);
-        }
+        }        
     }
 }
