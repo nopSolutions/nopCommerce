452d2c02ebf8f5ef9c4a556d6b86a25a6577ad6d
skoshelev <sergey.k@nopcommerce.com>
2016-01-28 11:35:47 +0300

    switch from IsStaticResource method. Placed function  logice from MapPath to  static method of CommonHelper class.

--- src/Libraries/Nop.Core/CommonHelper.cs
+++ src/Libraries/Nop.Core/CommonHelper.cs
@@ -338,5 +338,24 @@ namespace Nop.Core
                 age--;
             return age;
         }
+
+        /// <summary>
+        /// Maps a virtual path to a physical disk path.
+        /// </summary>
+        /// <param name="path">The path to map. E.g. "~/bin"</param>
+        /// <returns>The physical path. E.g. "c:\inetpub\wwwroot\bin"</returns>
+        public static string MapPath(string path)
+        {
+            if (HostingEnvironment.IsHosted)
+            {
+                //hosted
+                return HostingEnvironment.MapPath(path);
+            }
+
+            //not hosted. For example, run in unit tests
+            string baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            path = path.Replace("~/", "").TrimStart('/').Replace('/', '\\');
+            return Path.Combine(baseDirectory, path);
+        }        
     }
 }
