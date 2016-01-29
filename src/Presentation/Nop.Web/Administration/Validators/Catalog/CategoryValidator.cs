baab3c95289955672141dd32b3e9d0f54251d071
Reza Ahmadi <r.odises@gmail.com>
2016-01-29 17:17:40 +0330

    issue #1107 Avoid duplication when entering page sizes values

--- src/Presentation/Nop.Web/Administration/Validators/Catalog/CategoryValidator.cs
+++ src/Presentation/Nop.Web/Administration/Validators/Catalog/CategoryValidator.cs
@@ -1,4 +1,5 @@
-﻿using FluentValidation;
+﻿using System.Linq;
+using FluentValidation;
 using Nop.Admin.Models.Catalog;
 using Nop.Services.Localization;
 using Nop.Web.Framework.Validators;
@@ -10,6 +11,18 @@ namespace Nop.Admin.Validators.Catalog
         public CategoryValidator(ILocalizationService localizationService)
         {
             RuleFor(x => x.Name).NotEmpty().WithMessage(localizationService.GetResource("Admin.Catalog.Categories.Fields.Name.Required"));
+
+            RuleFor(x => x.PageSizeOptions).Must(UniqueOptionValidator).WithMessage("Admin.Catalog.Categories.Fields.PageSizeOptions.ShouldHaveUniqueItems");
+        }
+
+        private bool UniqueOptionValidator(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                return true;
+            }
+            var notValid = input.Split(',').Select(p => p.Trim()).GroupBy(p => p).Any(p => p.Count() > 1);
+            return !notValid;
         }
     }
 }
