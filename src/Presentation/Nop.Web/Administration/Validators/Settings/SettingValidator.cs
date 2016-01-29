4c52fcdd6b711cfd0302563f76e199336b2b71d7
Reza Ahmadi <r.odises@gmail.com>
2016-01-30 00:30:27 +0330

    Page size options validates in advanced settings.

--- src/Presentation/Nop.Web/Administration/Validators/Settings/SettingValidator.cs
+++ src/Presentation/Nop.Web/Administration/Validators/Settings/SettingValidator.cs
@@ -10,6 +10,8 @@ namespace Nop.Admin.Validators.Settings
         public SettingValidator(ILocalizationService localizationService)
         {
             RuleFor(x => x.Name).NotEmpty().WithMessage(localizationService.GetResource("Admin.Configuration.Settings.AllSettings.Fields.Name.Required"));
+
+            RuleFor(x => x.Value).Must(ValidatorUtilities.PageSizeOptionsInAdvancedSettingsValidator).WithMessage(localizationService.GetResource("Admin.Configuration.Settings.AllSettings.Fields.PageSizeOptions.IsInvalid"));
         }
     }
 }
