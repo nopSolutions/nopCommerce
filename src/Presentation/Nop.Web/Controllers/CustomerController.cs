8674e71c3f46161f5bcc35a6fa5a23d240806497
Dmitry Chmir <dmitry.chmir@nopcommerce.com>
2016-02-02 13:05:46 +0300

    #1092 All delete functionality should use "POST" (XSRF validation)

--- src/Presentation/Nop.Web/Controllers/CustomerController.cs
+++ src/Presentation/Nop.Web/Controllers/CustomerController.cs
@@ -1390,6 +1390,9 @@ namespace Nop.Web.Controllers
             return View(model);
         }
 
+        [HttpPost]
+        [PublicAntiForgery]
+        [NopHttpsRequirement(SslRequirement.Yes)]
         public ActionResult RemoveExternalAssociation(int id)
         {
             if (!_workContext.CurrentCustomer.IsRegistered())
@@ -1400,11 +1403,19 @@ namespace Nop.Web.Controllers
                 .FirstOrDefault(x => x.Id == id);
 
             if (ear == null)
-                return RedirectToAction("Info");
+            {
+                return Json(new
+                {
+                    redirect = Url.Action("Info"),
+                });
+            }
 
             _openAuthenticationService.DeletExternalAuthenticationRecord(ear);
 
-            return RedirectToAction("Info");
+            return Json(new
+            {
+                redirect = Url.Action("Info"),
+            });
         }
 
         #endregion
