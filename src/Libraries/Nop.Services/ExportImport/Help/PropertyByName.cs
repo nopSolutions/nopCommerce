37f849bf74760ed1868539c8c02e670658a8575a
skoshelev <sergey.k@nopcommerce.com>
2016-02-04 17:03:08 +0300

    Added helper class to access the property by name

---
+++ src/Libraries/Nop.Services/ExportImport/Help/PropertyByName.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Nop.Services.ExportImport.Help
+{
+    /// <summary>
+    /// A helper class to access the property by name
+    /// </summary>
+    /// <typeparam name="T">Object type</typeparam>
+    public class PropertyByName<T>
+    {
+        /// <summary>
+        /// Ctor
+        /// </summary>
+        /// <param name="propertyName">Property name</param>
+        /// <param name="func">Feature property access</param>
+        public PropertyByName(string propertyName, Func<T, object> func )
+        {
+            PropertyName = propertyName;
+            GetProperty = func;
+
+            PropertyOrderPosition = 1;
+        }
+
+        /// <summary>
+        /// Property order position
+        /// </summary>
+        public int PropertyOrderPosition { get; set; }
+
+        /// <summary>
+        /// Feature property access
+        /// </summary>
+        public Func<T, object> GetProperty { get; private set; }
+
+        /// <summary>
+        /// Property name
+        /// </summary>
+        public string PropertyName { get; private set; }
+
+        public override string ToString()
+        {
+            return PropertyName;
+        }
+    }
+}
