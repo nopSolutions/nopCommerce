8a82f860149f46e99bb92c10ffa8278806dbaed2
Dmitry Chmir <dmitry.chmir@nopcommerce.com>
2016-02-10 09:52:01 +0300

    #1179 Do not use Recaptcha.dll

--- src/Presentation/Nop.Web.Framework/Security/Captcha/GReCaptchaValidator.cs
+++ src/Presentation/Nop.Web.Framework/Security/Captcha/GReCaptchaValidator.cs
@@ -7,38 +7,78 @@ namespace Nop.Web.Framework.Security.Captcha
 {
     public class GReCaptchaValidator
     {
+        private const string RECAPTCHA_VERIFI_URL_VERSION1 = "https://www.google.com/recaptcha/api/verify?privatekey={0}&response={1}&remoteip={2}&challenge={3}";
+        private const string RECAPTCHA_VERIFI_URL_VERSION2 = "https://www.google.com/recaptcha/api/siteverify?secret={0}&response={1}&remoteip={2}";
+
         public string SecretKey { get; set; }
         public string RemoteIp { get; set; }
         public string Response { get; set; }
+        public string Challenge { get; set; }
+
+        private readonly ReCaptchaVersion _version;
+
+        public GReCaptchaValidator(ReCaptchaVersion version = ReCaptchaVersion.Version1)
+        {
+            _version = version;
+        }
 
         public GReCaptchaResponse Validate()
         {
-            var uriBuilder = new UriBuilder("https://www.google.com/recaptcha/api/siteverify")
+            GReCaptchaResponse result = null;
+            var httpClient = new HttpClient();
+            var requestUri = string.Empty;
+
+            switch (_version)
             {
-                Query = string.Format("secret={0}&response={1}&remoteip={2}", SecretKey, Response, RemoteIp)
-            };
+                case ReCaptchaVersion.Version2:
+                    requestUri = string.Format(RECAPTCHA_VERIFI_URL_VERSION2, SecretKey, Response, RemoteIp);
+                    break;
+                default:
+                    requestUri = string.Format(RECAPTCHA_VERIFI_URL_VERSION1, SecretKey, Response, RemoteIp, Challenge);
+                    break;
+            }
+
+            try
+            {
+                var taskResult = httpClient.GetAsync(requestUri);
+                taskResult.Wait();
+                var response = taskResult.Result;
+                response.EnsureSuccessStatusCode();
+                var taskString = response.Content.ReadAsStringAsync();
+                taskString.Wait();
+                result = ParseResponseResult(taskString.Result);
+            }
+            catch
+            {
+                result = new GReCaptchaResponse { IsValid = false };
+                result.ErrorCodes.Add("Unknown error");
+            }
+            finally
+            {
+                httpClient.Dispose();
+            }
+
+            return result;
+        }
+
+        private GReCaptchaResponse ParseResponseResult(string responseString)
+        {
             var result = new GReCaptchaResponse();
-            using (var httpClient = new HttpClient())
+
+            if (_version == ReCaptchaVersion.Version1)
             {
-                try
-                {
-                    var taskResult = httpClient.GetAsync(uriBuilder.Uri);
-                    taskResult.Wait();
-                    var response = taskResult.Result;
-                    response.EnsureSuccessStatusCode();
-                    var taskString = response.Content.ReadAsStringAsync();
-                    taskString.Wait();
-                    var resultObject = JObject.Parse(taskString.Result);
-                    result.IsValid = resultObject.Value<bool>("success");
-                    if (resultObject.Value<JToken>("error-codes") != null &&
+                var resultObject = responseString.Split('\n');
+                result.IsValid = resultObject.Contains("true");
+                if(!result.IsValid)
+                    result.ErrorCodes.AddRange(resultObject.Where(r => !r.Equals("false", StringComparison.InvariantCultureIgnoreCase)));
+            }
+            else if (_version == ReCaptchaVersion.Version2)
+            {
+                var resultObject = JObject.Parse(responseString);
+                result.IsValid = resultObject.Value<bool>("success");
+                if (resultObject.Value<JToken>("error-codes") != null &&
                         resultObject.Value<JToken>("error-codes").Values<string>().Any())
                         result.ErrorCodes = resultObject.Value<JToken>("error-codes").Values<string>().ToList();
-                }
-                catch (Exception ex)
-                {
-                    result.IsValid = false;
-                    result.ErrorCodes.Add("Unknown error");
-                }
             }
 
             return result;
