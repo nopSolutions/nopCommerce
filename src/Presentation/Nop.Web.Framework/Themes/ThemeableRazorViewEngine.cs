e5299e90cae41be3157039361a7ef81a50e89083
Pete Snyder <cybernexus00@yahoo.com>
2016-02-25 12:44:49 -0500

    Added dynamic detection of plugin view locations

--- src/Presentation/Nop.Web.Framework/Themes/ThemeableRazorViewEngine.cs
+++ src/Presentation/Nop.Web.Framework/Themes/ThemeableRazorViewEngine.cs
@@ -1,12 +1,38 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Hosting;
 using System.Web.Mvc;
+using Nop.Core.Plugins;
 
 namespace Nop.Web.Framework.Themes
 {
     public class ThemeableRazorViewEngine : ThemeableVirtualPathProviderViewEngine
     {
+        private const string InstalledPluginsFilePath = "~/App_Data/InstalledPlugins.txt";
+        private const string PluginReplacementString = "%PluginSystemName%";
+        private const string PluginsLocationFormat = "~/Plugins/" + PluginReplacementString + "/Views/{1}/{0}.cshtml";
+
         public ThemeableRazorViewEngine()
         {
+            var installedPluginSystemNames = PluginFileParser.ParseInstalledPluginsFile(HostingEnvironment.MapPath(InstalledPluginsFilePath));
+
+            var customViewLocationFormats = new List<string>()
+                                                {
+                                                    //themes
+                                                    "~/Themes/{2}/Views/{1}/{0}.cshtml",
+                                                    "~/Themes/{2}/Views/Shared/{0}.cshtml",
+
+                                                    //default
+                                                    "~/Views/{1}/{0}.cshtml",
+                                                    "~/Views/Shared/{0}.cshtml",
+
+                                                    //Admin
+                                                    "~/Administration/Views/{1}/{0}.cshtml",
+                                                    "~/Administration/Views/Shared/{0}.cshtml"
+                                                };
+
+            customViewLocationFormats.AddRange(installedPluginSystemNames.Select(systemName => PluginsLocationFormat.Replace(PluginReplacementString, systemName)));
+
             AreaViewLocationFormats = new[]
                                           {
                                               //themes
@@ -40,21 +66,8 @@ namespace Nop.Web.Framework.Themes
                                                     "~/Areas/{2}/Views/{1}/{0}.cshtml",
                                                     "~/Areas/{2}/Views/Shared/{0}.cshtml"
                                                  };
-
-            ViewLocationFormats = new[]
-                                      {
-                                            //themes
-                                            "~/Themes/{2}/Views/{1}/{0}.cshtml", 
-                                            "~/Themes/{2}/Views/Shared/{0}.cshtml",
-
-                                            //default
-                                            "~/Views/{1}/{0}.cshtml", 
-                                            "~/Views/Shared/{0}.cshtml",
-
-                                            //Admin
-                                            "~/Administration/Views/{1}/{0}.cshtml",
-                                            "~/Administration/Views/Shared/{0}.cshtml",
-                                      };
+            
+            ViewLocationFormats = customViewLocationFormats.ToArray();
 
             MasterLocationFormats = new[]
                                         {
@@ -67,20 +80,7 @@ namespace Nop.Web.Framework.Themes
                                             "~/Views/Shared/{0}.cshtml"
                                         };
 
-            PartialViewLocationFormats = new[]
-                                             {
-                                                 //themes
-                                                "~/Themes/{2}/Views/{1}/{0}.cshtml",
-                                                "~/Themes/{2}/Views/Shared/{0}.cshtml",
-
-                                                //default
-                                                "~/Views/{1}/{0}.cshtml", 
-                                                "~/Views/Shared/{0}.cshtml", 
-
-                                                //Admin
-                                                "~/Administration/Views/{1}/{0}.cshtml",
-                                                "~/Administration/Views/Shared/{0}.cshtml",
-                                             };
+            PartialViewLocationFormats = customViewLocationFormats.ToArray();
 
             FileExtensions = new[] { "cshtml" };
         }
@@ -88,6 +88,7 @@ namespace Nop.Web.Framework.Themes
         protected override IView CreatePartialView(ControllerContext controllerContext, string partialPath)
         {
             IEnumerable<string> fileExtensions = base.FileExtensions;
+
             return new RazorView(controllerContext, partialPath, null, false, fileExtensions);
             //return new RazorView(controllerContext, partialPath, layoutPath, runViewStartPages, fileExtensions, base.ViewPageActivator);
         }
