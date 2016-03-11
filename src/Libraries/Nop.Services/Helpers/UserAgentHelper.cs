d291568d77478bab22cb8467a810d3952362b0a5
skoshelev <sergey.k@nopcommerce.com>
2016-03-11 14:46:16 +0300

    #210 Move back to browscap.org to detect search crawlers

--- src/Libraries/Nop.Services/Helpers/UserAgentHelper.cs
+++ src/Libraries/Nop.Services/Helpers/UserAgentHelper.cs
@@ -5,7 +5,6 @@ using System.Web;
 using Nop.Core;
 using Nop.Core.Configuration;
 using Nop.Core.Infrastructure;
-using UserAgentStringLibrary;
 
 namespace Nop.Services.Helpers
 {
@@ -21,28 +20,28 @@ namespace Nop.Services.Helpers
         /// Ctor
         /// </summary>
         /// <param name="config">Config</param>
-        /// <param name="webHelper">Web helper</param>
         /// <param name="httpContext">HTTP context</param>
-        public UserAgentHelper(NopConfig config, IWebHelper webHelper, HttpContextBase httpContext)
+        public UserAgentHelper(NopConfig config, HttpContextBase httpContext)
         {
             this._config = config;
             this._httpContext = httpContext;
         }
 
         [MethodImpl(MethodImplOptions.Synchronized)]
-        protected virtual UasParser GetUasParser()
+        protected virtual BrowscapXmlHelper GetBrowscapXmlHelperr()
         {
-            if (Singleton<UasParser>.Instance == null)
-            {
-                //no database created
-                if (String.IsNullOrEmpty(_config.UserAgentStringsPath))
-                    return null;
+            if (Singleton<BrowscapXmlHelper>.Instance != null)
+                return Singleton<BrowscapXmlHelper>.Instance;
 
-                var filePath = CommonHelper.MapPath(_config.UserAgentStringsPath);
-                var uasParser = new UasParser(filePath);
-                Singleton<UasParser>.Instance = uasParser;
-            }
-            return Singleton<UasParser>.Instance;
+            //no database created
+            if (String.IsNullOrEmpty(_config.UserAgentStringsPath))
+                return null;
+
+            var filePath = CommonHelper.MapPath(_config.UserAgentStringsPath);
+            var bowscapXmlHelper = new BrowscapXmlHelper(filePath);
+            
+            Singleton<BrowscapXmlHelper>.Instance = bowscapXmlHelper;
+            return Singleton<BrowscapXmlHelper>.Instance;
         }
 
         /// <summary>
@@ -56,25 +55,23 @@ namespace Nop.Services.Helpers
 
             //we put required logic in try-catch block
             //more info: http://www.nopcommerce.com/boards/t/17711/unhandled-exception-request-is-not-available-in-this-context.aspx
-            bool result = false;
             try
             {
-                var uasParser = GetUasParser();
+                var bowscapXmlHelperr = GetBrowscapXmlHelperr();
 
                 //we cannot load parser
-                if (uasParser == null)
+                if (bowscapXmlHelperr == null)
                     return false;
 
                 var userAgent = _httpContext.Request.UserAgent;
-                result = uasParser.IsBot(userAgent);
-                //result = context.Request.Browser.Crawler;
+                return bowscapXmlHelperr.IsCrawler(userAgent);
             }
             catch (Exception exc)
             {
                 Debug.WriteLine(exc);
             }
-            return result;
-        }
 
+            return false;
+        }
     }
 }
