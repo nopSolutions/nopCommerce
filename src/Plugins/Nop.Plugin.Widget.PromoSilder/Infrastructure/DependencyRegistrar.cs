ad7f9842ed3391ffc97ad15ce37b8bef6faf63c2
xiong wenhua <xiong.wenhua@afusion.com>
2016-04-05 17:56:59 +0800

    sss

---
+++ src/Plugins/Nop.Plugin.Widget.PromoSilder/Infrastructure/DependencyRegistrar.cs
@@ -0,0 +1,27 @@
+﻿using Nop.Core.Infrastructure.DependencyManagement;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nop.Core.Configuration;
+using Nop.Core.Infrastructure;
+
+namespace Nop.Plugin.Widgets.PromoSilder.Infrastructure
+{
+    public class DependencyRegistrar : IDependencyRegistrar
+    {
+        public int Order
+        {
+            get
+            {
+                return 1;
+            }
+        }
+
+        public void Register(global::Autofac.ContainerBuilder builder, ITypeFinder typeFinder, NopConfig config)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Plugins/Nop.Plugin.Widget.PromoSilder/PromoSilderPlugin.cs
@@ -0,0 +1,57 @@
+﻿using Nop.Core.Plugins;
+using Nop.Services.Cms;
+using System;
+using System.Collections.Generic;
+using System.Web.Routing;
+
+namespace Nop.Plugin.Widgets.PromoSilder
+{
+    public class PromoSilderPlugin : BasePlugin, IWidgetPlugin
+    {
+        public PromoSilderPlugin()
+        {
+
+        }
+
+        public void GetConfigurationRoute(out string actionName, out string controllerName, out RouteValueDictionary routeValues)
+        {
+            //actionName = "PromoSilderWidget";
+            //controllerName = "PromoSilder";
+            //routeValues = new RouteValueDictionary()
+            //{
+            //    { "Namespace", "Nop.Plugin.Widgets.PromoSilder.Controllers" },
+            //    { "area", null }
+            //};
+            throw new NotImplementedException();
+        }
+
+        public void GetDisplayWidgetRoute(string widgetZone, out string actionName, out string controllerName, out RouteValueDictionary routeValues)
+        {
+            //actionName = "PromoSilderWidget";
+            //controllerName = "PromoSilder";
+            //routeValues = new RouteValueDictionary()
+            //{
+            //    { "Namespace", "Nop.Plugin.Widgets.PromoSilder.Controllers" },
+            //    { "area", null },
+            //    { "widgetZone", widgetZone }
+            //};
+            throw new NotImplementedException();
+        }
+
+        public IList<string> GetWidgetZones()
+        {
+            return new List<string>();
+        }
+
+        public override void Install()
+        {
+            base.Install();
+        }
+
+        public override void Uninstall()
+        {
+            base.Uninstall();
+        }
+    }
+
+}
