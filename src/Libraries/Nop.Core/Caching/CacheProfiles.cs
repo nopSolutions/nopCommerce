40d8a8124e0cf197621d7149c842d51927e07bd9
Kerem Demirer <no@no.com>
2016-04-22 02:22:27 +0300

    output cache attribute implementation. ps. logic taken from Orchard Project.

---
+++ src/Libraries/Nop.Core/Caching/CacheProfiles.cs
@@ -0,0 +1,39 @@
+﻿namespace Nop.Core.Caching
+{
+    /// <summary>
+    /// A class that defines constant strings that represent that known cache profiles 
+    /// to define timeout minutes of keys. Use these constants rather than 
+    /// using inline values for cache manager.
+    /// </summary>
+    public partial class CacheProfiles
+    {
+        /// <summary>
+        /// The default profile for the application.
+        /// profile
+        /// </summary>
+        public const int DefaultProfile = 60;
+
+        /// <summary>
+        /// A profile that quickly expires.
+        /// </summary>
+        public const int ShortLivedProfile = 2;
+
+        /// <summary>
+        /// A profile that lives for a longer period of time.
+        /// </summary>
+        public const int LongLivedProfile = 720;
+
+        /// <summary>
+        /// A short lived cache profile for caching customer specific content.
+        /// </summary>
+        public const int CustomerCacheProfile = 1;
+
+        /// <summary>
+        /// A short lived cache profile for caching customer specific content.
+        /// </summary>
+        public const int CustomerLongLivedCacheProfile = 5;
+
+
+
+    }
+}
--- src/Presentation/Nop.Web/Controllers/HomeController.cs
+++ src/Presentation/Nop.Web/Controllers/HomeController.cs
@@ -1,10 +1,14 @@
 ﻿using System.Web.Mvc;
+using Nop.Core.Caching;
+using Nop.Web.Framework.Mvc;
 using Nop.Web.Framework.Security;
+using Nop.Web.Infrastructure.Cache;
 
 namespace Nop.Web.Controllers
 {
     public partial class HomeController : BasePublicController
     {
+        [NopOutputCache(CacheKeyPrefix = ModelCacheEventConsumer.HOMEPAGE_OUTPUT_PATTERN_KEY, VaryByCustomer = true, DurationInMinutes = CacheProfiles.ShortLivedProfile)]
         [NopHttpsRequirement(SslRequirement.No)]
         public ActionResult Index()
         {
