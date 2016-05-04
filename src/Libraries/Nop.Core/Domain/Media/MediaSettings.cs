1ce8e84acd56729aca923fc8f200adbc9d90a33e
skoshelev <sergey.k@nopcommerce.com>
2016-05-04 09:20:33 +0300

    fix typo

--- src/Libraries/Nop.Core/Domain/Media/MediaSettings.cs
+++ src/Libraries/Nop.Core/Domain/Media/MediaSettings.cs
@@ -62,11 +62,11 @@ namespace Nop.Core.Domain.Media
         /// </summary>
         public int MaximumImageSize { get; set; }
         /// <summary>
-        /// Geta or sets a default quality used for image generation
+        /// Gets or sets a default quality used for image generation
         /// </summary>
         public int DefaultImageQuality { get; set; }
         /// <summary>
-        /// Geta or sets a vaue indicating whether single (/content/images/thumbs/) or multiple (/content/images/thumbs/001/ and /content/images/thumbs/002/) directories will used for picture thumbs
+        /// Gets or sets a vaue indicating whether single (/content/images/thumbs/) or multiple (/content/images/thumbs/001/ and /content/images/thumbs/002/) directories will used for picture thumbs
         /// </summary>
         public bool MultipleThumbDirectories { get; set; }
     }
