584c5b5193fd0a339e4562497ec5b3cd7ebe155d
AndreiMaz <andrey.maz@gmail.com>
2016-05-23 14:16:10 +0300

    #34 Got started on menu item selection

--- src/Presentation/Nop.Web.Framework/UI/LayoutExtensions.cs
+++ src/Presentation/Nop.Web.Framework/UI/LayoutExtensions.cs
@@ -332,5 +332,27 @@ namespace Nop.Web.Framework.UI
             var result = includeClassElement ? string.Format("class=\"{0}\"", classes) : classes;
             return MvcHtmlString.Create(result);
         }
+
+
+        /// <summary>
+        /// Specify system name of admin menu item that should be selected (expanded)
+        /// </summary>
+        /// <param name="html">HTML helper</param>
+        /// <param name="systemName">System name</param>
+        public static void SetActiveMenuItemSystemName(this HtmlHelper html, string systemName)
+        {
+            var pageHeadBuilder = EngineContext.Current.Resolve<IPageHeadBuilder>();
+            pageHeadBuilder.SetActiveMenuItemSystemName(systemName);
+        }
+        /// <summary>
+        /// Get system name of admin menu item that should be selected (expanded)
+        /// </summary>
+        /// <param name="html">HTML helper</param>
+        /// <returns>System name</returns>
+        public static string GetActiveMenuItemSystemName(this HtmlHelper html)
+        {
+            var pageHeadBuilder = EngineContext.Current.Resolve<IPageHeadBuilder>();
+            return pageHeadBuilder.GetActiveMenuItemSystemName();
+        }
     }
 }
