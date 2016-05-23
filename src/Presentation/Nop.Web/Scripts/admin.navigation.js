bcea36ec2116ec7d28167be0e5abc516b091758b
mariannk <marianna.k@nopcommerce.com>
2016-05-23 19:28:41 +0300

    #34 Added search by menu

---
+++ src/Presentation/Nop.Web/Scripts/admin.navigation.js
@@ -0,0 +1,43 @@
+﻿var Admin = Admin || {};
+
+Admin.Navigation = (function () {
+    var buildMap = function () {
+        var map = {};
+        $('.sidebar-menu a.menu-item-link').each(function () {
+            var linkTitle = $(this).children('.menu-item-title').text();
+            var href = $(this).attr('href');
+            map[href] = { root: linkTitle, node: null, title: linkTitle, url: href };
+        });
+        return map;
+    };
+
+    var map;
+   
+    var init = function () {
+        map = buildMap();
+    };
+    var events = {};
+    return {
+        enumerate: function (callback) {
+            for (var url in map) {
+                var node = map[url];
+                callback.call(node, node);
+            }
+        },
+        open: function (url) {
+            if (events['open']) {
+                var event = $.Event('open', { url: url });
+                events['open'].fire(event);
+                if (event.isDefaultPrevented())
+                    return;
+            }
+            window.location.href = url;
+        },
+        
+        initOnce: function () {
+            if (!map)
+                init();
+        },
+        init: init
+    };
+})();
---
+++ src/Presentation/Nop.Web/Scripts/admin.search.js
@@ -0,0 +1,98 @@
+﻿var Admin = Admin || {};
+
+Admin.Search = (function () {
+    var itemTemplate = function (data) {
+        var path = function() {
+            if (data.node) {
+                return $('<p/>').text([data.root, data.node].join(" > ")).html();
+            } else {
+                return data.root;
+            }
+        };
+        return '<div id="user-selection">' +
+                            '<h5>' + data.title + '</h5>' + 
+                            //'<p>' + path() + '</p>' +
+                            '</div>';
+    };
+
+    var substringMatcher = function (enumerate) {
+        var byRateAndTitle = function (a, b) {
+            if (a.rate < b.rate)
+                return 1;
+            if (a.rate > b.rate)
+                return -1;
+            if (a.title < b.title)
+                return -1;
+            if (a.title > b.title)
+                return 1;
+            return 0;
+        };
+
+        return function findMatches(q, cb) {
+
+            var matches = [];
+
+            substrRegex = new RegExp(q, 'i');
+
+            enumerate(function (item) {
+
+                var rate = 0;
+                var missKeyword = false;
+
+                if (substrRegex.test(item.title)) {
+                    rate += 10;
+                }
+                else if (item.node && substrRegex.test(item.node)) {
+                    rate += 5;
+                }
+                else if (substrRegex.test(item.root)) {
+                    rate += 1;
+                } else {
+                    missKeyword = true;
+                }
+
+                item.rate = rate;
+
+                if (!missKeyword) {
+                    matches.push(item);
+                }
+            });
+
+
+            matches.sort(byRateAndTitle);
+            cb(matches);
+        };
+    };
+
+    return {
+        init: function () {
+            Admin.Navigation.initOnce();
+
+            var $input = $('.admin-search-box');
+            $input.blur(function (e) { e.preventDefault(); e.stopPropagation(); });
+            $input.typeahead({ minLength: 2, highlight: true, hint: false },
+            {
+                name: 'pages',
+                displayKey: 'name',
+                templates: {
+                    empty: [
+                            '<div class="empty-message">',
+                              'NOT FOUND',
+                            '</div>'
+                    ].join('\n'),
+                    suggestion: itemTemplate
+                },
+                source: substringMatcher(Admin.Navigation.enumerate)
+            });
+
+            var navigateTo = function (item) {
+                $input.typeahead('val', '');
+                Admin.Navigation.open(item.url);
+            };
+
+            $input.on('typeahead:selected', function (e, item) {
+                navigateTo(item);
+            });
+        }
+    };
+})();
