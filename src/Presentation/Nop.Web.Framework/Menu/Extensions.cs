5763dd0d2ad8e3e7885e1aee4ad34493356ed0cd
AndreiMaz <andrey.maz@gmail.com>
2016-06-08 10:28:23 +0300

    Minor refactoring. Ensure that no exception is thrown

--- src/Presentation/Nop.Web.Framework/Menu/Extensions.cs
+++ src/Presentation/Nop.Web.Framework/Menu/Extensions.cs
@@ -19,7 +19,7 @@ namespace Nop.Web.Framework.Menu
             if (String.IsNullOrWhiteSpace(systemName))
                 return false;
 
-            if (node.SystemName.Equals(systemName, StringComparison.InvariantCultureIgnoreCase))
+            if (systemName.Equals(node.SystemName, StringComparison.InvariantCultureIgnoreCase))
                 return true;
 
             return node.ChildNodes.Any(cn => ContainsSystemName(cn, systemName));
