45e4c7c5d4bb3af8a3ed1d94fa23b2a226be5535
mariannk <marianna.k@nopcommerce.com>
2016-06-15 09:59:02 +0300

    #95 Scroll to top in admin area

--- src/Presentation/Nop.Web/Administration/Scripts/admin.common.js
+++ src/Presentation/Nop.Web/Administration/Scripts/admin.common.js
@@ -120,6 +120,30 @@ function saveDashboardPreferences(url, name, value) {
     });
 };
 
+//scroll to top
+(function ($) {
+    $.fn.backTop = function () {
+        var backBtn = this;
+
+        var position = 1000;
+        var speed = 900;
+
+        $(document).scroll(function () {
+            var pos = $(window).scrollTop();
+
+            if (pos >= position) {
+                backBtn.fadeIn(speed);
+            } else {
+                backBtn.fadeOut(speed);
+            }
+        });
+
+        backBtn.click(function () {
+            $("html, body").animate({ scrollTop: 0 }, 900);
+        });
+    }
+}(jQuery));
+
 // Ajax activity indicator bound to ajax start/stop document events
 $(document).ajaxStart(function () {
     $('#ajaxBusy').show();
