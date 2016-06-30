abdeadc5e081fabc583312e4a671acd2255c8982
Dmitry Chmir <dmitry.chmir@nopcommerce.com>
2016-06-30 14:32:08 +0300

    Fixed comment in the product review entity

--- src/Libraries/Nop.Core/Domain/Catalog/ProductReview.cs
+++ src/Libraries/Nop.Core/Domain/Catalog/ProductReview.cs
@@ -63,7 +63,7 @@ namespace Nop.Core.Domain.Catalog
         public DateTime CreatedOnUtc { get; set; }
 
         /// <summary>
-        /// Gets or sets the product
+        /// Gets or sets the customer
         /// </summary>
         public virtual Customer Customer { get; set; }
 
