e136e39d0edaf45558902b65f93995b65dc8be17
Ilya Verbitskiy <iverbitskiy@gmail.com>
2016-07-29 12:15:17 +0200

    Email validator does not accept 'cs_test@cs_test.com' as a valid email address. Use Microsoft email validator to validate emails. https://msdn.microsoft.com/en-us/library/01escwtf(v=vs.110).aspx

--- src/Libraries/Nop.Core/CommonHelper.cs
+++ src/Libraries/Nop.Core/CommonHelper.cs
@@ -47,12 +47,8 @@ namespace Nop.Core
         /// <returns>true if the string is a valid e-mail address and false if it's not</returns>
         public static bool IsValidEmail(string email)
         {
-            if (String.IsNullOrEmpty(email))
-                return false;
-
-            email = email.Trim();
-            var result = Regex.IsMatch(email, "^(?:[\\w\\!\\#\\$\\%\\&\\'\\*\\+\\-\\/\\=\\?\\^\\`\\{\\|\\}\\~]+\\.)*[\\w\\!\\#\\$\\%\\&\\'\\*\\+\\-\\/\\=\\?\\^\\`\\{\\|\\}\\~]+@(?:(?:(?:[a-zA-Z0-9](?:[a-zA-Z0-9\\-](?!\\.)){0,61}[a-zA-Z0-9]?\\.)+[a-zA-Z0-9](?:[a-zA-Z0-9\\-](?!$)){0,61}[a-zA-Z0-9]?)|(?:\\[(?:(?:[01]?\\d{1,2}|2[0-4]\\d|25[0-5])\\.){3}(?:[01]?\\d{1,2}|2[0-4]\\d|25[0-5])\\]))$", RegexOptions.IgnoreCase);
-            return result;
+            var validator = new Emailvalidator();
+            return validator.IsValidEmail(email);
         }
 
         /// <summary>
@@ -372,6 +368,64 @@ namespace Nop.Core
             string baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
             path = path.Replace("~/", "").TrimStart('/').Replace('/', '\\');
             return Path.Combine(baseDirectory, path);
-        }        
+        }
+
+        // Email Validator's code has been taken from Microsoft examples
+        // https://msdn.microsoft.com/en-us/library/01escwtf(v=vs.110).aspx
+        private class Emailvalidator
+        {
+            bool invalid = false;
+
+            public bool IsValidEmail(string strIn)
+            {
+                invalid = false;
+                if (String.IsNullOrEmpty(strIn))
+                    return false;
+
+                // Use IdnMapping class to convert Unicode domain names.
+                try
+                {
+                    strIn = Regex.Replace(strIn, @"(@)(.+)$", this.DomainMapper,
+                                          RegexOptions.None, TimeSpan.FromMilliseconds(200));
+                }
+                catch (RegexMatchTimeoutException)
+                {
+                    return false;
+                }
+
+                if (invalid)
+                    return false;
+
+                // Return true if strIn is in valid e-mail format.
+                try
+                {
+                    return Regex.IsMatch(strIn,
+                          @"^(?("")("".+?(?<!\\)""@)|(([0-9a-z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-z])@))" +
+                          @"(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-z][-\w]*[0-9a-z]*\.)+[a-z0-9][\-a-z0-9]{0,22}[a-z0-9]))$",
+                          RegexOptions.IgnoreCase, TimeSpan.FromMilliseconds(250));
+                }
+                catch (RegexMatchTimeoutException)
+                {
+                    return false;
+                }
+            }
+
+            private string DomainMapper(Match match)
+            {
+                // IdnMapping class with default property values.
+                IdnMapping idn = new IdnMapping();
+
+                string domainName = match.Groups[2].Value;
+                try
+                {
+                    domainName = idn.GetAscii(domainName);
+                }
+                catch (ArgumentException)
+                {
+                    invalid = true;
+                }
+                return match.Groups[1].Value + domainName;
+            }
+        }
     }
 }
