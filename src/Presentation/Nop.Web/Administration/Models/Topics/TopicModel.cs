5415244aa7eaa125cdc9d1b9e43b9be5503cc6ef
RomanovM <maxim.r@nopcommerce.com>
2017-03-06 10:56:24 +0300

    #2179 Topic SystemName isn't required (it can be null)

--- src/Presentation/Nop.Web/Administration/Models/Topics/TopicModel.cs
+++ src/Presentation/Nop.Web/Administration/Models/Topics/TopicModel.cs
@@ -2,14 +2,12 @@
 using System.ComponentModel.DataAnnotations;
 using System.Web.Mvc;
 using FluentValidation.Attributes;
-using Nop.Admin.Validators.Topics;
 using Nop.Web.Framework;
 using Nop.Web.Framework.Localization;
 using Nop.Web.Framework.Mvc;
 
 namespace Nop.Admin.Models.Topics
 {
-    [Validator(typeof(TopicValidator))]
     public partial class TopicModel : BaseNopEntityModel, ILocalizedModel<TopicLocalizedModel>
     {
         public TopicModel()
