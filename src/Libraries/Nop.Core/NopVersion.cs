342cfcdd952ba2980b03aec8d5c8b17b1da35844
AndreiMaz <andrey.maz@gmail.com>
2017-03-19 09:46:07 +0300

    Set nopCommerce version number to 4.00 (the next one)

--- src/Libraries/Nop.Core/NopVersion.cs
+++ src/Libraries/Nop.Core/NopVersion.cs
@@ -10,7 +10,7 @@ namespace Nop.Core
         {
             get
             {
-                return "3.90";
+                return "4.00";
             }
         }
     }
--- src/Presentation/Nop.Web.Framework/Properties/AssemblyInfo.cs
+++ src/Presentation/Nop.Web.Framework/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.9.0.0")]
-[assembly: AssemblyFileVersion("3.9.0.0")]
+[assembly: AssemblyVersion("4.0.0.0")]
+[assembly: AssemblyFileVersion("4.0.0.0")]
