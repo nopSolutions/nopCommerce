2717c9091c7e795b4a5bfefafa1151b84007b43d
RomanovM <maxim.r@nopcommerce.com>
2017-04-25 10:04:53 +0300

    Add XmlDownloadResult

--- src/Presentation/Nop.Web.Framework/Mvc/XmlDownloadResult.cs
+++ src/Presentation/Nop.Web.Framework/Mvc/XmlDownloadResult.cs
@@ -1,45 +1,87 @@
-﻿#if NET451
+﻿using System;
 using System.Text;
-using System.Web.Mvc;
 using System.Xml;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Internal;
+using Nop.Core.Infrastructure;
 
 namespace Nop.Web.Framework.Mvc
 {
+    /// <summary>
+    /// Represents a XML download result implementation of the action result
+    /// </summary>
     public class XmlDownloadResult : ActionResult
     {
+        private const string DefaultContentType = "text/xml; charset=utf-8";
+
+        #region Properties
+
+        /// <summary>
+        /// Gets or sets the name of download file
+        /// </summary>
+        public string FileDownloadName { get; set; }
+
+        /// <summary>
+        /// Gets or sets XML of the file
+        /// </summary>
+        public string Xml { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Content-Type header for the response
+        /// </summary>
+        public string ContentType { get; set; }
+
+        #endregion
+
+        #region Ctor
+
         public XmlDownloadResult(string xml, string fileDownloadName)
         {
             Xml = xml;
             FileDownloadName = fileDownloadName;
         }
 
-        public string FileDownloadName
-        {
-            get;
-            set;
-        }
+        #endregion
 
-        public string Xml
-        {
-            get;
-            set;
-        }
+        #region Methods
 
-        public override void ExecuteResult(ControllerContext context)
+        /// <summary>
+        /// Executes the result operation of the action method synchronously. This method is called by MVC to process the result of an action method.
+        /// </summary>
+        /// <param name="context">Action context</param>
+        public override void ExecuteResult(ActionContext context)
         {
+            if (context == null)
+                throw new ArgumentNullException(nameof(context));
+
+            var response = context.HttpContext.Response;
+
+            //create XML document from passed XML
             var document = new XmlDocument();
-            document.LoadXml(Xml);
-            var decl = document.FirstChild as XmlDeclaration;
-            if (decl != null)
+            document.LoadXml(this.Xml);
+            if (document.FirstChild is XmlDeclaration declaration)
+                declaration.Encoding = "utf-8";
+
+            //get content type and encoding
+            ResponseContentTypeHelper.ResolveContentTypeAndEncoding(ContentType, response.ContentType, DefaultContentType,
+                out string resolvedContentType, out Encoding resolvedContentTypeEncoding);
+
+            //set response details
+            response.ContentType = resolvedContentType;
+            response.Headers.Add("content-disposition", string.Format("attachment; filename={0}", FileDownloadName));
+            response.ContentLength = resolvedContentTypeEncoding.GetByteCount(document.InnerXml);
+
+            //try get IHttpResponseStreamWriterFactory
+            var httpResponseStreamWriterFactory = EngineContext.Current.Resolve<IHttpResponseStreamWriterFactory>();
+
+            //write XML to response
+            using (var textWriter = httpResponseStreamWriterFactory.CreateWriter(response.Body, resolvedContentTypeEncoding))
             {
-                decl.Encoding = "utf-8";
+                document.Save(textWriter);
+                textWriter.Flush();
             }
-            context.HttpContext.Response.Charset = "utf-8";
-            context.HttpContext.Response.ContentType = "text/xml";
-            context.HttpContext.Response.AddHeader("content-disposition", string.Format("attachment; filename={0}", FileDownloadName));
-            context.HttpContext.Response.BinaryWrite(Encoding.UTF8.GetBytes(document.InnerXml));
-            context.HttpContext.Response.End();
         }
+
+        #endregion
     }
 }
-#endif
