1fa8c48be5281b82fba0235cd4f85ff300c379f7
AndreiMaz <andrey.maz@gmail.com>
2017-05-11 13:57:11 +0300

    Widget component (not finished)

---
+++ src/Presentation/Nop.Web/Components/WidgetsByZone.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Nop.Web.Factories;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Nop.Web.Components
+{
+    public class WidgetsByZoneViewComponent : ViewComponent
+    {
+        private readonly IWidgetModelFactory _widgetModelFactory;
+
+        public WidgetsByZoneViewComponent(IWidgetModelFactory widgetModelFactory)
+        {
+            this._widgetModelFactory = widgetModelFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(string widgetZone, object additionalData = null)
+        {
+            var model = _widgetModelFactory.GetRenderWidgetModels(widgetZone, additionalData);
+
+            //no data?
+            if (!model.Any())
+                return Content("");
+
+            return View(model);
+        }
+    }
+}
