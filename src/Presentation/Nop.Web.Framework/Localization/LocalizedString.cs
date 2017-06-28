9ba827babd80613c65aca8d147505688e7f63d5c
AndreiMaz <andrey.maz@gmail.com>
2017-06-28 16:50:29 +0300

    Refactoring of view localization (do not encode)

--- src/Presentation/Nop.Web.Framework/Localization/LocalizedString.cs
+++ src/Presentation/Nop.Web.Framework/Localization/LocalizedString.cs
@@ -1,80 +1,20 @@
 ﻿using System;
+using Microsoft.AspNetCore.Html;
 
 namespace Nop.Web.Framework.Localization
 {
-    public class LocalizedString : MarshalByRefObject
+    public class LocalizedString : HtmlString
     {
         private readonly string _localized;
-        private readonly string _scope;
-        private readonly string _textHint;
-        private readonly object[] _args;
 
-        public LocalizedString(string localized)
+        public LocalizedString(string localized): base (localized)
         {
             _localized = localized;
         }
-
-        public LocalizedString(string localized, string scope, string textHint, object[] args)
-        {
-            _localized = localized;
-            _scope = scope;
-            _textHint = textHint;
-            _args = args;
-        }
-
-        public static LocalizedString TextOrDefault(string text, LocalizedString defaultValue)
-        {
-            if (string.IsNullOrEmpty(text))
-                return defaultValue;
-            return new LocalizedString(text);
-        }
-
-        public string Scope
-        {
-            get { return _scope; }
-        }
-
-        public string TextHint
-        {
-            get { return _textHint; }
-        }
-
-        public object[] Args
-        {
-            get { return _args; }
-        }
-
+        
         public string Text
         {
             get { return _localized; }
         }
-
-        public override string ToString()
-        {
-            return _localized;
-        }
-
-        public string ToHtmlString()
-        {
-            return _localized;
-        }
-
-        public override int GetHashCode()
-        {
-            var hashCode = 0;
-            if (_localized != null)
-                hashCode ^= _localized.GetHashCode();
-            return hashCode;
-        }
-
-        public override bool Equals(object obj)
-        {
-            if (obj == null || obj.GetType() != GetType())
-                return false;
-
-            var that = (LocalizedString)obj;
-            return string.Equals(_localized, that._localized);
-        }
-
     }
 }
