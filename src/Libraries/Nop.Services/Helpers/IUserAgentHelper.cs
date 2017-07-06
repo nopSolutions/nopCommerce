16cbd8ee4b9130b5bf51627d60fb8330a6d36d3e
AndreiMaz <andrey.maz@gmail.com>
2017-07-06 13:44:10 +0300

    > New implementation to detect mobile devices (server-side) > Source code typo fixed

--- src/Libraries/Nop.Services/Helpers/IUserAgentHelper.cs
+++ src/Libraries/Nop.Services/Helpers/IUserAgentHelper.cs
@@ -13,6 +13,12 @@ namespace Nop.Services.Helpers
         bool IsSearchEngine();
 
         /// <summary>
+        /// Get a value indicating whether the request is made by mobile device
+        /// </summary>
+        /// <returns></returns>
+        bool IsMobileDevice();
+
+        /// <summary>
         /// Get a value indicating whether the request is made by IE8 browser
         /// </summary>
         /// <returns></returns>
