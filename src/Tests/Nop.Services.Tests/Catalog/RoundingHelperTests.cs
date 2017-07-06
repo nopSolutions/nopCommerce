2e885eb2d07faad036712f0b27b65c9ee3ccaf6a
AndreiMaz <andrey.maz@gmail.com>
2017-07-06 19:48:34 +0300

    Rounding unit tests

--- src/Tests/Nop.Services.Tests/Catalog/RoundingHelperTests.cs
+++ src/Tests/Nop.Services.Tests/Catalog/RoundingHelperTests.cs
@@ -1,48 +1,13 @@
-﻿
-#if NET451
-using Nop.Core;
-using Nop.Core.Domain.Directory;
-using Nop.Core.Infrastructure;
-using Nop.Core.Infrastructure.DependencyManagement;
+﻿using Nop.Core.Domain.Directory;
 using Nop.Services.Catalog;
 using Nop.Tests;
 using NUnit.Framework;
-using Rhino.Mocks;
 
 namespace Nop.Services.Tests.Catalog
 {
     [TestFixture]
     public class RoundingHelperTests : ServiceTest
     {
-        private IWorkContext _workContext;
-
-        [SetUp]
-        public new void SetUp()
-        {
-            _workContext = MockRepository.GenerateMock<IWorkContext>();
-            _workContext.Expect(w => w.WorkingCurrency).Return(new Currency { RoundingType = RoundingType.Rounding001 });
-            
-            var nopEngine = MockRepository.GenerateMock<NopEngine>();
-            var containe = MockRepository.GenerateMock<IContainer>();
-            var containerManager = MockRepository.GenerateMock<ContainerManager>(containe);
-            nopEngine.Expect(x => x.ContainerManager).Return(containerManager);
-            containerManager.Expect(x => x.Resolve<IWorkContext>()).Return(_workContext);
-            EngineContext.Replace(nopEngine);
-        }
-
-        [OneTimeTearDown]
-        public void TearDown()
-        {
-            EngineContext.Replace(null);
-        }
-
-        [TestCase(12.366, 12.37)]
-        [TestCase(12.363, 12.36)]
-        public void can_round_price(decimal price, decimal roundedValue)
-        {
-            RoundingHelper.RoundPrice(price).ShouldEqual(roundedValue);
-        }
-
         [TestCase(12.366, 12.37, RoundingType.Rounding001)]
         [TestCase(12.363, 12.36, RoundingType.Rounding001)]
         [TestCase(12.34, 12.35, RoundingType.Rounding005Up)]
@@ -67,4 +32,3 @@ namespace Nop.Services.Tests.Catalog
         }
     }
 }
-#endif
