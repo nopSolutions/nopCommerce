ecd8f74ce01256017a960b978bca7d4906a5ef07
AndreiMaz <andrey.maz@gmail.com>
2017-07-06 19:21:51 +0300

    Updated MemoryCacheManager unit tests

--- src/Tests/Nop.Core.Tests/Caching/MemoryCacheManagerTests.cs
+++ src/Tests/Nop.Core.Tests/Caching/MemoryCacheManagerTests.cs
@@ -1,4 +1,5 @@
-﻿using Nop.Core.Caching;
+﻿using Microsoft.Extensions.Caching.Memory;
+using Nop.Core.Caching;
 using Nop.Tests;
 using NUnit.Framework;
 
@@ -7,11 +8,10 @@ namespace Nop.Core.Tests.Caching
     [TestFixture]
     public class MemoryCacheManagerTests
     {
-        #if NET451
         [Test]
         public void Can_set_and_get_object_from_cache()
         {
-            var cacheManager = new MemoryCacheManager();
+            var cacheManager = new MemoryCacheManager(new MemoryCache(new MemoryCacheOptions()));
             cacheManager.Set("some_key_1", 3, int.MaxValue);
 
             cacheManager.Get<int>("some_key_1").ShouldEqual(3);
@@ -20,7 +20,7 @@ namespace Nop.Core.Tests.Caching
         [Test]
         public void Can_validate_whetherobject_is_cached()
         {
-            var cacheManager = new MemoryCacheManager();
+            var cacheManager = new MemoryCacheManager(new MemoryCache(new MemoryCacheOptions()));
             cacheManager.Set("some_key_1", 3, int.MaxValue);
             cacheManager.Set("some_key_2", 4, int.MaxValue);
 
@@ -31,13 +31,12 @@ namespace Nop.Core.Tests.Caching
         [Test]
         public void Can_clear_cache()
         {
-            var cacheManager = new MemoryCacheManager();
+            var cacheManager = new MemoryCacheManager(new MemoryCache(new MemoryCacheOptions()));
             cacheManager.Set("some_key_1", 3, int.MaxValue);
 
             cacheManager.Clear();
 
             cacheManager.IsSet("some_key_1").ShouldEqual(false);
         }
-        #endif
     }
 }
