3acf96da8c5af0350753fa27731d81bddf41c217
mariannk <m.koroleva@nopcommerce.com>
2017-08-23 17:14:26 +0100

    Added support for "asp-postfix" attribute for <label>

---
+++ src/Presentation/Nop.Web.Framework/TagHelpers/Public/LabelTagHelper.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Nop.Web.Framework.TagHelpers.Public
+{
+    [HtmlTargetElement("label", Attributes = ForAttributeName)]
+    public class LabelTagHelper : Microsoft.AspNetCore.Mvc.TagHelpers.LabelTagHelper
+    {
+        private const string ForAttributeName = "asp-for";
+        private const string PostfixAttributeName = "asp-postfix";
+
+        /// <summary>
+        /// Indicates whether the input is disabled
+        /// </summary>
+        [HtmlAttributeName(PostfixAttributeName)]
+        public string Postfix { get; set; }
+
+        public LabelTagHelper(IHtmlGenerator generator) : base(generator)
+        {
+        }
+
+        public override Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            output.Content.Append(Postfix);
+
+            return base.ProcessAsync(context, output);
+        }
+    }
+}
