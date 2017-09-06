21aa362ee0b1488d86c6867776fba139532b02db
AndreiMaz <andrey.maz@gmail.com>
2017-09-06 13:32:56 +0300

    "RenderViewComponentToString" method now supports arguments (for a view component)

--- src/Presentation/Nop.Web.Framework/Controllers/BaseController.cs
+++ src/Presentation/Nop.Web.Framework/Controllers/BaseController.cs
@@ -41,8 +41,9 @@ namespace Nop.Web.Framework.Controllers
         /// Render component to string
         /// </summary>
         /// <param name="componentName">Component name</param>
+        /// <param name="arguments">Arguments</param>
         /// <returns>Result</returns>
-        protected virtual string RenderViewComponentToString(string componentName)
+        protected virtual string RenderViewComponentToString(string componentName, object arguments = null)
         {
             //original implementation: https://github.com/aspnet/Mvc/blob/dev/src/Microsoft.AspNetCore.Mvc.ViewFeatures/Internal/ViewComponentResultExecutor.cs
             //we customized it to allow running from controllers
@@ -57,7 +58,7 @@ namespace Nop.Web.Framework.Controllers
 
             var context = actionContextAccessor.ActionContext;
 
-            var viewComponentResult = ViewComponent(componentName);
+            var viewComponentResult = ViewComponent(componentName, arguments);
 
             var viewData = this.ViewData;
             if (viewData == null)
