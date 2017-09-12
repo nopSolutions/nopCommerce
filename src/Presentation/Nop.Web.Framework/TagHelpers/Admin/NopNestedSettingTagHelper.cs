08f89ae7b6c2b993d8b5c4ad80542d019a83b3f5
mariannk <m.koroleva@nopcommerce.com>
2017-09-12 20:22:31 +0100

    Displaying nested settings

---
+++ src/Presentation/Nop.Web.Framework/TagHelpers/Admin/NopNestedSettingTagHelper.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Nop.Core;
+using Nop.Web.Framework.Extensions;
+
+namespace Nop.Web.Framework.TagHelpers.Admin
+{
+    [HtmlTargetElement("nop-nested-setting", Attributes = ForAttributeName)]
+    public class NopNestedSettingTagHelper : TagHelper
+    {
+        private const string ForAttributeName = "asp-for";
+
+        /// <summary>
+        /// HtmlGenerator
+        /// </summary>
+        protected IHtmlGenerator Generator { get; set; }
+
+        /// <summary>
+        /// An expression to be evaluated against the current model
+        /// </summary>
+        [HtmlAttributeName(ForAttributeName)]
+        public ModelExpression For { get; set; }
+
+        /// <summary>
+        /// ViewContext
+        /// </summary>
+        [HtmlAttributeNotBound]
+        [ViewContext]
+        public ViewContext ViewContext { get; set; }
+
+        public NopNestedSettingTagHelper(IHtmlGenerator generator)
+        {
+            Generator = generator;
+        }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+
+            if (output == null)
+            {
+                throw new ArgumentNullException(nameof(output));
+            }
+
+            var parentSettingName = For.Name;
+
+            var random = CommonHelper.GenerateRandomInteger();
+            var nestedSettingId = $"nestedSetting{random}";
+            var parentSettingId = $"parentSetting{random}";
+
+            //tag details
+            output.TagName = "div";
+            output.TagMode = TagMode.StartTagAndEndTag;
+            output.Attributes.Add("class", "nested-setting");
+            output.Attributes.Add("id", nestedSettingId);
+
+            //script
+            var script = new TagBuilder("script");
+            script.InnerHtml.AppendHtml("$(document).ready(function () {" +
+                                            $"$('input[name=\"{parentSettingName}\"]').closest('.form-group').addClass('parent-setting').attr('id', '{parentSettingId}').css('height', ($('input[name=\"{parentSettingName}\"]').closest('.form-group').height() + 5) + 'px');" +
+
+                                            $"for(i = 0; i < $('#{nestedSettingId} .form-group').length-1; i++){{$($('#{nestedSettingId} .form-group')[i]).css('height', ($($('#{nestedSettingId} .form-group')[i]).height() + 5) + 'px');}}" +
+
+                                            $"function toggleNestedSetting() {{if ($('input[name=\"{parentSettingName}\"]').is(':checked')) {{$('#{parentSettingId}').addClass('opened')}} else {{$('#{parentSettingId}').removeClass('opened')}}}}" +
+                                            $"$('input[name=\"{parentSettingName}\"]').click(toggleNestedSetting);" +
+                                            "toggleNestedSetting();" +
+                                        "});");
+            output.PreContent.SetHtmlContent(script.RenderHtmlContent());
+        }
+
+        public override int Order => 100;
+    }
+}
