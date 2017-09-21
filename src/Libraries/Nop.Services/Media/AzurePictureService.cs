a3ebc7c05c752a860e91efa8581b878234f6af43
RomanovM <maxim.r@nopcommerce.com>
2017-09-21 14:51:18 +0300

    #2317 Add async methods to Azure blob container

--- src/Libraries/Nop.Services/Media/AzurePictureService.cs
+++ src/Libraries/Nop.Services/Media/AzurePictureService.cs
@@ -1,5 +1,6 @@
 using System;
-using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Blob;
@@ -22,9 +23,7 @@ namespace Nop.Services.Media
     {
         #region Fields
         
-        private static CloudStorageAccount _storageAccount = null;
-        private static CloudBlobClient blobClient = null;
-        private static CloudBlobContainer container_thumb = null;
+        private static CloudBlobContainer _container = null;
 
         private readonly MediaSettings _mediaSettings;
         private readonly NopConfig _config;
@@ -58,44 +57,48 @@ namespace Nop.Services.Media
             this._mediaSettings = mediaSettings;
             this._config = config;
 
-            if (String.IsNullOrEmpty(_config.AzureBlobStorageConnectionString))
+            if (string.IsNullOrEmpty(_config.AzureBlobStorageConnectionString))
                 throw new Exception("Azure connection string for BLOB is not specified");
-            if (String.IsNullOrEmpty(_config.AzureBlobStorageContainerName))
+
+            if (string.IsNullOrEmpty(_config.AzureBlobStorageContainerName))
                 throw new Exception("Azure container name for BLOB is not specified");
-            if (String.IsNullOrEmpty(_config.AzureBlobStorageEndPoint))
+
+            if (string.IsNullOrEmpty(_config.AzureBlobStorageEndPoint))
                 throw new Exception("Azure end point for BLOB is not specified");
 
-            _storageAccount = CloudStorageAccount.Parse(_config.AzureBlobStorageConnectionString);
-            if (_storageAccount == null)
-                throw new Exception("Azure connection string for BLOB is not wrong");
-
-            //should we do it for each HTTP request?
-            blobClient = _storageAccount.CreateCloudBlobClient();
-            BlobContainerPermissions containerPermissions = new BlobContainerPermissions();
-            containerPermissions.PublicAccess = BlobContainerPublicAccessType.Blob;
-            //container.SetPermissions(containerPermissions);
-            container_thumb = blobClient.GetContainerReference(_config.AzureBlobStorageContainerName);
-            container_thumb.CreateIfNotExists();
-            container_thumb.SetPermissions(containerPermissions);
+            CreateCloudBlobContainer();
         }
 
         #endregion
 
         #region Utilities
         
+        protected virtual async void CreateCloudBlobContainer()
+        {
+            var storageAccount = CloudStorageAccount.Parse(_config.AzureBlobStorageConnectionString);
+            if (storageAccount == null)
+                throw new Exception("Azure connection string for BLOB is not wrong");
+
+            //should we do it for each HTTP request?
+            var blobClient = storageAccount.CreateCloudBlobClient();
+
+            //GetContainerReference doesn't need to be async since it doesn't contact the server yet
+            _container = blobClient.GetContainerReference(_config.AzureBlobStorageContainerName);
+
+            await _container.CreateIfNotExistsAsync();
+            await _container.SetPermissionsAsync(new BlobContainerPermissions
+            {
+                PublicAccess = BlobContainerPublicAccessType.Blob
+            });
+        }
+
         /// <summary>
         /// Delete picture thumbs
         /// </summary>
         /// <param name="picture">Picture</param>
-        protected override void DeletePictureThumbs(Picture picture)
+        protected override async void DeletePictureThumbs(Picture picture)
         {
-            string filter = $"{picture.Id.ToString("0000000")}";
-            var files = container_thumb.ListBlobs(prefix: filter, useFlatBlobListing: false);
-            foreach (var ff in files)
-            {
-                CloudBlockBlob blockBlob = (CloudBlockBlob)ff;
-                blockBlob.Delete();
-            }
+            await DeletePictureThumbsAsync(picture);
         }
 
         /// <summary>
@@ -105,8 +108,7 @@ namespace Nop.Services.Media
         /// <returns>Local picture thumb path</returns>
         protected override string GetThumbLocalPath(string thumbFileName)
         {
-            var thumbFilePath = _config.AzureBlobStorageEndPoint + _config.AzureBlobStorageContainerName + "/" + thumbFileName;
-            return thumbFilePath;
+            return $"{_config.AzureBlobStorageEndPoint}{_config.AzureBlobStorageContainerName}/{thumbFileName}";
         }
 
         /// <summary>
@@ -117,10 +119,7 @@ namespace Nop.Services.Media
         /// <returns>Local picture thumb path</returns>
         protected override string GetThumbUrl(string thumbFileName, string storeLocation = null)
         {
-            var url = _config.AzureBlobStorageEndPoint + _config.AzureBlobStorageContainerName + "/";
-
-            url = url + thumbFileName;
-            return url;
+            return $"{_config.AzureBlobStorageEndPoint}{_config.AzureBlobStorageContainerName}/{thumbFileName}";
         }
 
         /// <summary>
@@ -131,16 +130,7 @@ namespace Nop.Services.Media
         /// <returns>Result</returns>
         protected override bool GeneratedThumbExists(string thumbFilePath, string thumbFileName)
         {
-            try
-            {
-                CloudBlockBlob blockBlob = container_thumb.GetBlockBlobReference(thumbFileName);
-                return blockBlob.Exists();
-            }
-            catch (Exception ex)
-            {
-                Debug.WriteLine(ex);
-                return false;
-            }
+            return GeneratedThumbExistsAsync(thumbFilePath, thumbFileName).Result;
         }
 
         /// <summary>
@@ -150,19 +140,75 @@ namespace Nop.Services.Media
         /// <param name="thumbFileName">Thumb file name</param>
         /// <param name="mimeType">MIME type</param>
         /// <param name="binary">Picture binary</param>
-        protected override void SaveThumb(string thumbFilePath, string thumbFileName, string mimeType, byte[] binary)
+        protected override async void SaveThumb(string thumbFilePath, string thumbFileName, string mimeType, byte[] binary)
         {
-            CloudBlockBlob blockBlob = container_thumb.GetBlockBlobReference(thumbFileName);
-            
+            await SaveThumbAsync(thumbFilePath, thumbFileName, mimeType, binary);
+        }
+
+        /// <summary>
+        /// Initiates an asynchronous operation to delete picture thumbs
+        /// </summary>
+        /// <param name="picture">Picture</param>
+        protected virtual async Task DeletePictureThumbsAsync(Picture picture)
+        {
+            //create a string containing the blob name prefix
+            var prefix = $"{picture.Id.ToString("0000000")}";
+
+            BlobContinuationToken continuationToken = null;
+            do
+            {
+                //get result segment
+                //listing snapshots is only supported in flat mode, so set the useFlatBlobListing parameter to true.
+                var resultSegment = await _container.ListBlobsSegmentedAsync(prefix, true, BlobListingDetails.All, null, continuationToken, null, null);
+
+                //delete files in result segment
+                await Task.WhenAll(resultSegment.Results.Select(blobItem => ((CloudBlockBlob)blobItem).DeleteAsync()));
+
+                //get the continuation token.
+                continuationToken = resultSegment.ContinuationToken;
+            }
+            while (continuationToken != null);
+        }
+
+        /// <summary>
+        /// Initiates an asynchronous operation to get a value indicating whether some file (thumb) already exists
+        /// </summary>
+        /// <param name="thumbFilePath">Thumb file path</param>
+        /// <param name="thumbFileName">Thumb file name</param>
+        /// <returns>Result</returns>
+        protected virtual async Task<bool> GeneratedThumbExistsAsync(string thumbFilePath, string thumbFileName)
+        {
+            try
+            {
+                //GetBlockBlobReference doesn't need to be async since it doesn't contact the server yet
+                var blockBlob = _container.GetBlockBlobReference(thumbFileName);
+
+                return await blockBlob.ExistsAsync();
+            }
+            catch { return false; }
+        }
+
+        /// <summary>
+        /// Initiates an asynchronous operation to save a value indicating whether some file (thumb) already exists
+        /// </summary>
+        /// <param name="thumbFilePath">Thumb file path</param>
+        /// <param name="thumbFileName">Thumb file name</param>
+        /// <param name="mimeType">MIME type</param>
+        /// <param name="binary">Picture binary</param>
+        protected virtual async Task SaveThumbAsync(string thumbFilePath, string thumbFileName, string mimeType, byte[] binary)
+        {
+            //GetBlockBlobReference doesn't need to be async since it doesn't contact the server yet
+            var blockBlob = _container.GetBlockBlobReference(thumbFileName);
+
             //set mime type
-            if (!String.IsNullOrEmpty(mimeType))
+            if (!string.IsNullOrEmpty(mimeType))
                 blockBlob.Properties.ContentType = mimeType;
 
             //set cache control
             if (!string.IsNullOrEmpty(_mediaSettings.AzureCacheControlHeader))
                 blockBlob.Properties.CacheControl = _mediaSettings.AzureCacheControlHeader;
 
-            blockBlob.UploadFromByteArray(binary, 0, binary.Length);
+            await blockBlob.UploadFromByteArrayAsync(binary, 0, binary.Length);
         }
 
         #endregion
