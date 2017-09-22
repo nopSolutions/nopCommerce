54ac9b3f7dcd2a7a74d5c77659513e224de8363a
RomanovM <maxim.r@nopcommerce.com>
2017-09-22 15:48:52 +0300

    #282 Add filter attribute for publishing ModelReceived and ModelPrepared events

--- src/Presentation/Nop.Web.Framework/Events/ModelPrepared.cs
+++ src/Presentation/Nop.Web.Framework/Events/ModelPrepared.cs
@@ -1,18 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Nop.Web.Framework.Mvc.Models;
+﻿using Nop.Web.Framework.Mvc.Models;
 
 namespace Nop.Web.Framework.Events
 {
+    /// <summary>
+    /// Represents an event that occurs after the model is prepared for view
+    /// </summary>
+    /// <typeparam name="T">Type of the model</typeparam>
     public class ModelPrepared<T> where T : BaseNopModel
     {
+        #region Ctor
+
         public ModelPrepared(T model)
         {
             this.Model = model;
         }
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Gets a model
+        /// </summary>
         public T Model { get; private set; }
+
+        #endregion
     }
 }
--- src/Presentation/Nop.Web.Framework/Events/ModelReceived.cs
+++ src/Presentation/Nop.Web.Framework/Events/ModelReceived.cs
@@ -1,22 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc.ModelBinding;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Nop.Web.Framework.Mvc.Models;
 
 namespace Nop.Web.Framework.Events
 {
+    /// <summary>
+    /// Represents an event that occurs after the model is receved from the view
+    /// </summary>
+    /// <typeparam name="T">Type of the model</typeparam>
     public class ModelReceived<T> where T : BaseNopModel
     {
+        #region Ctor
+
         public ModelReceived(T model, ModelStateDictionary modelState)
         {
             this.Model = model;
             this.ModelState = modelState;
         }
 
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Gets a model
+        /// </summary>
         public T Model { get; private set; }
+
+        /// <summary>
+        /// Gets a model state
+        /// </summary>
         public ModelStateDictionary ModelState { get; private set; }
+
+        #endregion
     }
 }
