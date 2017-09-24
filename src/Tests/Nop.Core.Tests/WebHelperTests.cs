b3b25dbe3dd74648d7ce3d21e0d94dd7283d89bb
Alessandro Alves da Cunha <alessandromontividiu03@gmail.com>
2017-09-24 16:02:04 -0300

    ModifyQueryString and RemoveQueryString methods were failing in unit tests

--- src/Tests/Nop.Core.Tests/WebHelperTests.cs
+++ src/Tests/Nop.Core.Tests/WebHelperTests.cs
@@ -82,7 +82,7 @@ namespace Nop.Core.Tests
         public void Can_remove_queryString_should_ignore_input_parameter_case()
         {
             _webHelper.RemoveQueryString("http://www.example.com/?param1=value1&parAm2=value2", "paRAm1")
-                .ShouldEqual("http://www.example.com/?param2=value2");
+                .ShouldEqual("http://www.example.com/?parAm2=value2");
         }
 
         [Test]
@@ -103,14 +103,14 @@ namespace Nop.Core.Tests
         public void Can_modify_queryString_with_anchor()
         {
             _webHelper.ModifyQueryString("http://www.example.com/?param1=value1&param2=value2", "param1=value3", "Test")
-                .ShouldEqual("http://www.example.com/?param1=value3&param2=value2#test");
+                .ShouldEqual("http://www.example.com/?param1=value3&param2=value2#Test");
         }
 
         [Test]
         public void Can_modify_queryString_new_anchor_should_remove_previous_one()
         {
             _webHelper.ModifyQueryString("http://www.example.com/?param1=value1&param2=value2#test1", "param1=value3", "Test2")
-                .ShouldEqual("http://www.example.com/?param1=value3&param2=value2#test2");
+                .ShouldEqual("http://www.example.com/?param1=value3&param2=value2#Test2");
         }
     }
 
