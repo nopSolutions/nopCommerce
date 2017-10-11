ce52a0ff417ffa81659e7416d7d1b517fa2299ee
AndreiMaz <andrey.maz@gmail.com>
2017-10-11 13:26:43 +0300

    Fixed the issue with "view cannot be found" for view components

--- src/Presentation/Nop.Web.Framework/Components/NopViewComponent.cs
+++ src/Presentation/Nop.Web.Framework/Components/NopViewComponent.cs
@@ -40,7 +40,7 @@ namespace Nop.Web.Framework.Components
         }
 
         /// <summary>
-        /// Returns a result which will render the partial view with name <c>&quot;Default&quot;</c>.
+        /// Returns a result which will render the partial view
         /// </summary>
         /// <param name="model">The model object for the view.</param>
         /// <returns>A <see cref="ViewViewComponentResult"/>.</returns>
@@ -50,6 +50,17 @@ namespace Nop.Web.Framework.Components
 
             //invoke the base method
             return base.View<TModel>(model);
+        }
+
+        /// <summary>
+        ///  Returns a result which will render the partial view with name viewName
+        /// </summary>
+        /// <param name="viewName">The name of the partial view to render.</param>
+        /// <returns>A <see cref="ViewViewComponentResult"/>.</returns>
+        public ViewViewComponentResult View(string viewName)
+        {
+            //invoke the base method
+            return base.View(viewName);
         }
     }
 }
