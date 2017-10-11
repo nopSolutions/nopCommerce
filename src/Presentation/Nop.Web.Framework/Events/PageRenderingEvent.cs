6c2af06d2d5e50e031647db7471f626664e2270e
AndreiMaz <andrey.maz@gmail.com>
2017-10-11 15:24:34 +0300

    Added "Page rendering" event. For example, you can use it to easily ad custom resources (e.g. css or js) to any page

---
+++ src/Presentation/Nop.Web.Framework/Events/PageRenderingEvent.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Nop.Web.Framework.Events
+{
+    /// <summary>
+    /// Page rendering event
+    /// </summary>
+    public class PageRenderingEvent
+    {
+        /// <summary>
+        /// Ctor
+        /// </summary>
+        /// <param name="helper">HTML Helper</param>
+        public PageRenderingEvent(IHtmlHelper helper)
+        {
+            this.Helper = helper;
+        }
+
+        /// <summary>
+        /// HTML helper
+        /// </summary>
+        public IHtmlHelper Helper { get; private set; }
+    }
+}
