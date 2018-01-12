72990f9b2cd6e08467de9e05c787e92d2ea31f1a
RomanovM <maxim.r@nopcommerce.com>
2018-01-12 11:33:22 +0300

    #2820 Use analytics.js as predefined script on installation GoogleAnalytics plugin

--- src/Plugins/Nop.Plugin.Widgets.GoogleAnalytics/GoogleAnalyticPlugin.cs
+++ src/Plugins/Nop.Plugin.Widgets.GoogleAnalytics/GoogleAnalyticPlugin.cs
@@ -72,17 +72,14 @@ namespace Nop.Plugin.Widgets.GoogleAnalytics
             {
                 GoogleId = "UA-0000000-0",
                 TrackingScript = @"<!-- Google code for Analytics tracking -->
-                    <script type=""text/javascript"">
-                    var _gaq = _gaq || [];
-                    _gaq.push(['_setAccount', '{GOOGLEID}']);
-                    {CustomerID}
-                    _gaq.push(['_trackPageview']);
-                    {ECOMMERCE}
-                    (function() {
-                        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-                        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-                        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-                    })();
+                    <script>
+                        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+                        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+                        ga('create', '{GOOGLEID}', 'auto');
+                        {CustomerID}
+                        ga('send', 'pageview');
                     </script>",
             };
             _settingService.SaveSetting(settings);
