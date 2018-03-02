13b85a913476a66b27a52171a2353a2dad7f2558
RomanovM <maxim.r@nopcommerce.com>
2018-03-02 11:16:51 +0300

    #1897 Add BasePagedListModel

---
+++ src/Presentation/Nop.Web.Framework/Models/BasePagedListModel.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace Nop.Web.Framework.Models
+{
+    /// <summary>
+    /// Represents the base paged list model (implementation for KendoUI grids)
+    /// </summary>
+    public abstract partial class BasePagedListModel<T> : BaseNopModel, IPagedModel<T> where T : BaseNopModel
+    {
+        /// <summary>
+        /// Gets or sets data records
+        /// </summary>
+        public IEnumerable<T> Data { get; set; }
+
+        /// <summary>
+        /// Gets or sets total records number
+        /// </summary>
+        public int Total { get; set; }
+
+        /// <summary>
+        /// Gets or sets an extra data
+        /// </summary>
+        public object ExtraData { get; set; }
+
+        /// <summary>
+        /// Gets or sets an errors
+        /// </summary>
+        public object Errors { get; set; }
+    }
+}
---
+++ src/Presentation/Nop.Web.Framework/Models/IPagedModel.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Nop.Web.Framework.Models
+{
+    /// <summary>
+    /// Represents a paged model
+    /// </summary>
+    public partial interface IPagedModel<T> where T : BaseNopModel
+    {
+    }
+}
