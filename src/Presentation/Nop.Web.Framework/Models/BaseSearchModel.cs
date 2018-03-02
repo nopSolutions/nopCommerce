ad211dfef31fac8728155d4091037b953b80ba92
RomanovM <maxim.r@nopcommerce.com>
2018-03-02 10:31:54 +0300

    #1897 Add BaseSearchModel

---
+++ src/Presentation/Nop.Web.Framework/Models/BaseSearchModel.cs
@@ -0,0 +1,34 @@
+﻿
+namespace Nop.Web.Framework.Models
+{
+    /// <summary>
+    /// Represents base search model
+    /// </summary>
+    public abstract partial class BaseSearchModel : BaseNopModel, IPagingRequestModel
+    {
+        #region Ctor
+        
+        public BaseSearchModel()
+        {
+            //set the default values
+            this.Page = 1;
+            this.PageSize = 10;
+        }
+
+        #endregion
+        
+        #region Properties
+
+        /// <summary>
+        /// Gets or sets a page number
+        /// </summary>
+        public int Page { get; set; }
+
+        /// <summary>
+        /// Gets or sets a page size
+        /// </summary>
+        public int PageSize { get; set; }
+
+        #endregion
+    }
+}
