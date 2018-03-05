e1a186454a7f4046fe42b5cacfd0b99d423b3ed9
RomanovM <maxim.r@nopcommerce.com>
2018-03-05 11:19:51 +0300

    #196 Update RedLock library to the latest version

--- src/Libraries/Nop.Core/Caching/RedisConnectionWrapper.cs
+++ src/Libraries/Nop.Core/Caching/RedisConnectionWrapper.cs
@@ -2,7 +2,8 @@ using System;
 using System.Linq;
 using System.Net;
 using Nop.Core.Configuration;
-using RedLock;
+using RedLockNet.SERedis;
+using RedLockNet.SERedis.Configuration;
 using StackExchange.Redis;
 
 namespace Nop.Core.Caching
@@ -18,7 +19,7 @@ namespace Nop.Core.Caching
 
         private readonly Lazy<string> _connectionString;
         private volatile ConnectionMultiplexer _connection;
-        private volatile RedisLockFactory _redisLockFactory;
+        private volatile RedLockFactory _redisLockFactory;
         private readonly object _lock = new object();
 
         #endregion
@@ -72,34 +73,26 @@ namespace Nop.Core.Caching
         }
 
         /// <summary>
-        /// Create instance of RedisLockFactory
+        /// Create instance of RedLock factory
         /// </summary>
-        /// <returns>RedisLockFactory</returns>
-        protected RedisLockFactory CreateRedisLockFactory()
+        /// <returns>RedLock factory</returns>
+        protected RedLockFactory CreateRedisLockFactory()
         {
-            //get password and value whether to use ssl from connection string
-            var password = string.Empty;
-            var useSsl = false;
-            foreach (var option in _connectionString.Value.Split(',').Where(option => option.Contains('=')))
-            {
-                switch (option.Substring(0, option.IndexOf('=')).Trim().ToLowerInvariant())
-                {
-                    case "password":
-                        password = option.Substring(option.IndexOf('=') + 1).Trim();
-                        break;
-                    case "ssl":
-                        bool.TryParse(option.Substring(option.IndexOf('=') + 1).Trim(), out useSsl);
-                        break;
-                }
-            }
-
-            //create RedisLockFactory for using Redlock distributed lock algorithm
-            return new RedisLockFactory(GetEndPoints().Select(endPoint => new RedisLockEndPoint
+            //get RedLock endpoints
+            var configurationOptions = ConfigurationOptions.Parse(_connectionString.Value);
+            var redLockEndPoints = GetEndPoints().Select(endPoint => new RedLockEndPoint
             {
                 EndPoint = endPoint,
-                Password = password,
-                Ssl = useSsl
-            }));
+                Password = configurationOptions.Password,
+                Ssl = configurationOptions.Ssl,
+                RedisDatabase = configurationOptions.DefaultDatabase,
+                ConfigCheckSeconds = configurationOptions.ConfigCheckSeconds,
+                ConnectionTimeout = configurationOptions.ConnectTimeout,
+                SyncTimeout = configurationOptions.SyncTimeout
+            }).ToList();
+
+            //create RedLock factory to use RedLock distributed lock algorithm
+            return RedLockFactory.Create(redLockEndPoints);
         }
 
         #endregion
@@ -159,7 +152,7 @@ namespace Nop.Core.Caching
         public bool PerformActionWithLock(string resource, TimeSpan expirationTime, Action action)
         {
             //use RedLock library
-            using (var redisLock = _redisLockFactory.Create(resource, expirationTime))
+            using (var redisLock = _redisLockFactory.CreateLock(resource, expirationTime))
             {
                 //ensure that lock is acquired
                 if (!redisLock.IsAcquired)
@@ -180,7 +173,7 @@ namespace Nop.Core.Caching
             //dispose ConnectionMultiplexer
             _connection?.Dispose();
 
-            //dispose RedisLockFactory
+            //dispose RedLock factory
             _redisLockFactory?.Dispose();
         }
 
