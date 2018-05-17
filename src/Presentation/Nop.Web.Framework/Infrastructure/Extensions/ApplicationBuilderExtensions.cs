b0625aa8ef9d0f953307bebf4021b980d38cacc1
RomanovM <maxim.r@nopcommerce.com>
2018-05-17 15:07:44 +0300

    Update MiniProfiler package

--- src/Presentation/Nop.Web.Framework/Infrastructure/Extensions/ApplicationBuilderExtensions.cs
+++ src/Presentation/Nop.Web.Framework/Infrastructure/Extensions/ApplicationBuilderExtensions.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Diagnostics;
+﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Hosting;
@@ -7,16 +6,12 @@ using Microsoft.AspNetCore.Http;
 using Nop.Core;
 using Nop.Core.Configuration;
 using Nop.Core.Data;
-using Nop.Core.Domain;
 using Nop.Core.Http;
 using Nop.Core.Infrastructure;
 using Nop.Services.Authentication;
 using Nop.Services.Logging;
-using Nop.Services.Security;
-using System.Threading.Tasks;
 using Nop.Web.Framework.Globalization;
 using Nop.Web.Framework.Mvc.Routing;
-using StackExchange.Profiling.Storage;
 
 namespace Nop.Web.Framework.Infrastructure.Extensions
 {
@@ -206,32 +201,6 @@ namespace Nop.Web.Framework.Infrastructure.Extensions
                 //register all routes
                 EngineContext.Current.Resolve<IRoutePublisher>().RegisterRoutes(routeBuilder);
             });
-        }
-
-        /// <summary>
-        /// Create and configure MiniProfiler service
-        /// </summary>
-        /// <param name="application">Builder for configuring an application's request pipeline</param>
-        public static void UseMiniProfiler(this IApplicationBuilder application)
-        {
-            //whether database is already installed
-            if (!DataSettingsManager.DatabaseIsInstalled)
-                return;
-
-            //whether MiniProfiler should be displayed
-            if (EngineContext.Current.Resolve<StoreInformationSettings>().DisplayMiniProfilerInPublicStore)
-            {
-                application.UseMiniProfiler(miniProfilerOptions =>
-                {
-                    //use memory cache provider for storing each result
-                    miniProfilerOptions.Storage = new MemoryCacheStorage(TimeSpan.FromMinutes(60));
-
-                    //determine who can access the MiniProfiler results
-                    miniProfilerOptions.ResultsAuthorize = request =>
-                        !EngineContext.Current.Resolve<StoreInformationSettings>().DisplayMiniProfilerForAdminOnly ||
-                        EngineContext.Current.Resolve<IPermissionService>().Authorize(StandardPermissionProvider.AccessAdminPanel);
-                });
-            }
         }
     }
 }
--- src/Presentation/Nop.Web.Framework/Infrastructure/NopMvcStartup.cs
+++ src/Presentation/Nop.Web.Framework/Infrastructure/NopMvcStartup.cs
@@ -19,7 +19,7 @@ namespace Nop.Web.Framework.Infrastructure
         public void ConfigureServices(IServiceCollection services, IConfigurationRoot configuration)
         {
             //add MiniProfiler services
-            services.AddMiniProfiler();
+            services.AddNopMiniProfiler();
 
             //add and configure MVC feature
             services.AddNopMvc();
