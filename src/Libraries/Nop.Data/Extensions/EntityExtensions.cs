32ab149eceaf83b2449cee7d5e952418337831d1
AndreiMaz <andrey.maz@gmail.com>
2018-06-14 15:01:50 +0300

    Added source code comments

--- src/Libraries/Nop.Data/Extensions/EntityExtensions.cs
+++ src/Libraries/Nop.Data/Extensions/EntityExtensions.cs
@@ -12,17 +12,17 @@ namespace Nop.Data.Extensions
         /// <summary>
         /// Check whether an entity is proxy
         /// </summary>
-        /// <param name="entity"></param>
-        /// <returns></returns>
+        /// <param name="entity">Entity</param>
+        /// <returns>Result</returns>
         private static bool IsProxy(this BaseEntity entity)
         {
             if (entity == null)
                 throw new ArgumentNullException(nameof(entity));
 
-            //in EF 6 we cold use ObjectContext.GetObjectType. Now it's not available. Here is a workaround:
+            //in EF 6 we could use ObjectContext.GetObjectType. Now it's not available. Here is a workaround:
 
             var type = entity.GetType();
-            //e.g. CustomerProxy will be derived from Customer. And Customer is derived from BaseEntity
+            //e.g. "CustomerProxy" will be derived from "Customer". And "Customer" is derived from BaseEntity
             return type.BaseType != null && type.BaseType.BaseType != null && type.BaseType.BaseType == typeof(BaseEntity);
         }
 
