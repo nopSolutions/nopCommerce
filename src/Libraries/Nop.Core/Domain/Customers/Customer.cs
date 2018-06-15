f08125d4d834799ff2e7de11d14e7650822cb6d8
AndreiMaz <andrey.maz@gmail.com>
2018-06-15 13:27:32 +0300

    Performance optimization. A workaround to load navigation properties (many-many tables) was too slow. Done only for the mostly used property

--- src/Libraries/Nop.Core/Domain/Customers/Customer.cs
+++ src/Libraries/Nop.Core/Domain/Customers/Customer.cs
@@ -16,7 +16,8 @@ namespace Nop.Core.Domain.Customers
         private ICollection<ShoppingCartItem> _shoppingCartItems;
         private ICollection<ReturnRequest> _returnRequests;
         private ICollection<CustomerAddressMapping> _customerAddressMappings;
-        
+        private IList<CustomerRole> _customerRoles;
+
         /// <summary>
         /// Ctor
         /// </summary>
@@ -157,9 +158,12 @@ namespace Nop.Core.Domain.Customers
         }
 
         /// <summary>
-        /// Gets or sets the customer roles
+        /// Gets or sets customer roles
         /// </summary>
-        public IList<CustomerRole> CustomerRoles => CustomerCustomerRoleMappings.Select(mapping => mapping.CustomerRole).ToList();
+        public virtual IList<CustomerRole> CustomerRoles
+        {
+            get => _customerRoles ?? (_customerRoles = CustomerCustomerRoleMappings.Select(mapping => mapping.CustomerRole).ToList());
+        }
 
         /// <summary>
         /// Gets or sets customer-customer role mappings
