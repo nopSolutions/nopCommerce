162bcb72fbcbcb462bf2e901b9c1a6ca37afc343
skoshelev <sergey.k@nopcommerce.com>
2018-06-21 14:21:08 +0300

    #3039 Removed IsProxy method from EntityExtensions class

--- src/Libraries/Nop.Data/Extensions/EntityExtensions.cs
+++ src/Libraries/Nop.Data/Extensions/EntityExtensions.cs
@@ -1,6 +1,7 @@
 using System;
+using Microsoft.EntityFrameworkCore;
 using Nop.Core;
-using Nop.Core.Caching;
+using Nop.Core.Infrastructure;
 
 namespace Nop.Data.Extensions
 {
@@ -9,23 +10,6 @@ namespace Nop.Data.Extensions
     /// </summary>
     public static class EntityExtensions
     {
-        /// <summary>
-        /// Check whether an entity is proxy
-        /// </summary>
-        /// <param name="entity">Entity</param>
-        /// <returns>Result</returns>
-        private static bool IsProxy(this BaseEntity entity)
-        {
-            if (entity == null)
-                throw new ArgumentNullException(nameof(entity));
-
-            //in EF 6 we could use ObjectContext.GetObjectType. Now it's not available. Here is a workaround:
-
-            var type = entity.GetType();
-            //e.g. "CustomerProxy" will be derived from "Customer". And "Customer" is derived from BaseEntity
-            return type.BaseType != null && type.BaseType.BaseType != null && type.BaseType.BaseType == typeof(BaseEntity);
-        }
-
         /// <summary>
         /// Get unproxied entity type
         /// </summary>
@@ -36,22 +20,14 @@ namespace Nop.Data.Extensions
         /// for tracking changes and lazy loading.
         /// </remarks>
         /// <param name="entity"></param>
-        /// <returns></returns>
+        /// <returns>Unproxied entity type</returns>
         public static Type GetUnproxiedEntityType(this BaseEntity entity)
         {
             if (entity == null)
                 throw new ArgumentNullException(nameof(entity));
 
-            Type type = null;
-            //cachable entity (get the base entity type)
-            if (entity is IEntityForCaching)
-                type = ((IEntityForCaching) entity).GetType().BaseType;
-            //EF proxy
-            else if (entity.IsProxy())
-                type = entity.GetType().BaseType;
-            //not proxied entity
-            else
-                type = entity.GetType();
+            var dbContext = EngineContext.Current.Resolve<IDbContext>() as DbContext;
+                var type = dbContext?.Model.FindRuntimeEntityType(entity.GetType()).ClrType;
 
             if (type == null)
                 throw new Exception("Original entity type cannot be loaded");
