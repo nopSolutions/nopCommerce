1d4b92db9d497f90a2de80aee984b689f302aba4
AndreiMaz <andrey.maz@gmail.com>
2018-06-22 10:23:47 +0300

    #3050 Items were not properly added to collections (EF 2.1 changes)

--- src/Libraries/Nop.Core/BaseEntity.cs
+++ src/Libraries/Nop.Core/BaseEntity.cs
@@ -1,5 +1,3 @@
-using System;
-
 namespace Nop.Core
 {
     /// <summary>
@@ -11,87 +9,5 @@ namespace Nop.Core
         /// Gets or sets the entity identifier
         /// </summary>
         public int Id { get; set; }
-
-        /// <summary>
-        /// Is transient
-        /// </summary>
-        /// <param name="obj">Object</param>
-        /// <returns>Result</returns>
-        private static bool IsTransient(BaseEntity obj)
-        {
-            return obj != null && Equals(obj.Id, default(int));
-        }
-
-        /// <summary>
-        /// Get unproxied type
-        /// </summary>
-        /// <returns></returns>
-        private Type GetUnproxiedType()
-        {
-            return GetType();
-        }
-
-        /// <summary>
-        /// Equals
-        /// </summary>
-        /// <param name="obj">Object</param>
-        /// <returns>Result</returns>
-        public override bool Equals(object obj)
-        {
-            return Equals(obj as BaseEntity);
-        }
-
-        /// <summary>
-        /// Equals
-        /// </summary>
-        /// <param name="other">other entity</param>
-        /// <returns>Result</returns>
-        public virtual bool Equals(BaseEntity other)
-        {
-            if (other == null)
-                return false;
-
-            if (ReferenceEquals(this, other))
-                return true;
-
-            if (IsTransient(this) || IsTransient(other) || !Equals(Id, other.Id)) 
-                return false;
-
-            var otherType = other.GetUnproxiedType();
-            var thisType = GetUnproxiedType();
-
-            return thisType.IsAssignableFrom(otherType) || otherType.IsAssignableFrom(thisType);
-        }
-
-        /// <summary>
-        /// Get hash code
-        /// </summary>
-        /// <returns></returns>
-        public override int GetHashCode()
-        {
-            return Equals(Id, default(int)) ? base.GetHashCode() : Id.GetHashCode();
-        }
-
-        /// <summary>
-        /// Equal
-        /// </summary>
-        /// <param name="x">x</param>
-        /// <param name="y">y</param>
-        /// <returns>Result</returns>
-        public static bool operator ==(BaseEntity x, BaseEntity y)
-        {
-            return Equals(x, y);
-        }
-
-        /// <summary>
-        /// Not equal
-        /// </summary>
-        /// <param name="x">x</param>
-        /// <param name="y">y</param>
-        /// <returns>Result</returns>
-        public static bool operator !=(BaseEntity x, BaseEntity y)
-        {
-            return !(x == y);
-        }
     }
 }
