45d9b126650812aea4598a89ec2c0b5abba5bacb
RuS984 <ruslan.s@nopcommerce.com>
2018-07-16 10:05:43 +0300

    #2539 Quick search in admin area include plugins added

--- src/Presentation/Nop.Web/wwwroot/js/admin.navigation.js
+++ src/Presentation/Nop.Web/wwwroot/js/admin.navigation.js
@@ -3,7 +3,7 @@
 Admin.Navigation = (function () {
     var buildMap = function () {
         var map = {};
-        
+
         var linkElements = $("a.menu-item-link");
 
         linkElements.each(function () {
@@ -50,9 +50,20 @@ Admin.Navigation = (function () {
     };
 
     var map;
-   
+
     var init = function () {
         map = buildMap();
+        $.ajax({
+            cache: false,
+            url: '/admin/plugin/SearchList',
+            type: 'get',
+            async:false,
+            success: function (res) { result = res;}
+        });
+
+        for (i = 0; i < result.length; i++) {
+            map[result[i].link] = result[i];
+        }
     };
     var events = {};
     return {
@@ -71,7 +82,7 @@ Admin.Navigation = (function () {
             }
             window.location.href = url;
         },
-        
+
         initOnce: function () {
             if (!map)
                 init();
