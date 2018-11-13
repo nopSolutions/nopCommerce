894a3dec6abb120424dd054221ac04af52da995a
Diogenes Polanco Martinez <diogenes_polanco@claro.com.do>
2018-11-13 14:44:03 -0400

    ValueCountLimit and ValueLengthLimit = int.MaxValue

---
+++ src/Presentation/Nop.Web.Framework/Mvc/Filters/RequestFormSizeLimitAttribute.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+
+namespace Nop.Web.Framework.Mvc.Filters
+{
+    /// <summary>
+    /// Filter to set size limits for request form data
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
+    public class RequestFormSizeLimitAttribute : Attribute, IAuthorizationFilter, IOrderedFilter
+    {
+        private readonly FormOptions _formOptions;
+
+        public RequestFormSizeLimitAttribute(int valueCountLimit = int.MaxValue, int valueLengthLimit = int.MaxValue)
+        {
+            _formOptions = new FormOptions()
+            {
+                ValueCountLimit = valueCountLimit,
+                ValueLengthLimit = valueLengthLimit
+            };
+        }
+
+        public int Order { get; set; }
+
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            var features = context.HttpContext.Features;
+            var formFeature = features.Get<IFormFeature>();
+
+            if (formFeature == null || formFeature.Form == null)
+            {
+                // Request form has not been read yet, so set the limits
+                features.Set<IFormFeature>(new FormFeature(context.HttpContext.Request, _formOptions));
+            }
+        }
+    }
+
+}
