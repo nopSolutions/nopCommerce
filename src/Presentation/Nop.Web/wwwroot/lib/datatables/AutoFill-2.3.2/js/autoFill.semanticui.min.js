c3e6b49cbed0fdee513dc73dae74b07c12703e67
LubovKhokhlova <lubov.k@nopcommerce.com>
2018-12-05 14:24:02 +0300

    AdminLTE table styles

--- src/Presentation/Nop.Web/wwwroot/lib/datatables/AutoFill-2.3.2/js/autoFill.semanticui.min.js
+++
@@ -1,5 +0,0 @@
-/*!
- Bootstrap integration for DataTables' AutoFill
- ©2015 SpryMedia Ltd - datatables.net/license
-*/
-(function(b){"function"===typeof define&&define.amd?define(["jquery","datatables.net-se","datatables.net-autofill"],function(a){return b(a,window,document)}):"object"===typeof exports?module.exports=function(a,c){a||(a=window);c&&c.fn.dataTable||(c=require("datatables.net-se")(a,c).$);c.fn.dataTable.AutoFill||require("datatables.net-autofill")(a,c);return b(c,a,a.document)}:b(jQuery,window,document)})(function(b,a,c,d){b=b.fn.dataTable;b.AutoFill.classes.btn="ui button";return b});
--- src/Presentation/Nop.Web/wwwroot/lib/datatables/FixedColumns-3.2.5/js/fixedColumns.semanticui.min.js
+++
@@ -1,5 +0,0 @@
-/*!
- Semanic UI styling wrapper for FixedColumns
- ©2018 SpryMedia Ltd - datatables.net/license
-*/
-(function(c){"function"===typeof define&&define.amd?define(["jquery","datatables.net-se","datatables.net-fixedcolumns"],function(a){return c(a,window,document)}):"object"===typeof exports?module.exports=function(a,b){a||(a=window);if(!b||!b.fn.dataTable)b=require("datatables.net-se")(a,b).$;b.fn.dataTable.FixedColumns||require("datatables.net-fixedcolumns")(a,b);return c(b,a,a.document)}:c(jQuery,window,document)})(function(c){return c.fn.dataTable});
