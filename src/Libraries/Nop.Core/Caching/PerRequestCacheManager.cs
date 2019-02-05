1756928c9db19af97a0f50352e3f0d789f407ffc
Todd Carter <toddca@users.noreply.github.com>
2019-02-05 16:57:52 -0600

    Update PerRequestCacheManager.cs

--- src/Libraries/Nop.Core/Caching/PerRequestCacheManager.cs
+++ src/Libraries/Nop.Core/Caching/PerRequestCacheManager.cs
@@ -1,10 +1,3 @@
-// -----------------------------------------------------------------------
-// <copyright from="2019" to="2019" file="PerRequestCacheManager.cs" company="Lindell Technologies">
-//    Copyright (c) Lindell Technologies All Rights Reserved.
-//    Information Contained Herein is Proprietary and Confidential.
-// </copyright>
-// -----------------------------------------------------------------------
-
 using System;
 using System.Collections.Generic;
 using System.Linq;
