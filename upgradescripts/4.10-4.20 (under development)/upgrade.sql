6ed5261c60c03d3b9ffe4bee83bba413e34a3ed5
Dmitriy Borowskiy <d.borowskij@gmail.com>
2019-02-28 08:00:26 +0300

    remove data from sql file, locales noprex.xml and upgrade.sql, in action recoverypassword checking validation captcha

--- upgradescripts/4.10-4.20 (under development)/upgrade.sql
+++ upgradescripts/4.10-4.20 (under development)/upgrade.sql
@@ -284,6 +284,12 @@ set @resources='
   <LocaleResource Name="Admin.Catalog.Products.AssociatedProducts.TryToAddSelfGroupedProduct">
     <Value>You cannot add current group product to related ones. This group product was ignored while adding.</Value>
   </LocaleResource>
+  <LocaleResource Name="Admin.Configuration.Settings.GeneralCommon.CaptchaShowOnForgotPasswordPage">
+    <Value>Show on forgot password page.</Value>
+  </LocaleResource>
+  <LocaleResource Name="Admin.Configuration.Settings.GeneralCommon.CaptchaShowOnForgotPasswordPage.Hint">
+    <Value>Check to show CAPTCHA on forgot password page when restore password</Value>
+  </LocaleResource>
 </Language>'
 
 CREATE TABLE #LocaleStringResourceTmp
