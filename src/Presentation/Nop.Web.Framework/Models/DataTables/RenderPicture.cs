155d00a469746e2a940dc06ad42a37b95eddf507
AndreiMaz <andrey.maz@gmail.com>
2019-04-08 17:47:10 +0300

    #75 Support for URL prefix for "img" columns

--- src/Presentation/Nop.Web.Framework/Models/DataTables/RenderPicture.cs
+++ src/Presentation/Nop.Web.Framework/Models/DataTables/RenderPicture.cs
@@ -5,7 +5,21 @@
     /// </summary>
     public partial class RenderPicture : IRender
     {
+        #region Ctor
+
+        public RenderPicture(string srcPrefix = "")
+        {
+            SrcPrefix = srcPrefix;
+        }
+
+        #endregion
+
         #region Properties
+
+        /// <summary>
+        /// Gets or sets picture URL prefix
+        /// </summary>
+        public string SrcPrefix { get; set; }
 
         /// <summary>
         /// Gets or sets picture source
