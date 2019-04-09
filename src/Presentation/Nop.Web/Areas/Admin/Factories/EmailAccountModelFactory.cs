d715d910c247aaed0df1e3899d0a9faf2bd4391d
DmitriyKulagin <kulagin87@gmail.com>
2019-04-09 16:16:37 +0300

    #75 Moved "EmailAccounts" table to DataTables

--- src/Presentation/Nop.Web/Areas/Admin/Factories/EmailAccountModelFactory.cs
+++ src/Presentation/Nop.Web/Areas/Admin/Factories/EmailAccountModelFactory.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Nop.Core.Domain.Messages;
+using Nop.Services.Localization;
 using Nop.Services.Messages;
 using Nop.Web.Areas.Admin.Infrastructure.Mapper.Extensions;
 using Nop.Web.Areas.Admin.Models.Messages;
+using Nop.Web.Framework.Models.DataTables;
 using Nop.Web.Framework.Models.Extensions;
 
 namespace Nop.Web.Areas.Admin.Factories
@@ -17,16 +20,88 @@ namespace Nop.Web.Areas.Admin.Factories
 
         private readonly EmailAccountSettings _emailAccountSettings;
         private readonly IEmailAccountService _emailAccountService;
+        private readonly ILocalizationService _localizationService;
 
         #endregion
 
         #region Ctor
 
         public EmailAccountModelFactory(EmailAccountSettings emailAccountSettings,
-            IEmailAccountService emailAccountService)
+            IEmailAccountService emailAccountService,
+            ILocalizationService localizationService)
         {
             _emailAccountSettings = emailAccountSettings;
             _emailAccountService = emailAccountService;
+            _localizationService = localizationService;
+        }
+
+        #endregion
+
+        #region Utilities
+
+        /// <summary>
+        /// Prepare email account datatables model
+        /// </summary>
+        /// <param name="searchModel">Email account search model</param>
+        /// <returns>Email account datatables model</returns>
+        protected virtual DataTablesModel PrepareEmailAccountGridModel(EmailAccountSearchModel searchModel)
+        {
+            //prepare common properties
+            var model = new DataTablesModel
+            {
+                Name = "email-accounts-grid",
+                UrlRead = new DataUrl("List", "EmailAccount", null),
+                Length = searchModel.PageSize,
+                LengthMenu = searchModel.AvailablePageSizes
+            };
+
+            //prepare filters to search
+            model.Filters = null;
+
+            //prepare model columns
+            model.ColumnCollection = new List<ColumnProperty>
+            {
+                new ColumnProperty(nameof(EmailAccountModel.Email))
+                {
+                    Title = _localizationService.GetResource("Admin.Configuration.EmailAccounts.Fields.Email")
+                },
+                new ColumnProperty(nameof(EmailAccountModel.DisplayName))
+                {
+                    Title = _localizationService.GetResource("Admin.Configuration.EmailAccounts.Fields.DisplayName"),
+                    Width = "200"
+                },
+                new ColumnProperty(nameof(EmailAccountModel.IsDefaultEmailAccount))
+                {
+                    Title = _localizationService.GetResource("Admin.Configuration.EmailAccounts.Fields.IsDefaultEmailAccount"),
+                    Width = "200",
+                    Render = new RenderBoolean()
+                },
+                new ColumnProperty(nameof(EmailAccountModel.Id))
+                {
+                    Title = _localizationService.GetResource("Admin.Configuration.EmailAccounts.Fields.MarkAsDefaultEmail"),
+                    Width = "200",
+                    Render = new RenderCustom("renderColumnMark")
+                    
+                },
+                new ColumnProperty(nameof(EmailAccountModel.Id))
+                {
+                    Title = _localizationService.GetResource("Admin.Common.Edit"),
+                    Width = "100",
+                    Render = new RenderButtonEdit(new DataUrl("Edit"))
+                }
+            };
+
+            //prepare column definitions
+            model.ColumnDefinitions = new List<ColumnDefinition>
+            {
+                new ColumnDefinition()
+                {
+                    Targets = "[-1,2,3]",
+                    ClassName =  StyleColumn.CenterAll
+                }
+            };
+
+            return model;
         }
 
         #endregion
@@ -45,6 +120,7 @@ namespace Nop.Web.Areas.Admin.Factories
 
             //prepare page parameters
             searchModel.SetGridPageSize();
+            searchModel.Grid = PrepareEmailAccountGridModel(searchModel);
 
             return searchModel;
         }
@@ -63,9 +139,9 @@ namespace Nop.Web.Areas.Admin.Factories
             var emailAccounts = _emailAccountService.GetAllEmailAccounts().ToPagedList(searchModel);
 
             //prepare grid model
-            var model = new EmailAccountListModel
+            var model = new EmailAccountListModel().PrepareToGrid(searchModel, emailAccounts, () =>
             {
-                Data = emailAccounts.Select(emailAccount =>
+                return emailAccounts.Select(emailAccount =>
                 {
                     //fill in model values from the entity
                     var emailAccountModel = emailAccount.ToModel<EmailAccountModel>();
@@ -74,9 +150,8 @@ namespace Nop.Web.Areas.Admin.Factories
                     emailAccountModel.IsDefaultEmailAccount = emailAccount.Id == _emailAccountSettings.DefaultEmailAccountId;
 
                     return emailAccountModel;
-                }),
-                Total = emailAccounts.TotalCount
-            };
+                });
+            });
 
             return model;
         }
