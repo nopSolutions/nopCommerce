dca546df528ed11db3f68628b19d23d023d9292e
AndreiMaz <andrey.maz@gmail.com>
2019-04-16 17:06:28 +0300

    #75 Moved "Add required product" table to DataTables

--- src/Presentation/Nop.Web/Areas/Admin/Factories/ProductModelFactory.cs
+++ src/Presentation/Nop.Web/Areas/Admin/Factories/ProductModelFactory.cs
@@ -864,6 +864,59 @@ namespace Nop.Web.Areas.Admin.Factories
             return model;
         }
 
+        /// <summary>
+        /// Prepare datatables model
+        /// </summary>
+        /// <param name="searchModel">Search model</param>
+        /// <returns>Datatables model</returns>
+        protected virtual DataTablesModel PrepareAddRequiredProductGridModel(AddRequiredProductSearchModel searchModel)
+        {
+            //prepare common properties
+            var model = new DataTablesModel
+            {
+                Name = "products-grid",
+                UrlRead = new DataUrl("RequiredProductAddPopupList", "Product", null),
+                SearchButtonId = "search-products",
+                Length = searchModel.PageSize,
+                LengthMenu = searchModel.AvailablePageSizes
+            };
+
+            //prepare filters to search
+            model.Filters = new List<FilterParameter>
+            {
+                new FilterParameter(nameof(searchModel.SearchProductName)),
+                new FilterParameter(nameof(searchModel.SearchCategoryId)),
+                new FilterParameter(nameof(searchModel.SearchManufacturerId)),
+                new FilterParameter(nameof(searchModel.SearchStoreId)),
+                new FilterParameter(nameof(searchModel.SearchVendorId)),
+                new FilterParameter(nameof(searchModel.SearchProductTypeId))
+            };
+
+            //prepare model columns
+            model.ColumnCollection = new List<ColumnProperty>
+            {
+                new ColumnProperty(nameof(ProductModel.Id))
+                {
+                    Title = _localizationService.GetResource("Admin.Common.Select"),
+                    Render = new RenderCustom("renderColumnSelectProduct"),
+                    ClassName =  StyleColumn.CenterAll,
+                    Width = "50",
+                },
+                new ColumnProperty(nameof(ProductModel.Name))
+                {
+                    Title = _localizationService.GetResource("Admin.Catalog.Products.Fields.Name")
+                },
+                new ColumnProperty(nameof(ProductModel.Published))
+                {
+                    Title = _localizationService.GetResource("Admin.Catalog.Products.Fields.Published"),
+                    Width = "100",
+                    Render = new RenderBoolean()
+                }
+            };
+
+            return model;
+        }
+
         #endregion
 
         #region Methods
@@ -1192,7 +1245,8 @@ namespace Nop.Web.Areas.Admin.Factories
 
             //prepare page parameters
             searchModel.SetPopupGridPageSize();
-
+            searchModel.Grid = PrepareAddRequiredProductGridModel(searchModel);
+            
             return searchModel;
         }
 
@@ -1221,18 +1275,16 @@ namespace Nop.Web.Areas.Admin.Factories
                 pageIndex: searchModel.Page - 1, pageSize: searchModel.PageSize);
 
             //prepare grid model
-            var model = new AddRequiredProductListModel
+            var model = new AddRequiredProductListModel().PrepareToGrid(searchModel, products, () =>
             {
-                //fill in model values from the entity
-                Data = products.Select(product =>
+                return products.Select(product =>
                 {
                     var productModel = product.ToModel<ProductModel>();
                     productModel.SeName = _urlRecordService.GetSeName(product, 0, true, false);
 
                     return productModel;
-                }),
-                Total = products.TotalCount
-            };
+                });
+            });
 
             return model;
         }
