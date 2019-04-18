f68b1356cf69cb402ee166ffd18ba618b5fc2b7b
AndreiMaz <andrey.maz@gmail.com>
2019-04-18 21:54:35 +0300

    #75 New way to define tables -  "Message templates"

--- src/Presentation/Nop.Web/Areas/Admin/Factories/MessageTemplateModelFactory.cs
+++ src/Presentation/Nop.Web/Areas/Admin/Factories/MessageTemplateModelFactory.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using Nop.Core.Domain.Catalog;
 using Nop.Core.Domain.Messages;
@@ -10,7 +9,6 @@ using Nop.Web.Areas.Admin.Infrastructure.Mapper.Extensions;
 using Nop.Web.Areas.Admin.Models.Messages;
 using Nop.Web.Framework.Extensions;
 using Nop.Web.Framework.Factories;
-using Nop.Web.Framework.Models.DataTables;
 using Nop.Web.Framework.Models.Extensions;
 
 namespace Nop.Web.Areas.Admin.Factories
@@ -55,69 +53,7 @@ namespace Nop.Web.Areas.Admin.Factories
         }
 
         #endregion
-
-        #region Utilities
-
-        /// <summary>
-        /// Prepare datatables model
-        /// </summary>
-        /// <param name="searchModel">Search model</param>
-        /// <returns>Datatables model</returns>
-        protected virtual DataTablesModel PrepareMessageTemplateGridModel(MessageTemplateSearchModel searchModel)
-        {
-            //prepare common properties
-            var model = new DataTablesModel
-            {
-                Name = "templates-grid",
-                UrlRead = new DataUrl("List", "MessageTemplate", null),
-                SearchButtonId = "search-templates",
-                Length = searchModel.PageSize,
-                LengthMenu = searchModel.AvailablePageSizes
-            };
-
-            //prepare filters to search
-            model.Filters = new List<FilterParameter>
-            {
-                new FilterParameter(nameof(searchModel.SearchStoreId))
-            };
-
-            //prepare model columns
-            model.ColumnCollection = new List<ColumnProperty>
-            {
-                new ColumnProperty(nameof(MessageTemplateModel.Name))
-                {
-                    Title = _localizationService.GetResource("Admin.ContentManagement.MessageTemplates.Fields.Name")
-                },
-                new ColumnProperty(nameof(MessageTemplateModel.Subject))
-                {
-                    Title = _localizationService.GetResource("Admin.ContentManagement.MessageTemplates.Fields.Subject")
-                },
-                new ColumnProperty(nameof(MessageTemplateModel.IsActive))
-                {
-                    Title = _localizationService.GetResource("Admin.ContentManagement.MessageTemplates.Fields.IsActive"),
-                    Width = "100",
-                    ClassName =  StyleColumn.CenterAll,
-                    Render = new RenderBoolean()
-                },
-                new ColumnProperty(nameof(MessageTemplateModel.ListOfStores))
-                {
-                    Title = _localizationService.GetResource("Admin.ContentManagement.MessageTemplates.Fields.LimitedToStores"),
-                    Width = "300"
-                },
-                new ColumnProperty(nameof(MessageTemplateModel.Id))
-                {
-                    Title = _localizationService.GetResource("Admin.Common.Edit"),
-                    Width = "100",
-                    ClassName =  StyleColumn.ButtonStyle,
-                    Render = new RenderButtonEdit(new DataUrl("Edit"))
-                }
-            };
-
-            return model;
-        }
-
-        #endregion
-
+        
         #region Methods
 
         /// <summary>
@@ -137,7 +73,6 @@ namespace Nop.Web.Areas.Admin.Factories
 
             //prepare page parameters
             searchModel.SetGridPageSize();
-            searchModel.Grid = PrepareMessageTemplateGridModel(searchModel);
 
             return searchModel;
         }
