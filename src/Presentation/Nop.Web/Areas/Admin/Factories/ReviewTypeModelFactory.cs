65dcc73be12a24a350486f3492084f78cf2d52ef
AndreiMaz <andrey.maz@gmail.com>
2019-04-18 16:11:45 +0300

    #75 New way to define tables -  "Product review type"

--- src/Presentation/Nop.Web/Areas/Admin/Factories/ReviewTypeModelFactory.cs
+++ src/Presentation/Nop.Web/Areas/Admin/Factories/ReviewTypeModelFactory.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using Nop.Core.Domain.Catalog;
 using Nop.Services.Catalog;
@@ -7,7 +6,6 @@ using Nop.Services.Localization;
 using Nop.Web.Areas.Admin.Infrastructure.Mapper.Extensions;
 using Nop.Web.Areas.Admin.Models.Catalog;
 using Nop.Web.Framework.Factories;
-using Nop.Web.Framework.Models.DataTables;
 using Nop.Web.Framework.Models.Extensions;
 
 namespace Nop.Web.Areas.Admin.Factories
@@ -38,63 +36,6 @@ namespace Nop.Web.Areas.Admin.Factories
 
         #endregion
 
-        #region Utilities
-
-        /// <summary>
-        /// Prepare datatables model
-        /// </summary>
-        /// <param name="searchModel">Search model</param>
-        /// <returns>Datatables model</returns>
-        protected virtual DataTablesModel PrepareReviewTypeGridModel(ReviewTypeSearchModel searchModel)
-        {
-            //prepare common properties
-            var model = new DataTablesModel
-            {
-                Name = "reviewtypes-grid",
-                UrlRead = new DataUrl("List", "ReviewType", null),
-                Length = searchModel.PageSize,
-                LengthMenu = searchModel.AvailablePageSizes
-            };
-            
-            //prepare model columns
-            model.ColumnCollection = new List<ColumnProperty>
-            {
-                new ColumnProperty(nameof(ReviewTypeModel.Name))
-                {
-                    Title = _localizationService.GetResource("Admin.Settings.ReviewType.Fields.Name"),
-                    Width = "300"
-                },
-                new ColumnProperty(nameof(ReviewTypeModel.IsRequired))
-                {
-                    Title = _localizationService.GetResource("Admin.Settings.ReviewType.Fields.IsRequired"),
-                    Width = "100",
-                    Render = new RenderBoolean()
-                },
-                new ColumnProperty(nameof(ReviewTypeModel.VisibleToAllCustomers))
-                {
-                    Title = _localizationService.GetResource("Admin.Settings.ReviewType.Fields.VisibleToAllCustomers"),
-                    Width = "100",
-                    Render = new RenderBoolean()
-                },
-                new ColumnProperty(nameof(ReviewTypeModel.DisplayOrder))
-                {
-                    Title = _localizationService.GetResource("Admin.Settings.ReviewType.Fields.DisplayOrder"),
-                    Width = "100"
-                },
-                new ColumnProperty(nameof(ReviewTypeModel.Id))
-                {
-                    Title = _localizationService.GetResource("Admin.Common.Edit"),
-                    Width = "100",
-                    ClassName =  StyleColumn.ButtonStyle,
-                    Render = new RenderButtonEdit(new DataUrl("~/Admin/ReviewType/Edit/"))
-                }
-            };
-
-            return model;
-        }
-
-        #endregion
-
         #region Methods
 
         /// <summary>
@@ -109,7 +50,6 @@ namespace Nop.Web.Areas.Admin.Factories
 
             //prepare page parameters
             searchModel.SetGridPageSize();
-            searchModel.Grid = PrepareReviewTypeGridModel(searchModel);
 
             return searchModel;
         }
