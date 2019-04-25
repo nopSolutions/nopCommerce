4733f0d35a3b6ad8ce0a4872ee2768a4271040ad
DmitriyKulagin <kulagin87@gmail.com>
2019-04-25 18:53:37 +0300

    #75 Fixed editing for "Shipping providers" table

--- src/Presentation/Nop.Web/Areas/Admin/Factories/ShippingModelFactory.cs
+++ src/Presentation/Nop.Web/Areas/Admin/Factories/ShippingModelFactory.cs
@@ -158,9 +158,9 @@ namespace Nop.Web.Areas.Admin.Factories
             var shippingProviders = _shippingPluginManager.LoadAllPlugins().ToPagedList(searchModel);
 
             //prepare grid model
-            var model = new ShippingProviderListModel
+            var model = new ShippingProviderListModel().PrepareToGrid(searchModel, shippingProviders, () =>
             {
-                Data = shippingProviders.Select(provider =>
+                return shippingProviders.Select(provider =>
                 {
                     //fill in model values from the entity
                     var shippingProviderModel = provider.ToPluginModel<ShippingProviderModel>();
@@ -171,9 +171,8 @@ namespace Nop.Web.Areas.Admin.Factories
                     shippingProviderModel.LogoUrl = _shippingPluginManager.GetPluginLogoUrl(provider);
 
                     return shippingProviderModel;
-                }),
-                Total = shippingProviders.TotalCount
-            };
+                });
+            });
 
             return model;
         }
