2ec1a39e6f4935151183ec31df810f265d398376
David Powell <davidandrewpowell81@gmail.com>
2019-05-08 12:01:52 -0400

    #3810 Uncommented and fixed commented out tests in BlogPostValidatorTests and ProductReviewsValidatorTests.

--- src/Tests/Nop.Web.MVC.Tests/Public/Validators/Catalog/ProductReviewsValidatorTests.cs
+++ src/Tests/Nop.Web.MVC.Tests/Public/Validators/Catalog/ProductReviewsValidatorTests.cs
@@ -1,4 +1,6 @@
-﻿using Nop.Web.Validators.Catalog;
+﻿using FluentValidation.TestHelper;
+using Nop.Web.Models.Catalog;
+using Nop.Web.Validators.Catalog;
 using NUnit.Framework;
 
 namespace Nop.Web.MVC.Tests.Public.Validators.Catalog
@@ -14,42 +16,40 @@ namespace Nop.Web.MVC.Tests.Public.Validators.Catalog
             _validator = new ProductReviewsValidator(_localizationService);
         }
 
-        //TODO uncomment tests when the following FluentVlidation issue is fixed http://fluentvalidation.codeplex.com/workitem/7095
+        [Test]
+        public void Should_have_error_when_title_is_null_or_empty()
+        {
+            var model = new ProductReviewsModel();
+            model.AddProductReview.Title = null;
+            _validator.ShouldHaveValidationErrorFor(x => x.AddProductReview.Title, model);
+            model.AddProductReview.Title = "";
+            _validator.ShouldHaveValidationErrorFor(x => x.AddProductReview.Title, model);
+        }
 
-        //[Test]
-        //public void Should_have_error_when_title_is_null_or_empty()
-        //{
-        //    var model = new ProductReviewsModel();
-        //    model.AddProductReview.Title = null;
-        //    _validator.ShouldHaveValidationErrorFor(x => x.AddProductReview.Title, model);
-        //    model.AddProductReview.Title = "";
-        //    _validator.ShouldHaveValidationErrorFor(x => x.AddProductReview.Title, model);
-        //}
+        [Test]
+        public void Should_not_have_error_when_title_is_specified()
+        {
+            var model = new ProductReviewsModel();
+            model.AddProductReview.Title = "some comment";
+            _validator.ShouldNotHaveValidationErrorFor(x => x.AddProductReview.Title, model);
+        }
 
-        //[Test]
-        //public void Should_not_have_error_when_title_is_specified()
-        //{
-        //    var model = new ProductReviewsModel();
-        //    model.AddProductReview.Title = "some comment";
-        //    _validator.ShouldNotHaveValidationErrorFor(x => x.AddProductReview.Title, model);
-        //}
+        [Test]
+        public void Should_have_error_when_reviewText_is_null_or_empty()
+        {
+            var model = new ProductReviewsModel();
+            model.AddProductReview.ReviewText = null;
+            _validator.ShouldHaveValidationErrorFor(x => x.AddProductReview.ReviewText, model);
+            model.AddProductReview.ReviewText = "";
+            _validator.ShouldHaveValidationErrorFor(x => x.AddProductReview.ReviewText, model);
+        }
 
-        //[Test]
-        //public void Should_have_error_when_reviewText_is_null_or_empty()
-        //{
-        //    var model = new ProductReviewsModel();
-        //    model.AddProductReview.ReviewText = null;
-        //    _validator.ShouldHaveValidationErrorFor(x => x.AddProductReview.ReviewText, model);
-        //    model.AddProductReview.ReviewText = "";
-        //    _validator.ShouldHaveValidationErrorFor(x => x.AddProductReview.ReviewText, model);
-        //}
-
-        //[Test]
-        //public void Should_not_have_error_when_reviewText_is_specified()
-        //{
-        //    var model = new ProductReviewsModel();
-        //    model.AddProductReview.ReviewText = "some comment";
-        //    _validator.ShouldNotHaveValidationErrorFor(x => x.AddProductReview.ReviewText, model);
-        //}
+        [Test]
+        public void Should_not_have_error_when_reviewText_is_specified()
+        {
+            var model = new ProductReviewsModel();
+            model.AddProductReview.ReviewText = "some comment";
+            _validator.ShouldNotHaveValidationErrorFor(x => x.AddProductReview.ReviewText, model);
+        }
     }
 }
