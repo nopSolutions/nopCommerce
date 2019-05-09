0f671afdab0d1376a81da2abb7f6b945447dd072
David Powell <davidandrewpowell81@gmail.com>
2019-05-09 10:09:40 -0400

    #3833 Removed unused methods from Nop.Tests/TypeAssert.

--- src/Tests/Nop.Tests/TypeAssert.cs
+++ src/Tests/Nop.Tests/TypeAssert.cs
@@ -5,15 +5,6 @@ namespace Nop.Tests
 {
     public static class TypeAssert
     {
-        public static void AreEqual(object expected, object instance)
-        {
-            if (expected == null)
-                Assert.IsNull(instance);
-            else
-                Assert.IsNotNull(instance, "Instance was null");
-            Assert.AreEqual(expected.GetType(), instance.GetType(), "Expected: " + expected.GetType() + ", was: " + instance.GetType() + " was not of type " + instance.GetType());
-        }
-
         public static void AreEqual(Type expected, object instance)
         {
             if (expected == null)
@@ -21,16 +12,6 @@ namespace Nop.Tests
             else
                 Assert.IsNotNull(instance, "Instance was null");
             Assert.AreEqual(expected, instance.GetType(), "Expected: " + expected + ", was: " + instance.GetType() + " was not of type " + instance.GetType());
-        }
-
-        public static void Equals<T>(object instance)
-        {
-            AreEqual(typeof(T), instance);
-        }
-
-        public static void Is<T>(object instance)
-        {
-            Assert.IsTrue(instance is T, "Instance " + instance + " was not of type " + typeof(T));
         }
     }
 }
