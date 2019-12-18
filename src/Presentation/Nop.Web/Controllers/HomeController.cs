8ccabfef3fedc4846fcfe2ce163dfa09325f8a28
skoshelev <serega.dn@gmail.com>
2019-12-18 14:12:05 +0300

    Removed test code from HomeController class

--- src/Presentation/Nop.Web/Controllers/HomeController.cs
+++ src/Presentation/Nop.Web/Controllers/HomeController.cs
@@ -1,7 +1,4 @@
-﻿using System.Linq;
-using Microsoft.AspNetCore.Mvc;
-using Nop.Core.Domain.Customers;
-using Nop.Data;
+﻿using Microsoft.AspNetCore.Mvc;
 using Nop.Web.Framework.Mvc.Filters;
 using Nop.Web.Framework.Security;
 
@@ -9,18 +6,6 @@ namespace Nop.Web.Controllers
 {
     public partial class HomeController : BasePublicController
     {
-        public HomeController(IRepository<Customer> cRepository, IRepository<CustomerPassword> cpRepository)
-        {
-            var query = from c in cRepository.Table
-                join cp in cpRepository.Table on c.Id equals cp.CustomerId
-                where cp.PasswordSalt == "qXIzFdY="
-                select c;
-
-            var key = query.Expression.ToString();
-
-            key.ToUpper();
-        }
-
         [HttpsRequirement(SslRequirement.No)]
         public virtual IActionResult Index()
         {
