afb4293ccccc9e14034976619a8e2dacfbdee434
skoshelev <serega.dn@gmail.com>
2019-12-26 11:52:47 +0300

    239 Added new caching to GenericAttributeService

--- src/Libraries/Nop.Services/Caching/CacheEventConsumers/Common/GenericAttributeCacheEventConsumer.cs
+++ src/Libraries/Nop.Services/Caching/CacheEventConsumers/Common/GenericAttributeCacheEventConsumer.cs
@@ -3,11 +3,13 @@ using Nop.Services.Caching.CachingDefaults;
 
 namespace Nop.Services.Caching.CacheEventConsumers.Common
 {
-    public partial class GenericAttributeCacheEventConsumer : CacheEventConsumer<GenericAttribute>
+    public partial class GenericAttributeCacheEventConsumer : EntityCacheEventConsumer<GenericAttribute>
     {
         public override void ClearCashe(GenericAttribute entity)
         {
             _cacheManager.RemoveByPrefix(NopCommonCachingDefaults.GenericAttributePrefixCacheKey);
+
+            base.ClearCashe(entity);
         }
     }
 }
--- src/Libraries/Nop.Services/Common/GenericAttributeService.cs
+++ src/Libraries/Nop.Services/Common/GenericAttributeService.cs
@@ -78,7 +78,7 @@ namespace Nop.Services.Common
             if (attributeId == 0)
                 return null;
 
-            return _genericAttributeRepository.GetById(attributeId);
+            return _genericAttributeRepository.ToCachedGetById(attributeId);
         }
 
         /// <summary>
