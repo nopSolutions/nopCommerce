385505f1014e4609c5828f11b448a1e627bb0789
Huseyin ELMAS <hsynlms47@gmail.com>
2020-01-02 01:42:16 +0300

    lowercase non-western char from the seo char table

--- src/Libraries/Nop.Services/Seo/UrlRecordService.cs
+++ src/Libraries/Nop.Services/Seo/UrlRecordService.cs
@@ -1589,7 +1589,7 @@ namespace Nop.Services.Seo
                 if (convertNonWesternChars)
                 {
                     if (_seoCharacterTable?.ContainsKey(c2) ?? false)
-                        c2 = _seoCharacterTable[c2];
+                        c2 = _seoCharacterTable[c2].ToLowerInvariant();
                 }
 
                 if (allowUnicodeCharsInUrls)
