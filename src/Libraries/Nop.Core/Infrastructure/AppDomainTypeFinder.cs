76fc8f06f1281e2ed73f4149a9829b8dbb1b72e2
Sergey Koshelev <sergey.k@nopcommerce.com>
2020-01-17 10:41:27 +0300

    Code refactoring

--- src/Libraries/Nop.Core/Infrastructure/AppDomainTypeFinder.cs
+++ src/Libraries/Nop.Core/Infrastructure/AppDomainTypeFinder.cs
@@ -163,11 +163,8 @@ namespace Nop.Core.Infrastructure
                     if (!implementedInterface.IsGenericType)
                         continue;
 
-                    var isMatch = genericTypeDefinition.IsAssignableFrom(implementedInterface.GetGenericTypeDefinition());
-                    if (isMatch)
-                    {
+                    if (genericTypeDefinition.IsAssignableFrom(implementedInterface.GetGenericTypeDefinition()))
                         return true;
-                    }
                 }
 
                 return false;
