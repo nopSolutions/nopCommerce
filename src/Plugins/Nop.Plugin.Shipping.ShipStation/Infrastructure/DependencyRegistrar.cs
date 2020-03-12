cd0cb8c778b8f231ffce377b4056d2f3aa291ee4
skoshelev <serega.dn@gmail.com>
2020-03-12 11:22:32 +0300

    #3812 Added the ShipStation plugin

---
+++ src/Plugins/Nop.Plugin.Shipping.ShipStation/Infrastructure/DependencyRegistrar.cs
@@ -0,0 +1,30 @@
+﻿using Autofac;
+using Nop.Core.Configuration;
+using Nop.Core.Infrastructure;
+using Nop.Core.Infrastructure.DependencyManagement;
+using Nop.Plugin.Shipping.ShipStation.Services;
+
+namespace Nop.Plugin.Shipping.ShipStation.Infrastructure
+{
+    /// <summary>
+    /// Dependency registrar
+    /// </summary>
+    public class DependencyRegistrar : IDependencyRegistrar
+    {
+        /// <summary>
+        /// Register services and interfaces
+        /// </summary>
+        /// <param name="builder">Container builder</param>
+        /// <param name="typeFinder">Type finder</param>
+        /// <param name="config">Config</param>
+        public virtual void Register(ContainerBuilder builder, ITypeFinder typeFinder, NopConfig config)
+        {
+            builder.RegisterType<ShipStationService>().As<IShipStationService>().InstancePerLifetimeScope();
+        }
+
+        /// <summary>
+        /// Order of this dependency registrar implementation
+        /// </summary>
+        public int Order => 2;
+    }
+}
---
+++ src/Plugins/Nop.Plugin.Shipping.ShipStation/RouteProvider.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Routing;
+using Nop.Web.Framework.Mvc.Routing;
+
+namespace Nop.Plugin.Shipping.ShipStation
+{
+    public partial class RouteProvider : IRouteProvider
+    {
+        /// <summary>
+        /// Register routes
+        /// </summary>
+        /// <param name="endpointRouteBuilder">Route builder</param>
+        public void RegisterRoutes(IEndpointRouteBuilder endpointRouteBuilder)
+        {
+            //Webhook
+            endpointRouteBuilder.MapControllerRoute("Plugin.Payments.ShipStation.WebhookHandler", "Plugins/ShipStation/Webhook",
+                new { controller = "ShipStation", action = "Webhook" });
+        }
+
+        public int Priority => 0;
+    }
+}
