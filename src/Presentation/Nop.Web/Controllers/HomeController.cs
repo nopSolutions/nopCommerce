6ae884a839bed59a82b6425be53ae4170d63f4be
Anokhin Alexey <exile.developer@gmail.com>
2020-03-18 12:07:24 +0300

    Removed test data

--- src/Presentation/Nop.Web/Controllers/HomeController.cs
+++ src/Presentation/Nop.Web/Controllers/HomeController.cs
@@ -1,7 +1,4 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Caching.Memory;
-using Nop.Core.Caching;
-using Nop.Services.Caching.CachingDefaults;
 using Nop.Web.Framework.Mvc.Filters;
 using Nop.Web.Framework.Security;
 
@@ -9,12 +6,6 @@ namespace Nop.Web.Controllers
 {
     public partial class HomeController : BasePublicController
     {
-        public HomeController(IStaticCacheManager cacheManager, IMemoryCache manager)
-        {
-            var c1 = NopBlogsCachingDefaults.BlogCommentsNumberCacheKey.FillCacheKey(1, 2, 3);
-            var c2 = NopBlogsCachingDefaults.BlogCommentsNumberCacheKey.FillCacheKey(4, 5, 6);
-        }
-
         [HttpsRequirement(SslRequirement.No)]
         public virtual IActionResult Index()
         {
