ef13049f1016a7e1b27f144e5c0191bd2c0bb88e
skoshelev <serega.dn@gmail.com>
2020-03-25 14:21:23 +0300

    Fixed problem with the duplicate images on the DB table

--- src/Libraries/Nop.Services/Media/RoxyFileman/DatabaseRoxyFilemanService.cs
+++ src/Libraries/Nop.Services/Media/RoxyFileman/DatabaseRoxyFilemanService.cs
@@ -346,6 +346,9 @@ namespace Nop.Services.Media.RoxyFileman
             {
                 var uniqueFileName = GetUniqueFileName(filePath, _fileProvider.GetFileNameWithoutExtension(filePath));
 
+                if (_pictureService.GetPictureSeName(uniqueFileName) != null)
+                    continue;
+
                 var picture = new Picture
                 {
                     IsNew = true,
