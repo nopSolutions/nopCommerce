9df93cc80d5b256224697f6b2128694c15bf11e1
Veronica <veronika.s@nopCommerce.com>
2020-03-27 14:30:36 +0300

    #4367 Renamed SendDimensio to PassDimensions
    (cherry picked from commit f341b6ef47f3b5521c59411bbbb9fd074b652e92)

--- src/Plugins/Nop.Plugin.Shipping.ShipStation/ShipStationComputationMethod.cs
+++ src/Plugins/Nop.Plugin.Shipping.ShipStation/ShipStationComputationMethod.cs
@@ -136,8 +136,8 @@ namespace Nop.Plugin.Shipping.ShipStation
             _localizationService.AddOrUpdatePluginLocaleResource("Plugins.Shipping.ShipStation.Fields.PackingType", "Packing type");
             _localizationService.AddOrUpdatePluginLocaleResource("Plugins.Shipping.ShipStation.Fields.Password.Hint", "Specify ShipStation password");
             _localizationService.AddOrUpdatePluginLocaleResource("Plugins.Shipping.ShipStation.Fields.Password", "Password");
-            _localizationService.AddOrUpdatePluginLocaleResource("Plugins.Shipping.ShipStation.Fields.SendDimensio.Hint", "Check if need send the dimension to the ShipStation server");
-            _localizationService.AddOrUpdatePluginLocaleResource("Plugins.Shipping.ShipStation.Fields.SendDimensio", "Send dimensio");
+            _localizationService.AddOrUpdatePluginLocaleResource("Plugins.Shipping.ShipStation.Fields.PassDimensions.Hint", "Check if need send dimensions to the ShipStation server");
+            _localizationService.AddOrUpdatePluginLocaleResource("Plugins.Shipping.ShipStation.Fields.PassDimensions", "Pass dimensions");
             _localizationService.AddOrUpdatePluginLocaleResource("Plugins.Shipping.ShipStation.Fields.UserName", "User name");
             _localizationService.AddOrUpdatePluginLocaleResource("Plugins.Shipping.ShipStation.Fields.UserName.Hint", "Specify ShipStation user name");
 
@@ -165,8 +165,8 @@ namespace Nop.Plugin.Shipping.ShipStation
             _localizationService.DeletePluginLocaleResource("Plugins.Shipping.ShipStation.Fields.PackingType");
             _localizationService.DeletePluginLocaleResource("Plugins.Shipping.ShipStation.Fields.Password.Hint");
             _localizationService.DeletePluginLocaleResource("Plugins.Shipping.ShipStation.Fields.Password");
-            _localizationService.DeletePluginLocaleResource("Plugins.Shipping.ShipStation.Fields.SendDimensio.Hint");
-            _localizationService.DeletePluginLocaleResource("Plugins.Shipping.ShipStation.Fields.SendDimensio");
+            _localizationService.DeletePluginLocaleResource("Plugins.Shipping.ShipStation.Fields.PassDimensions.Hint");
+            _localizationService.DeletePluginLocaleResource("Plugins.Shipping.ShipStation.Fields.PassDimensions");
             _localizationService.DeletePluginLocaleResource("Plugins.Shipping.ShipStation.Fields.UserName.Hint");
             _localizationService.DeletePluginLocaleResource("Plugins.Shipping.ShipStation.Fields.UserName");
 
