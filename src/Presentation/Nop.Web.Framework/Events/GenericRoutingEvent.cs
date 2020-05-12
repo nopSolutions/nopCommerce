aa8c03d777e5f5c56158ef3030789cc3bd581ddd
RomanovM <maxim.r@nopcommerce.com>
2020-05-12 21:45:18 +0200

    #4698 Added a new event to allow handling generic routes in plugins

---
+++ src/Presentation/Nop.Web.Framework/Events/GenericRoutingEvent.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Routing;
+using Nop.Core.Domain.Seo;
+
+namespace Nop.Web.Framework.Events
+{
+    /// <summary>
+    /// Represents an event that occurs when a generic route is processed and no default handlers are found
+    /// </summary>
+    public class GenericRoutingEvent
+    {
+        #region Ctor
+
+        /// <summary>
+        /// Ctor
+        /// </summary>
+        /// <param name="values">Route values</param>
+        /// <param name="urlRecord">Found URL record</param>
+        public GenericRoutingEvent(RouteValueDictionary values, UrlRecord urlRecord)
+        {
+            RouteValues = values;
+            UrlRecord = urlRecord;
+        }
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Gets route values
+        /// </summary>
+        public RouteValueDictionary RouteValues { get; private set; }
+
+        /// <summary>
+        /// Gets URL record found by the route slug
+        /// </summary>
+        public UrlRecord UrlRecord { get; private set; }
+
+        #endregion
+    }
+}
