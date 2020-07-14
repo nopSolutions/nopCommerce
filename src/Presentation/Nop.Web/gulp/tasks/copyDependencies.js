311ed49e84c58543c9caa88fa97b671a2bc27915
DmitriyKulagin <kulagin87@gmail.com>
2020-07-14 10:30:18 +0300

    #4240 Added system of management client-side libraries

---
+++ src/Presentation/Nop.Web/gulp/tasks/copyDependencies.js
@@ -0,0 +1,37 @@
+﻿const gulp = require('gulp');
+const npmDist = require('gulp-npm-dist');
+const rename = require('gulp-rename');
+
+const nodeModules = './node_modules/';
+const targetPath = './wwwroot/lib_npm/';
+
+function copyDependencies(cb) {
+  gulp.src(npmDist({
+    excludes: ['nuget/**/*']
+  }), { base: nodeModules })
+    .pipe(rename(function (path) {
+      path.dirname = path.dirname.replace(/\/dist/, '').replace(/\\dist/, '');
+    }))
+    .pipe(gulp.dest(targetPath))
+  cb();
+};
+
+function jquery_DevTools(cb){
+  //jquery-migrate
+  gulp.src(nodeModules + "jquery-migrate/dist/jquery-migrate.js").pipe(gulp.dest(targetPath + "/jquery-migrate"));
+  gulp.src(nodeModules + "jquery-migrate/dist/jquery-migrate.min.map").pipe(gulp.dest(targetPath + "/jquery-migrate"));
+
+  //jquery.validate.globalize
+  gulp.src(nodeModules + "jquery-validation-globalize/jquery.validate.globalize.min.js.map").pipe(gulp.dest(targetPath + "/jquery-validation-globalize"));
+
+  //bootstrap
+  gulp.src(nodeModules + "bootstrap/dist/css/bootstrap.min.css.map").pipe(gulp.dest(targetPath + "/bootstrap/css"));
+
+  //jsrender
+  gulp.src(nodeModules + "jsrender/jsrender.min.js.map").pipe(gulp.dest(targetPath + "/jsrender"));
+  cb();
+}
+
+exports.jquery_DevTools = jquery_DevTools;
+
+exports.Execute = gulp.parallel(copyDependencies, jquery_DevTools);
---
+++ src/Presentation/Nop.Web/wwwroot/lib_npm/fine-uploader/lib/core/s3.js
@@ -0,0 +1,3 @@
+"use strict";
+
+module.exports = require("../../s3.fine-uploader/s3.fine-uploader.core");
