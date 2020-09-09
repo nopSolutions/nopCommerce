f01042963d1a3a0ce0ccf87cdd767aec4c1461bb
Max <maxkp1994@outlook.com>
2020-09-09 14:39:54 +0200

    #4927 Extract getting "lang pattern" into separate function to avoid code duplication (#4959)
    * Extract getting "lang pattern" into separate function to avoid code duplication
    
    * Fix route provider
    
    Co-authored-by: Maksym Badii <maksym.badii@pwc.com>

---
+++ src/Presentation/Nop.Web/Infrastructure/BaseRouteProvider.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+using Nop.Core.Domain.Localization;
+using Nop.Data;
+using Nop.Services.Localization;
+
+namespace Nop.Web.Infrastructure
+{
+    public class BaseRouteProvider
+    {
+        protected string GetRouterPattern(IEndpointRouteBuilder endpointRouteBuilder, string seoCode = "")
+        {
+            if (DataSettingsManager.DatabaseIsInstalled)
+            {
+                var localizationSettings = endpointRouteBuilder.ServiceProvider.GetRequiredService<LocalizationSettings>();
+                if (localizationSettings.SeoFriendlyUrlsForLanguagesEnabled)
+                {
+                    var langservice = endpointRouteBuilder.ServiceProvider.GetRequiredService<ILanguageService>();
+                    var languages = langservice.GetAllLanguages().ToList();
+                    return "{language:lang=" + languages.FirstOrDefault().UniqueSeoCode + $"}}/{seoCode}";
+                }
+            }
+            return seoCode;
+        }
+    }
+}
