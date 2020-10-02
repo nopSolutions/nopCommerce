b8251c0400c7d57057437969666b1dad58ac5a54
Yevheniy Solovey <jeniasolovey48@gmail.com>
2020-10-02 09:22:22 +0200

    remove unused using

--- src/Presentation/Nop.Web.Framework/Infrastructure/NopRoutingStartup.cs
+++ src/Presentation/Nop.Web.Framework/Infrastructure/NopRoutingStartup.cs
@@ -2,7 +2,6 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Nop.Core.Infrastructure;
-using Nop.Web.Framework.Infrastructure.Extensions;
 
 namespace Nop.Web.Framework.Infrastructure
 {
