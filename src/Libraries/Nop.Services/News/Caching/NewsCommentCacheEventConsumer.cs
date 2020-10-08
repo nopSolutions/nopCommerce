450cd4ed74da494903de0a6edb8594d020b78971
holydk <omigoneo@gmail.com>
2020-10-08 16:11:24 +0300

    #5075 Fix the caching bug the number of comments to news

--- src/Libraries/Nop.Services/News/Caching/NewsCommentCacheEventConsumer.cs
+++ src/Libraries/Nop.Services/News/Caching/NewsCommentCacheEventConsumer.cs
@@ -9,16 +9,12 @@ namespace Nop.Services.News.Caching
     public partial class NewsCommentCacheEventConsumer : CacheEventConsumer<NewsComment>
     {
         /// <summary>
-        /// entity
+        /// Clear cache data
         /// </summary>
         /// <param name="entity">Entity</param>
-        /// <param name="entityEventType">Entity event type</param>
-        protected override void ClearCache(NewsComment entity, EntityEventType entityEventType)
+        protected override void ClearCache(NewsComment entity)
         {
-            if (entityEventType == EntityEventType.Delete)
-                RemoveByPrefix(NopNewsDefaults.NewsCommentsNumberPrefix, entity.NewsItemId);
-
-            base.ClearCache(entity, entityEventType);
+            RemoveByPrefix(NopNewsDefaults.NewsCommentsNumberPrefix, entity.NewsItemId);
         }
     }
 }
