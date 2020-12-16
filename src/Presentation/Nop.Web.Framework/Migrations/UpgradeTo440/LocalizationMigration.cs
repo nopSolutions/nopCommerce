431135f76cc95de23d55998217033a3c8f6d33b1
RomanovM <maxim.r@nopcommerce.com>
2020-12-16 14:54:22 +0200

    #5213 Further changes to add reference to premium support on log pages

--- src/Presentation/Nop.Web.Framework/Migrations/UpgradeTo440/LocalizationMigration.cs
+++ src/Presentation/Nop.Web.Framework/Migrations/UpgradeTo440/LocalizationMigration.cs
@@ -280,6 +280,10 @@ namespace Nop.Web.Framework.Migrations.UpgradeTo440
                 //#1730
                 ["Admin.Configuration.AppSettings.Installation.InstallRegionalResources"] = "Install regional resources",
                 ["Admin.Configuration.AppSettings.Installation.InstallRegionalResources.Hint"] = "Enable this setting to download and setup the regional language pack during installation.",
+
+                //#5213
+                ["Admin.System.Log.List.PremiumSupport"] = "Have questions or need help? Get dedicated support from the nopCommerce team with a guaranteed response within 24 hours. Please find more about our premium support services <a href=\"https://www.nopcommerce.com/nopcommerce-premium-support-services?utm_source=admin-panel&utm_medium=menu&utm_campaign=premium_support&utm_content=log-list\" target=\"_blank\">here</a>.",
+                ["Admin.System.Log.PremiumSupport"] = "Have questions or need help? Get dedicated support from the nopCommerce team with a guaranteed response within 24 hours. Please find more about our premium support services <a href=\"https://www.nopcommerce.com/nopcommerce-premium-support-services?utm_source=admin-panel&utm_medium=menu&utm_campaign=premium_support&utm_content=log-details\" target=\"_blank\">here</a>.",
             });
 
             // rename locales
