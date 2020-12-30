ae0731788477ffe6d69523e33aacb1b4a0ff474a
RomanovM <maxim.r@nopcommerce.com>
2020-12-30 11:08:15 +0200

    #5252 Returned using of StartsWith to detect the install page

--- src/Libraries/Nop.Services/Installation/InstallUrlMiddleware.cs
+++ src/Libraries/Nop.Services/Installation/InstallUrlMiddleware.cs
@@ -40,7 +40,7 @@ namespace Nop.Services.Installation
             if (!await DataSettingsManager.IsDatabaseInstalledAsync())
             {
                 var installUrl = $"{webHelper.GetStoreLocation()}{NopInstallationDefaults.InstallPath}";
-                if (!webHelper.GetThisPageUrl(false).Equals(installUrl, StringComparison.InvariantCultureIgnoreCase))
+                if (!webHelper.GetThisPageUrl(false).StartsWith(installUrl, StringComparison.InvariantCultureIgnoreCase))
                 {
                     //redirect
                     context.Response.Redirect(installUrl);
