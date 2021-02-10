a35e753345a2efdd052822d8d9cb2c072cdccd9c
bobac <4517566+bo-bac@users.noreply.github.com>
2021-02-10 17:52:53 +0200

    add SEO Title, Description and Keywords to the Home page

--- src/Presentation/Nop.Web/Controllers/HomeController.cs
+++ src/Presentation/Nop.Web/Controllers/HomeController.cs
@@ -1,12 +1,34 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Nop.Web.Factories;
 
 namespace Nop.Web.Controllers
 {
     public partial class HomeController : BasePublicController
     {
-        public virtual IActionResult Index()
+        #region Fields
+
+        private readonly ITopicModelFactory _topicModelFactory;
+
+        #endregion
+
+        #region Ctor
+
+        public HomeController(ITopicModelFactory topicModelFactory)
         {
-            return View();
+            _topicModelFactory = topicModelFactory;
         }
+
+        #endregion
+
+        #region Methods
+
+        public virtual async System.Threading.Tasks.Task<IActionResult> IndexAsync()
+        {
+            var model = await _topicModelFactory.PrepareTopicModelBySystemNameAsync("HomepageText");
+
+            return View(model);
+        }
+
+        #endregion
     }
 }
