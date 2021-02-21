1eb66dd32b39690e001c9b92fe3c1cb18e72bd6e
PWND <PWND@PWND.COM>
2021-02-22 00:58:32 +0400

    Refactor + support of multiple vendor chats

--- src/Plugins/Nop.Plugin.ExternalAuth.ExtendedAuth/Infrastructure/Cache/OrderPlacedEventConsumer.cs
+++ src/Plugins/Nop.Plugin.ExternalAuth.ExtendedAuth/Infrastructure/Cache/OrderPlacedEventConsumer.cs
@@ -11,27 +11,58 @@ using Telegram.Bot;
 using System.Security.Claims;
 using Nop.Services.Authentication.External;
 using System.Linq;
+using Nop.Core.Configuration;
+using Nop.Services.Orders;
 
 namespace Nop.Plugin.ExternalAuth.ExtendedAuth.Infrastructure.Cache
 {
+    struct VendorToChatMap
+    {
+        public int VendorId { get; set; }
+        public int ChatGroupId { get; set; }
+    }
+
     class OrderPlacedEventConsumer
         : IConsumer<OrderPlacedEvent>
     {
-        private readonly ITelegramBotClient _telegramBotClient;
+        private readonly Lazy<ITelegramBotClient> _telegramBotClient;
         private readonly ICustomerService _customerService;
         private readonly IExternalAuthenticationService _externalAuthenticationService;
-        private const int OrdersGroupId = -408158341;
+        private readonly NopConfig _config;
+        private readonly IOrderService _orderService;
 
-        public OrderPlacedEventConsumer(ITelegramBotClient telegramBotClient,
-            ICustomerService customerService, IExternalAuthenticationService externalAuthenticationService)
+        static private readonly List<VendorToChatMap> _vendorToChat = new List<VendorToChatMap>{
+            new VendorToChatMap{ VendorId = 1, ChatGroupId = -408158341 },  // StrEat Kitchen
+            new VendorToChatMap{ VendorId = 2, ChatGroupId = -407631866 }   // Root
+        };
+
+        public OrderPlacedEventConsumer(Lazy<ITelegramBotClient> telegramBotClient,
+            ICustomerService customerService, 
+            IExternalAuthenticationService externalAuthenticationService,
+            NopConfig config,
+            IOrderService orderService)
         {
             this._telegramBotClient = telegramBotClient;
             this._customerService = customerService;
             this._externalAuthenticationService = externalAuthenticationService;
+            this._config = config;
+            this._orderService = orderService;
         }
 
         public void HandleEvent(OrderPlacedEvent eventMessage)
         {
+            if (!_config.TelegramBotEnabled)
+                return;
+
+            var chatGroupsToNotify =
+                _vendorToChat.Where(x => 
+                    _orderService.GetOrderItems(eventMessage.Order.Id, vendorId: x.VendorId).Any())
+                    .Select(x => x.ChatGroupId)
+                    .ToList();
+
+            if (!chatGroupsToNotify.Any())
+                return;
+
             var customer = _customerService.GetCustomerById(eventMessage.Order.CustomerId);
             var externalRecord = _externalAuthenticationService.GetCustomerExternalAuthenticationRecords(customer).FirstOrDefault();
             var externalDisplayIdentifier =
@@ -41,9 +72,11 @@ namespace Nop.Plugin.ExternalAuth.ExtendedAuth.Infrastructure.Cache
             if(string.IsNullOrEmpty(externalDisplayIdentifier))
                 externalDisplayIdentifier = "Unknown";
 
-            System.Threading.Tasks.Task.Run(async () => {
-                var chat = await _telegramBotClient.GetChatAsync(OrdersGroupId);
-                await _telegramBotClient.SendTextMessageAsync(chat, $"New order from {externalDisplayIdentifier}");
+            chatGroupsToNotify.ForEach(chatGroupId => {
+                System.Threading.Tasks.Task.Run(async () => {
+                    var chat = await _telegramBotClient.Value.GetChatAsync(chatGroupId);
+                    await _telegramBotClient.Value.SendTextMessageAsync(chat, $"New order from {externalDisplayIdentifier}");
+                });
             });
         }
     }
--- src/Plugins/Nop.Plugin.ExternalAuth.ExtendedAuth/Infrastructure/WeekdayProductRotation.cs
+++ src/Plugins/Nop.Plugin.ExternalAuth.ExtendedAuth/Infrastructure/WeekdayProductRotation.cs
@@ -5,6 +5,7 @@ using Nop.Services.Catalog;
 using Nop.Services.Tasks;
 using System.Linq;
 using Nop.Services.Helpers;
+using Nop.Services.Logging;
 
 namespace Nop.Plugin.ExternalAuth.ExtendedAuth.Infrastructure
 {
@@ -17,16 +18,19 @@ namespace Nop.Plugin.ExternalAuth.ExtendedAuth.Infrastructure
         private readonly IScheduleTaskService _scheduleTaskService;
         private readonly IDateTimeHelper _dateTimeHelper;
         private readonly IProductService _productService;
+        private readonly ILogger _logger;
 
         public WeekdayProductRotation(ISpecificationAttributeService specificationAttribute,
             IScheduleTaskService scheduleTaskService, 
             IDateTimeHelper dateTimeHelper,
-            IProductService productService)
+            IProductService productService,
+            ILogger logger)
         {
             this._specificationAttribute = specificationAttribute;
             this._scheduleTaskService = scheduleTaskService;
             this._dateTimeHelper = dateTimeHelper;
             this._productService = productService;
+            this._logger = logger;
         }
 
         string GetNextWeekDay(DayOfWeek dayOfWeek)
@@ -40,17 +44,26 @@ namespace Nop.Plugin.ExternalAuth.ExtendedAuth.Infrastructure
         {
             var currentTask = _scheduleTaskService.GetTaskByType(PRODUCT_ROTATION_TASK);
             if (currentTask == null)
+            {
+                _logger.Warning("WeekdayProductRotation: Can't get current task info");
                 throw new Exception("Can't get current task info");
+            }
 
             var specificationAttributes = _specificationAttribute.GetSpecificationAttributes();
             var availableOnWeekdayAttribute = specificationAttributes.FirstOrDefault(x => x.Name.Equals("Available On Weekday"));
-            if(availableOnWeekdayAttribute == null)
+            if (availableOnWeekdayAttribute == null)
+            {
+                _logger.Warning("WeekdayProductRotation: Can't get available on weekday attribute");
                 throw new Exception("Can't get available on weekday attribute");
+            }
             
             var availableOnWeekdayOptions = 
                 _specificationAttribute.GetSpecificationAttributeOptionsBySpecificationAttribute(availableOnWeekdayAttribute.Id);
-            if(!availableOnWeekdayOptions.Any())
+            if (!availableOnWeekdayOptions.Any())
+            {
+                _logger.Warning("WeekdayProductRotation: Can't get options");
                 throw new Exception("Can't get options");
+            }
 
             var storeDateTime = _dateTimeHelper.ConvertToUserTime(DateTime.Now);
             var today3PMLocal = new DateTime(storeDateTime.Year,
@@ -75,8 +88,17 @@ namespace Nop.Plugin.ExternalAuth.ExtendedAuth.Infrastructure
                         .First(x => x.Name.Equals(targetWeekdayMenu))
                         .Id;
 
-                var allProducts = _productService.SearchProducts();
-                foreach(var product in allProducts)
+                _logger.Information(
+                    $"WeekdayProductRotation: Target weekday = {targetWeekdayMenu}, option id = {nextWeekDaySpecificationAttributeOptionId}");
+
+                var allProducts = _productService.SearchProducts(showHidden: true);
+
+                _logger.Information(
+                    $"WeekdayProductRotation: Found {allProducts.TotalCount} products, " +
+                    $"{allProducts.TotalPages} pages");
+
+                int unpublishedCount = 0, publishedCount = 0, untouchedCount = 0;
+                foreach (var product in allProducts)
                 {
                     var productSpecificationAttributes = 
                         _specificationAttribute.GetProductSpecificationAttributes(productId: product.Id);
@@ -87,16 +109,36 @@ namespace Nop.Plugin.ExternalAuth.ExtendedAuth.Infrastructure
                         if (productSpecificationAttributes.Any(x => 
                             x.SpecificationAttributeOptionId == nextWeekDaySpecificationAttributeOptionId))
                         {
+                            _logger.Information(
+                                $"WeekdayProductRotation: Publishing product {product.Name}");
                             product.Published = true;
+                            publishedCount++;
                         }
                         else
                         {
+                            _logger.Information(
+                                $"WeekdayProductRotation: Unpublishing product {product.Name}");
                             product.Published = false;
+                            unpublishedCount++;
                         }
 
                         _productService.UpdateProduct(product);
                     }
+                    else
+                    {
+                        _logger.Information(
+                            $"WeekdayProductRotation: No weekday specification attribute for product {product.Name}");
+                        untouchedCount++;
+                    }
                 }
+
+                _logger.Information($"Published {publishedCount}, " +
+                    $"unpublished {unpublishedCount}, " +
+                    $"left untouched {untouchedCount} products");
+            }
+            else
+            {
+                _logger.Information("WeekdayProductRotation: Skipping for now");
             }
         }
     }
