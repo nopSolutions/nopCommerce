f3d117116cd361da4a495286ff59a9b8d43c852a
skoshelev <sergey.k@nopcommerce.com>
2021-03-09 09:29:19 +0300

    #5504 Added an event to the " UpdateAsync" method

--- src/Libraries/Nop.Data/EntityRepository.cs
+++ src/Libraries/Nop.Data/EntityRepository.cs
@@ -383,6 +383,13 @@ namespace Nop.Data
                 return;
 
             await _dataProvider.UpdateEntitiesAsync(entities);
+
+            //event notification
+            if (!publishEvent)
+                return;
+
+            foreach (var entity in entities)
+                await _eventPublisher.EntityUpdatedAsync(entity);
         }
 
         /// <summary>
