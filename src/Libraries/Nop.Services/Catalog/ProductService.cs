63f080652d24b47b3e53f594f8c7296194855ba6
AndreiMaz <andrey.maz@gmail.com>
2021-03-09 15:52:41 +0300

    Minor refactoring and preparations for #5510

--- src/Libraries/Nop.Services/Catalog/ProductService.cs
+++ src/Libraries/Nop.Services/Catalog/ProductService.cs
@@ -230,6 +230,22 @@ namespace Nop.Services.Catalog
 
             string stockMessage;
 
+            /*TODO implement #5510
+            if (_catalogSettings.AttributeValueOutOfStockDisplayType == AttributeValueOutOfStockDisplayType.AlwaysDisplay)
+            {
+                //let's check whether all required attributes are already selected
+                //if some attribute is not selected, then return a "Products.Availability.SelectRequiredAttributes" locale 
+            }
+            else
+            {
+                //let's check whether all required attributes that could be selected are already selected
+
+                //note that it's possible that some required attribute is not selected yet, but its values are already disabled (not available)
+                //hence a customer cannot select it. in this case proceed to the logic below
+
+                //otherwise, return a "Products.Availability.SelectRequiredAttributes" locale
+            }*/
+
             var combination = await _productAttributeParser.FindProductAttributeCombinationAsync(product, attributesXml);
             if (combination != null)
             {
@@ -273,8 +289,7 @@ namespace Nop.Services.Catalog
                 }
                 else
                 {
-                    stockMessage = !(await _productAttributeService.GetProductAttributeMappingsByProductIdAsync(product.Id))
-                        .Any(pam => pam.IsRequired) ? await _localizationService.GetResourceAsync("Products.Availability.InStock") : await _localizationService.GetResourceAsync("Products.Availability.SelectRequiredAttributes");
+                    stockMessage = await _localizationService.GetResourceAsync("Products.Availability.InStock");
                 }
             }
 
