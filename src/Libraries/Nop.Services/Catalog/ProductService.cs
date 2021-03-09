c9df8760a22db63b39c01b195d58fa94ba8047ea
AndreiMaz <andrey.maz@gmail.com>
2021-03-09 14:58:36 +0300

    Minor refactoring

--- src/Libraries/Nop.Services/Catalog/ProductService.cs
+++ src/Libraries/Nop.Services/Catalog/ProductService.cs
@@ -215,7 +215,7 @@ namespace Nop.Services.Catalog
         }
 
         /// <summary>
-        /// Get stock message
+        /// Get stock message for a product with attributes
         /// </summary>
         /// <param name="product">Product</param>
         /// <param name="attributesXml">Attributes in XML format</param>
@@ -223,7 +223,7 @@ namespace Nop.Services.Catalog
         /// A task that represents the asynchronous operation
         /// The task result contains the message
         /// </returns>
-        protected virtual async Task<string> GeStockMessageAsync(Product product, string attributesXml)
+        protected virtual async Task<string> GetStockMessageForAttributesAsync(Product product, string attributesXml)
         {
             if (!product.DisplayStockAvailability)
                 return string.Empty;
@@ -285,16 +285,16 @@ namespace Nop.Services.Catalog
         /// Get stock message
         /// </summary>
         /// <param name="product">Product</param>
-        /// <param name="stockMessage">Message</param>
         /// <returns>
         /// A task that represents the asynchronous operation
         /// The task result contains the message
         /// </returns>
-        protected virtual async Task<string> GetStockMessageAsync(Product product, string stockMessage)
+        protected virtual async Task<string> GetStockMessageAsync(Product product)
         {
             if (!product.DisplayStockAvailability)
                 return string.Empty;
 
+            var stockMessage = string.Empty;
             var stockQuantity = await GetTotalStockQuantityAsync(product);
             if (stockQuantity > 0)
             {
@@ -1450,10 +1450,10 @@ namespace Nop.Services.Catalog
             switch (product.ManageInventoryMethod)
             {
                 case ManageInventoryMethod.ManageStock:
-                    stockMessage = await GetStockMessageAsync(product, stockMessage);
+                    stockMessage = await GetStockMessageAsync(product);
                     break;
                 case ManageInventoryMethod.ManageStockByAttributes:
-                    stockMessage = await GeStockMessageAsync(product, attributesXml);
+                    stockMessage = await GetStockMessageForAttributesAsync(product, attributesXml);
                     break;
             }
 
