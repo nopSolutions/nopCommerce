468c68628d07191387bf2ca962b88dd1f28726df
Jadyn <jadyn.wong@live.com>
2021-04-26 10:44:39 +0800

    Fix: ResourcesLocker GetLocker

--- src/Libraries/Nop.Core/ComponentModel/ResourcesLocker.cs
+++ src/Libraries/Nop.Core/ComponentModel/ResourcesLocker.cs
@@ -11,17 +11,13 @@ namespace Nop.Core.ComponentModel
 {
     public static class ResourcesLocker
     {
-        private static readonly ConcurrentDictionary<string, Lazy<AsyncLock>> _lockerItems = new ConcurrentDictionary<string, Lazy<AsyncLock>>();
+        private static readonly ConcurrentDictionary<string, AsyncLock> _lockerItems = new();
 
         public static AsyncLock GetLocker(string resource)
         {
-            return _lockerItems.GetOrAdd(resource, key =>
-            {
-                return new Lazy<AsyncLock>(() =>
-                {
-                    return new AsyncLock();
-                }, true);
-            }).Value;
+            _lockerItems.TryAdd(resource, new AsyncLock());
+
+            return _lockerItems[resource];
         }
     }
 }
