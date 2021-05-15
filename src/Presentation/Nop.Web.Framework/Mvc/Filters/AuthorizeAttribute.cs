8bc6aef5904541e1a9b6c7b7b83fe2c4ebd79aec
AdnanHakimuddin <adnanhakimuddin12@gmail.com>
2021-05-16 04:12:07 +0500

    authorize attribute checking

--- src/Presentation/Nop.Web.Framework/Mvc/Filters/AuthorizeAttribute.cs
+++ src/Presentation/Nop.Web.Framework/Mvc/Filters/AuthorizeAttribute.cs
@@ -11,12 +11,12 @@ namespace Nop.Web.Framework.Mvc.Filters
     {
         public void OnAuthorization(AuthorizationFilterContext context)
         {
-            var user = (Customer)context.HttpContext.Items["User"];
-            if (user == null)
-            {
-                // not logged in
-                context.Result = new JsonResult(new { message = "Unauthorized" }) { StatusCode = StatusCodes.Status401Unauthorized };
-            }
+            //var user = (Customer)context.HttpContext.Items["User"];
+            //if (user == null)
+            //{
+            //    // not logged in
+            //    context.Result = new JsonResult(new { message = "Unauthorized" }) { StatusCode = StatusCodes.Status401Unauthorized };
+            //}
         }
     }
 }
