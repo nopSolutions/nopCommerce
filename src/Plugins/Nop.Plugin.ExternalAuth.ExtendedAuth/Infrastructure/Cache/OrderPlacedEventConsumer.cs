227e57071e02e38eb5c1f83d8612fe8e39355ca9
PWND <PWND@PWND.COM>
2021-06-27 23:59:17 +0400

    refreshed telegram chat ids

--- src/Plugins/Nop.Plugin.ExternalAuth.ExtendedAuth/Infrastructure/Cache/OrderPlacedEventConsumer.cs
+++ src/Plugins/Nop.Plugin.ExternalAuth.ExtendedAuth/Infrastructure/Cache/OrderPlacedEventConsumer.cs
@@ -19,7 +19,7 @@ namespace Nop.Plugin.ExternalAuth.ExtendedAuth.Infrastructure.Cache
     struct VendorToChatMap
     {
         public int VendorId { get; set; }
-        public int ChatGroupId { get; set; }
+        public long ChatGroupId { get; set; }
     }
 
     class OrderPlacedEventConsumer
@@ -32,10 +32,10 @@ namespace Nop.Plugin.ExternalAuth.ExtendedAuth.Infrastructure.Cache
         private readonly IOrderService _orderService;
 
         static private readonly List<VendorToChatMap> _vendorToChat = new List<VendorToChatMap>{
-            new VendorToChatMap{ VendorId = 1, ChatGroupId = -408158341 },  // StrEat Kitchen
-            new VendorToChatMap{ VendorId = 2, ChatGroupId = -407631866 },  // Root
-            new VendorToChatMap{ VendorId = 4, ChatGroupId = -558910079 },  // 33 Pizzas
-            new VendorToChatMap{ VendorId = 6, ChatGroupId = -403350589 }   // Kaufmann
+            new VendorToChatMap{ VendorId = 1, ChatGroupId = -1001213896599 },  // StrEat Kitchen
+            new VendorToChatMap{ VendorId = 2, ChatGroupId = -1001440235406 },  // Root
+            new VendorToChatMap{ VendorId = 4, ChatGroupId = -1001490513385 },  // 33 Pizzas
+            new VendorToChatMap{ VendorId = 6, ChatGroupId = -1001378455507 }   // Kaufmann
         };
 
         public OrderPlacedEventConsumer(Lazy<ITelegramBotClient> telegramBotClient,
