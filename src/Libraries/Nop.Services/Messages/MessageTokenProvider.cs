5e0743c818b0e9575e548c1c6f20401f500f1df6
Jaber <mhsjaber@gmail.com>
2022-02-02 22:10:21 +0600

    instagram added to social media links settings

--- src/Libraries/Nop.Services/Messages/MessageTokenProvider.cs
+++ src/Libraries/Nop.Services/Messages/MessageTokenProvider.cs
@@ -941,6 +941,7 @@ namespace Nop.Services.Messages
             tokens.Add(new Token("Facebook.URL", _storeInformationSettings.FacebookLink));
             tokens.Add(new Token("Twitter.URL", _storeInformationSettings.TwitterLink));
             tokens.Add(new Token("YouTube.URL", _storeInformationSettings.YoutubeLink));
+            tokens.Add(new Token("Instagram.URL", _storeInformationSettings.InstagramLink));
 
             //event notification
             await _eventPublisher.EntityTokensAddedAsync(store, tokens);
