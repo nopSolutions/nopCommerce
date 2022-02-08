f2273eaf72b283fe7be9e8649f65992c531b1fe0
Romanov Maksim <maxim.r@nopcommerce.com>
2022-02-08 18:48:04 +0200

    Update SettingMigration.cs

--- src/Presentation/Nop.Web.Framework/Migrations/UpgradeTo460/SettingMigration.cs
+++ src/Presentation/Nop.Web.Framework/Migrations/UpgradeTo460/SettingMigration.cs
@@ -1,13 +1,9 @@
 ﻿using FluentMigrator;
-using Nop.Core.Domain.Common;
 using Nop.Core.Domain.Catalog;
-using Nop.Core.Domain.Configuration;
-using Nop.Core.Domain.Orders;
 using Nop.Core.Infrastructure;
 using Nop.Data;
 using Nop.Data.Migrations;
 using Nop.Services.Configuration;
-using Nop.Core.Domain.Shipping;
 
 namespace Nop.Web.Framework.Migrations.UpgradeTo460
 {
