410996e4582a4103f982bced84feb7693c17fe51
RomanovM <maxim.r@nopcommerce.com>
2022-03-24 15:30:33 +0200

    #5809 Fix of tests

--- src/Tests/Nop.Tests/Nop.Services.Tests/ScheduleTasks/ScheduleTaskServiceTests.cs
+++ src/Tests/Nop.Tests/Nop.Services.Tests/ScheduleTasks/ScheduleTaskServiceTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
 using FluentAssertions;
 using Nop.Core.Domain.ScheduleTasks;
@@ -34,6 +35,82 @@ namespace Nop.Tests.Nop.Services.Tests.ScheduleTasks
             await _scheduleTaskService.DeleteTaskAsync(_task);
         }
 
+        #region CRUD tests
+
+        [Test]
+        public async Task CanInsertAndGetTask()
+        {
+            _task.Id = 0;
+            await _scheduleTaskService.InsertTaskAsync(_task);
+            var task = await _scheduleTaskService.GetTaskByIdAsync(_task.Id);
+            await _scheduleTaskService.DeleteTaskAsync(_task);
+
+            _task.Id.Should().NotBe(0);
+            task.Id.Should().Be(_task.Id);
+            task.Name.Should().Be(_task.Name);
+        }
+
+        [Test]
+        public void InsertTaskShouldRaiseExceptionIfTaskIsNull()
+        {
+            Assert.Throws<AggregateException>(() =>
+                 _scheduleTaskService.InsertTaskAsync(null).Wait());
+        }
+
+        [Test]
+        public async Task GetTaskByIdShouldReturnNullIfTaskIdIsZero()
+        {
+            var task = await _scheduleTaskService.GetTaskByIdAsync(0);
+            task.Should().BeNull();
+        }
+
+        [Test]
+        public async Task GetTaskByIdShouldReturnNullIfTaskIdIsNotExists()
+        {
+            var task = await _scheduleTaskService.GetTaskByIdAsync(int.MaxValue);
+            task.Should().BeNull();
+        }
+
+        [Test]
+        public async Task CanUpdateTask()
+        {
+            _task.Id = 0;
+            await _scheduleTaskService.InsertTaskAsync(_task);
+            var task = await _scheduleTaskService.GetTaskByIdAsync(_task.Id);
+            task.Name = "new test name";
+            await _scheduleTaskService.UpdateTaskAsync(task);
+            var task2 = await _scheduleTaskService.GetTaskByIdAsync(_task.Id);
+            await _scheduleTaskService.DeleteTaskAsync(_task);
+
+            task.Id.Should().Be(task2.Id);
+            task2.Name.Should().NotBe(_task.Name);
+        }
+
+        [Test]
+        public void UpdateTaskShouldRaiseExceptionIfTaskIsNull()
+        {
+            Assert.Throws<AggregateException>(() =>
+                _scheduleTaskService.UpdateTaskAsync(null).Wait());
+        }
+
+        public async Task CanDeleteTask()
+        {
+            _task.Id = 0;
+            await _scheduleTaskService.InsertTaskAsync(_task);
+            await _scheduleTaskService.DeleteTaskAsync(_task);
+            var task = await _scheduleTaskService.GetTaskByIdAsync(_task.Id);
+            task.Should().BeNull();
+        }
+
+        [Test]
+        public void DeleteTaskShouldRaiseExceptionIfTaskIsNull()
+        {
+            Assert.Throws<AggregateException>(() =>
+                _scheduleTaskService.DeleteTaskAsync(null).Wait());
+        }
+
+        #endregion
+
         [Test]
         public async Task CanGetTaskByType()
         {
@@ -67,7 +144,7 @@ namespace Nop.Tests.Nop.Services.Tests.ScheduleTasks
 
             tasks = await _scheduleTaskService.GetAllTasksAsync(true);
 
-            tasks.Count.Should().Be(7);
+            tasks.Count.Should().Be(8);
             tasks.Any(p => p.Enabled).Should().BeTrue();
             tasks.Any(p => p.Id == _task.Id).Should().BeTrue();
         }
