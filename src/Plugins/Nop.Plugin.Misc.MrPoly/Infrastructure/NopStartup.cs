b536b6354cacf689d85d3c07230cbe052d291f83
Greg Taylor <greg.taylor@ignitionls.com>
2022-04-27 18:42:06 +0930

    Replaced DependencyRegistrar.cs with NopStartup.cs

---
+++ src/Plugins/Nop.Plugin.Misc.MrPoly/Infrastructure/NopStartup.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Nop.Core.Infrastructure;
+using Nop.Plugin.Misc.MrPoly.Catalog;
+using Nop.Services.Catalog;
+
+namespace Nop.Plugin.Misc.MrPoly.Infrastructure
+{
+    public class NopStartup : INopStartup
+    {
+        public int Order => 2;
+
+        public void Configure(IApplicationBuilder application)
+        {
+            
+        }
+
+        public void ConfigureServices(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddTransient<IProductService, PolyProductService>();
+        }
+    }
+}
