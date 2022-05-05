1ae165075c7de4fe6477c4c83adea25b177b22ec
Jan Hlavac <jan@postsharp.net>
2022-05-05 11:54:43 +0200

    #30271 Integrated PostSharp.Engineering and Metalama.

---
+++ Build.ps1
@@ -0,0 +1,9 @@
+if ( $env:VisualStudioVersion -eq $null ) {
+    Import-Module "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\Common7\Tools\Microsoft.VisualStudio.DevShell.dll"
+    Enter-VsDevShell -VsInstallPath "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\" -StartInPath $(Get-Location)
+}
+
+(& dotnet nuget locals http-cache -c) | Out-Null
+& dotnet run --project "$PSScriptRoot\eng\src\BuildMetalamaTestsNopCommerce.csproj" -- $args
+exit $LASTEXITCODE
+
---
+++ eng/src/Program.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) SharpCrafters s.r.o. All rights reserved.
+// This project is not open source. Please see the LICENSE.md file in the repository root for details.
+
+using PostSharp.Engineering.BuildTools;
+using PostSharp.Engineering.BuildTools.Build.Model;
+using PostSharp.Engineering.BuildTools.Build.Solutions;
+using PostSharp.Engineering.BuildTools.Dependencies.Model;
+using Spectre.Console.Cli;
+
+var productDependencyDefinition =
+    new DependencyDefinition("Metalama.Tests.NopCommerce", VcsProvider.GitHub, "Metalama", false);
+
+var product = new Product(productDependencyDefinition)
+{
+    ProductName = productDependencyDefinition.Name,
+    Solutions = new Solution[]
+    {
+        new DotNetSolution("src\\NopCommerce.sln"),
+    },
+    PublicArtifacts = Pattern.Create("Nop.Web.Framework.4.5.0.nupkg"),
+    Dependencies = new[] { Dependencies.PostSharpEngineering, Dependencies.Metalama },
+};
+
+var commandApp = new CommandApp();
+
+commandApp.AddProductCommands( product );
+
+return commandApp.Run( args );
