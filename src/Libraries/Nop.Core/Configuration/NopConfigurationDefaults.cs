b45bd3fa0fbac6e82eda9bce0785aa678f4cefda
RomanovM <maxim.r@nopcommerce.com>
2022-05-19 11:50:19 +0200

    #6260 Added support of environment specific appsettings.json files

--- src/Libraries/Nop.Core/Configuration/NopConfigurationDefaults.cs
+++ src/Libraries/Nop.Core/Configuration/NopConfigurationDefaults.cs
@@ -9,5 +9,11 @@
         /// Gets the path to file that contains app settings
         /// </summary>
         public static string AppSettingsFilePath => "App_Data/appsettings.json";
+
+        /// <summary>
+        /// Gets the path to file that contains app settings for specific hosting environment
+        /// </summary>
+        /// <remarks>0 - Environment name</remarks>
+        public static string AppSettingsEnvironmentFilePath => "App_Data/appsettings.{0}.json";
     }
 }
