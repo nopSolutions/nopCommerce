5f567173e16bf49bd1914da130727e3ef529def1
Gherman Adrian <gherman.adrian2001@gmail.com>
2022-05-26 10:02:28 +0300

    Added WebhookSetting

--- src/Presentation/Nop.Web/Areas/Admin/Models/Webhook/WebhookSettingModel.cs
+++ src/Presentation/Nop.Web/Areas/Admin/Models/Webhook/WebhookSettingModel.cs
@@ -3,12 +3,11 @@ using Nop.Web.Framework.Mvc.ModelBinding;
 
 namespace Nop.Web.Areas.Admin.Models.Webhook;
 
-public record WebhookSettingModel : BaseNopEntityModel, ISettingsModel
+public record WebhookSettingModel : BaseNopModel, ISettingsModel
 {
     public int ActiveStoreScopeConfiguration { get; set; }
     
-    [NopResourceDisplayName("Admin.Configuration.Settings.News.Enabled")]
-
+    [NopResourceDisplayName("Admin.Configuration.Settings.Webhooks.Enabled")]
     
     public bool ConfigurationEnabled { get; set; }
     
