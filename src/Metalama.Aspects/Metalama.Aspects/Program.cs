2bf7938ae3063c68de5f587f7bcdf754b0bd83cf
Jan Hlavac <jan@postsharp.net>
2022-07-21 16:17:26 +0200

    #30637 Added Fabric with OverrideMethod.

---
+++ src/Metalama.Aspects/Metalama.Aspects/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Metalama.Aspects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
