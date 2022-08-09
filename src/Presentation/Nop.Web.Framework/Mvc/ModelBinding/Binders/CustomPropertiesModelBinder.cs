56a5eb9677644c29d79a891ba483ec3f5beb7ef7
DmitriyKulagin <kulagin87@gmail.com>
2022-08-09 11:35:02 +0300

    #6361 Fixed model bindind for BaseNopModel.CustomProperties[Obsolete]

---
+++ src/Presentation/Nop.Web.Framework/Mvc/ModelBinding/Binders/CustomPropertiesModelBinder.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace Nop.Web.Framework.Mvc.ModelBinding.Binders
+{
+    /// <summary>
+    /// Represents model binder for CustomProperties
+    /// </summary>
+    [Obsolete]
+    public class CustomPropertiesModelBinder : IModelBinder
+    {
+        Task IModelBinder.BindModelAsync(ModelBindingContext bindingContext)
+        {
+            if (bindingContext == null)
+                throw new ArgumentNullException(nameof(bindingContext));
+
+            var modelName = bindingContext.ModelName;
+
+            var result = new Dictionary<string, object>();
+            if (bindingContext.HttpContext.Request.Method == "POST")
+            {
+                var keys = bindingContext.HttpContext.Request.Form.Keys.ToList().Where(x => x.IndexOf(modelName) == 0);
+
+                if (keys != null && keys.Any())
+                {
+                    foreach (var key in keys)
+                    {
+                        var dicKey = key.Replace(modelName + "[", "").Replace("]", "");
+                        bindingContext.HttpContext.Request.Form.TryGetValue(key, out var value);
+                        result.Add(dicKey, value.ToString());
+                    }
+                }
+            }
+            if (bindingContext.HttpContext.Request.Method == "GET")
+            {
+                var keys = bindingContext.HttpContext.Request.QueryString.Value.Split('&').Where(x => x.StartsWith(modelName));
+
+                if (keys != null && keys.Any())
+                {
+                    foreach (var key in keys)
+                    {
+                        var dicKey = key[(key.IndexOf("[") + 1)..key.IndexOf("]")];
+                        var value = key[(key.IndexOf("=") + 1)..];
+
+                        result.Add(dicKey, value);
+                    }
+                }
+            }
+            bindingContext.Result = ModelBindingResult.Success(result);
+            
+            return Task.CompletedTask;
+
+        }
+    }
+}
