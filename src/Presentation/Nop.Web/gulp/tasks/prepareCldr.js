d4b18d527815f3b62842b41685dd678046e08b85
AlexeyA <exile.developer@gmail.com>
2022-08-15 16:52:47 +0300

    #196 Update some packages to the latest version

--- src/Presentation/Nop.Web/gulp/tasks/prepareCldr.js
+++ src/Presentation/Nop.Web/gulp/tasks/prepareCldr.js
@@ -1,10 +1,10 @@
-﻿const del = require('del');
-const gulp = require('gulp');
-const zip = require('gulp-zip');
+﻿import { deleteAsync } from 'del';
+import gulp from 'gulp';
+import zip from 'gulp-zip';
 
 //Clear before ZIP
 function pre_del_cldr(cb) {
-  return del(['./wwwroot/lib_npm/cldr-data/main/**/*.json',
+  return deleteAsync(['./wwwroot/lib_npm/cldr-data/main/**/*.json',
     '!./wwwroot/lib_npm/cldr-data/main/**/{ca-gregorian,currencies,numbers,timeZoneNames}.json',
     '!./wwwroot/lib_npm/cldr-data/main/*.zip']).then(() => {
       cb()
@@ -12,23 +12,23 @@ function pre_del_cldr(cb) {
 }
 
 function makeMainZip(cb) {
-  del(['./wwwroot/lib_npm/cldr-data/**/*',
+  deleteAsync(['./wwwroot/lib_npm/cldr-data/**/*',
     '!./wwwroot/lib_npm/cldr-data/main',
     '!./wwwroot/lib_npm/cldr-data/main/**/{ca-gregorian,currencies,numbers,timeZoneNames}.json',
     '!./wwwroot/lib_npm/cldr-data/supplemental',
     '!./wwwroot/lib_npm/cldr-data/main/*.zip']).then(() => {
-    gulp.src(['./wwwroot/lib_npm/cldr-data/main/**'])
+      gulp.src(['./wwwroot/lib_npm/cldr-data/main/**'])
       .pipe(zip('main.zip'))
       .pipe(gulp.dest('./wwwroot/lib_npm/cldr-data/main/')).on('end', cb)
   }).catch(cb)
 }
 
 function del_cldr(cb) {
-  return del(['./wwwroot/lib_npm/cldr-data/main/**',
+  return deleteAsync(['./wwwroot/lib_npm/cldr-data/main/**',
     '!./wwwroot/lib_npm/cldr-data/main/en',
     '!./wwwroot/lib_npm/cldr-data/main/*.zip']).then(() => {
       cb()
     })
 }
 
-exports.Execute = gulp.series(pre_del_cldr, makeMainZip, del_cldr);
+export const prepareCldr = gulp.series(pre_del_cldr, makeMainZip, del_cldr);
--- src/Presentation/Nop.Web/wwwroot/lib_npm/globalize/globalize.js
+++ src/Presentation/Nop.Web/wwwroot/lib_npm/globalize/globalize.js
@@ -1,16 +1,16 @@
 /**
- * Globalize v1.6.0
+ * Globalize v1.7.0
  *
- * http://github.com/jquery/globalize
+ * https://github.com/globalizejs/globalize
  *
- * Copyright jQuery Foundation and other contributors
+ * Copyright OpenJS Foundation and other contributors
  * Released under the MIT license
- * http://jquery.org/license
+ * https://jquery.org/license
  *
- * Date: 2020-09-08T11:54Z
+ * Date: 2021-08-02T11:53Z
  */
 /*!
- * Globalize v1.6.0 2020-09-08T11:54Z Released under the MIT license
+ * Globalize v1.7.0 2021-08-02T11:53Z Released under the MIT license
  * http://git.io/TrdQbw
  */
 (function( root, factory ) {
@@ -185,7 +185,7 @@ var partsJoin = function( parts ) {
 
 
 var runtimeStringify = function( args ) {
-	return JSON.stringify( args, function( key, value ) {
+	return JSON.stringify( args, function( _key, value ) {
 		if ( value && value.runtimeKey ) {
 			return value.runtimeKey;
 		}
