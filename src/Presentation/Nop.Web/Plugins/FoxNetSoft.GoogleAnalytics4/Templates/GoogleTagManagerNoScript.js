4ee2e654427606009661c4b70d76c6be0de49f53
Dave Farinelli <d@dfar.io>
2022-11-07 17:41:08 +0000

    Adds GA4 plugin

---
+++ src/Presentation/Nop.Web/Plugins/FoxNetSoft.GoogleAnalytics4/Templates/GoogleTagManagerNoScript.js
@@ -0,0 +1,3 @@
+﻿<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=%MerchantId%" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
