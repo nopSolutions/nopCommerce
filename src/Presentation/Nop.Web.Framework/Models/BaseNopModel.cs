bd4e30903771b18e2fb2a3dc4bb03b4854357dde
Sergey Koshelev <sergey.k@nopcommerce.com>
2022-12-20 12:02:13 +0300

    Revert "Removed the BaseNopModel.CustomProperties property"
    This reverts commit bea6cbb6d507fd5fbfa4d2e9c760bc142d78e0c3.

--- src/Presentation/Nop.Web.Framework/Models/BaseNopModel.cs
+++ src/Presentation/Nop.Web.Framework/Models/BaseNopModel.cs
@@ -1,4 +1,9 @@
-﻿namespace Nop.Web.Framework.Models
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace Nop.Web.Framework.Models
 {
     /// <summary>
     /// Represents base nopCommerce model
@@ -10,15 +15,26 @@
         /// <summary>
         /// Ctor
         /// </summary>
+        [Obsolete]
         public BaseNopModel()
         {
+            CustomProperties = new Dictionary<string, object>();
             PostInitialize();
         }
 
         #endregion
 
         #region Methods
-        
+
+        /// <summary>
+        /// Perform additional actions for binding the model
+        /// </summary>
+        /// <param name="bindingContext">Model binding context</param>
+        /// <remarks>Developers can override this method in custom partial classes in order to add some custom model binding</remarks>
+        public virtual void BindModel(ModelBindingContext bindingContext)
+        {
+        }
+
         /// <summary>
         /// Perform additional actions for the model initialization
         /// </summary>
@@ -28,5 +44,21 @@
         }
 
         #endregion
+
+        #region Properties
+
+        ////MVC is suppressing further validation if the IFormCollection is passed to a controller method. That's why we add it to the model
+        //[XmlIgnore]
+        //public IFormCollection Form { get; set; }
+
+        /// <summary>
+        /// Gets or sets property to store any custom values for models 
+        /// </summary>
+        [XmlIgnore]
+        [Obsolete]
+        public Dictionary<string, object> CustomProperties { get; set; }
+
+        #endregion
+
     }
 }
