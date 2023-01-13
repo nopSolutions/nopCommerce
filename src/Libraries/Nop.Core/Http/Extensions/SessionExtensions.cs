6dcf71d444a2cdfaf1f5c4956f1ec4c5fcf8d119
ductv <ductv@fabbi.com.vn>
2023-01-13 13:59:35 +0700

    Add async version of Get, Set and Remove methods for SessionExtensions

--- src/Libraries/Nop.Core/Http/Extensions/SessionExtensions.cs
+++ src/Libraries/Nop.Core/Http/Extensions/SessionExtensions.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Newtonsoft.Json;
 
 namespace Nop.Core.Http.Extensions
@@ -34,6 +35,62 @@ namespace Nop.Core.Http.Extensions
                 return default;
 
             return JsonConvert.DeserializeObject<T>(value);
+        }
+
+        /// <summary>
+        /// Remove value from Session
+        /// </summary>
+        /// <param name="session">Session</param>
+        /// <param name="key">Key</param>
+        public static void Remove(this ISession session, string key)
+        {
+            if (session.TryGetValue(key, out var _))
+                session.Remove(key);
+        }
+
+        /// <summary>
+        /// Set value to Session
+        /// </summary>
+        /// <typeparam name="T">Type of value</typeparam>
+        /// <param name="session">Session</param>
+        /// <param name="key">Key</param>
+        /// <param name="value">Value</param>
+        /// <returns>A task that represents the asynchronous operation</returns>
+        public static Task SetAsync<T>(this ISession session, string key, T value)
+        {
+            session.SetString(key, JsonConvert.SerializeObject(value));
+            return Task.CompletedTask;
+        }
+
+        /// <summary>
+        /// Get value from session
+        /// </summary>
+        /// <typeparam name="T">Type of value</typeparam>
+        /// <param name="session">Session</param>
+        /// <param name="key">Key</param>
+        /// <returns>Value</returns>
+        /// <returns>A task that represents the asynchronous operation</returns>
+        public static Task<T> GetAsync<T>(this ISession session, string key)
+        {
+            var value = session.GetString(key);
+            if (value == null)
+                return default;
+
+            return Task.FromResult(JsonConvert.DeserializeObject<T>(value));
+        }
+
+        /// <summary>
+        /// Remove value from Session
+        /// </summary>
+        /// <param name="session">Session</param>
+        /// <param name="key">Key</param>
+        /// <returns>A task that represents the asynchronous operation</returns>
+        public static Task RemoveAsync(this ISession session, string key)
+        {
+            if (session.TryGetValue(key, out var _))
+                session.Remove(key);
+
+            return Task.CompletedTask;
         }
     }
 }
