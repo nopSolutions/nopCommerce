d0b9017b9c3446049611f8285936856ccab57f86
Sergey Koshelev <sergey.k@nopcommerce.com>
2023-03-07 15:47:53 +0300

    #6631 Added address validator for the CustomerAddressModel model

---
+++ src/Presentation/Nop.Web/Areas/Admin/Validators/Customers/CustomerAddressValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using Nop.Core.Domain.Common;
+using Nop.Data.Mapping;
+using Nop.Services.Localization;
+using Nop.Web.Areas.Admin.Models.Customers;
+using Nop.Web.Areas.Admin.Validators.Common;
+
+namespace Nop.Web.Areas.Admin.Validators.Customers
+{
+    public partial class CustomerAddressValidator : AbstractValidator<CustomerAddressModel>
+    {
+        public CustomerAddressValidator(ILocalizationService localizationService,
+            IMappingEntityAccessor mappingEntityAccessor,
+            AddressSettings addressSettings)
+        {
+            RuleFor(model => model.Address).SetValidator(new AddressValidator(addressSettings, localizationService, mappingEntityAccessor));
+        }
+    }
+}
