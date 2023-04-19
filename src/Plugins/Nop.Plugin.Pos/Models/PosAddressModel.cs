4560e9c346633ae783188acf5de050e2a7635807
Mukesh Karthikeyan <Mukesh.K@datayaan.com>
2023-04-19 11:58:00 +0530

    POS_Plugin

---
+++ src/Plugins/Nop.Plugin.Pos/Models/PosAddressModel.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Nop.Core.Domain.Common;
+using Nop.Web.Framework.Models;
+using Nop.Web.Framework.Mvc.ModelBinding;
+
+namespace Nop.Plugin.Pos.Models
+{
+    public record PosAddressModel : BaseNopModel
+    {
+        public Address posaddress { get; set; }
+        public string Country { get; set; }
+
+        public string state { get; set; }
+    }
+}
