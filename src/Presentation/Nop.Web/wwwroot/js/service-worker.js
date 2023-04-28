47dc5f84ae4044b5b6e496f794d4cc233c8d624c
Mukesh Karthikeyan <Mukesh.K@datayaan.com>
2023-04-28 16:01:49 +0530

    PWA

---
+++ src/Presentation/Nop.Web/wwwroot/js/service-worker.js
@@ -0,0 +1,22 @@
+﻿// Cache static assets
+self.addEventListener('install', function (event) {
+  event.waitUntil(
+    caches.open('your-cache-name').then(function (cache) {
+      return cache.addAll([
+        '/css/style.css',
+        '/js/script.js',
+        '/images/logo.png'
+        // Add more static assets to cache
+      ]);
+    })
+  );
+});
+
+// Serve cached assets when offline
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      return response || fetch(event.request);
+    })
+  );
+});
