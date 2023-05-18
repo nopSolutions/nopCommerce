e8135901fdce41af304322d8e1f61f6e3a859485
Praveen Matoria <praveen.matoria@gmail.com>
2023-05-18 15:12:07 +0530

    #6693
    Rename "Sendinblue" to "Brevo"

---
+++ src/Plugins/Nop.Plugin.Misc.Brevo/Domain/BrevoAccountLanguage.cs
@@ -0,0 +1,38 @@
+﻿namespace Nop.Plugin.Misc.Brevo.Domain
+{
+    /// <summary>
+    /// Represents Brevo account language enumeration
+    /// </summary>
+    public enum BrevoAccountLanguage
+    {
+        /// <summary>
+        /// English (default)
+        /// </summary>
+        English = 0,
+
+        /// <summary>
+        /// French
+        /// </summary>
+        French = 1,
+
+        /// <summary>
+        /// Italian
+        /// </summary>
+        Italian = 2,
+
+        /// <summary>
+        /// Spanish
+        /// </summary>
+        Spanish = 3,
+
+        /// <summary>
+        /// German
+        /// </summary>
+        German = 4,
+
+        /// <summary>
+        /// Portuguese
+        /// </summary>
+        Portuguese = 5
+    }
+}
