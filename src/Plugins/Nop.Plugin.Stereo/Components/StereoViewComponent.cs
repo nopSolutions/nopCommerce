6c1ee7131b9d8a9cb4b77f9394f9969264f875d1
Mukesh Karthikeyan <Mukesh.K@datayaan.com>
2023-05-24 07:52:21 +0530

    Stereo_Plugin

---
+++ src/Plugins/Nop.Plugin.Stereo/Components/StereoViewComponent.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Nop.Web.Framework.Components;
+
+namespace Nop.Plugin.Stereo.Components
+{
+    [ViewComponent(Name = "Stereo")]
+    public class StereoViewComponent : NopViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View("~/Plugins/Stereo/Views/OpenStore.cshtml");
+        }
+    }
+}
