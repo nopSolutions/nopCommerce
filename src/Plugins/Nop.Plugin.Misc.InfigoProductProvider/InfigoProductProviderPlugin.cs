beb57c2e5b00906a57de337d464d81049185f41d
gabrielplacinta1 <gabiplacinta77@gmail.com>
2023-06-06 11:56:29 +0300

    Added plugin to solution

---
+++ src/Plugins/Nop.Plugin.Misc.InfigoProductProvider/InfigoProductProviderPlugin.cs
@@ -0,0 +1,9 @@
+﻿using Nop.Services.Common;
+using Nop.Services.Plugins;
+
+namespace Nop.Plugin.Misc.InfigoProductProvider;
+
+public class InfigoProductProviderPlugin : BasePlugin, IMiscPlugin
+{
+    
+}
