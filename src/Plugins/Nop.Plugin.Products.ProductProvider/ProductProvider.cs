25b81c585af65b7678fc7de33f8049ba076214f5
Virgiliu Plesca <virgiliu.plesca@iis.utm.md>
2023-06-06 12:39:01 +0300

    plugin can be installed

--- src/Plugins/Nop.Plugin.Products.ProductProvider/ProductProvider.cs
+++ src/Plugins/Nop.Plugin.Products.ProductProvider/ProductProvider.cs
@@ -1,34 +1,8 @@
-﻿using System;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Nop.Services.Plugins;
 
-namespace Nop.Plugin.ProductProvider;
+namespace Nop.Plugin.Products.ProductProvider;
 
 public class ProductProvider : BasePlugin, IPlugin
 {
-    public string GetConfigurationPageUrl()
-    {
-        throw new NotImplementedException();
-    }
-
-    public PluginDescriptor PluginDescriptor { get; set; }
-    public Task InstallAsync()
-    {
-        return Task.CompletedTask;
-    }
-
-    public Task UninstallAsync()
-    {
-        return Task.CompletedTask;
-    }
-
-    public Task UpdateAsync(string currentVersion, string targetVersion)
-    {
-        return Task.CompletedTask;
-    }
-
-    public Task PreparePluginToUninstallAsync()
-    {
-        return Task.CompletedTask;
-    }
 }
