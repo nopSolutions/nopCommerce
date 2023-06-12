113e4733acb84ed7e6d429de2c86467ae5c6cfc0
gabrielplacinta1 <gabiplacinta77@gmail.com>
2023-06-12 16:00:33 +0300

    Add empty plugin

---
+++ src/Plugins/Nop.Plugin.Misc.MetaTagsManager/MetaTagsManager/MetaTagsManagerPlugin.cs
@@ -0,0 +1,9 @@
+﻿using Nop.Services.Common;
+using Nop.Services.Plugins;
+
+namespace Nop.Plugin.Misc.MetaTagsManager.MetaTagsManager;
+
+public class MetaTagsManagerPlugin : BasePlugin, IMiscPlugin
+{
+    
+}
