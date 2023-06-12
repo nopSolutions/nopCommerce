56df63b9cc4e2da5168716cab81c8ff7969228c2
Virgiliu Plesca <virgiliu.plesca@iis.utm.md>
2023-06-12 09:08:04 +0300

    added try catch in the http client and ExternalPrductNotFoundException + log if the catch the error

--- src/Plugins/Nop.Plugin.Test.ProductProvider/Api/ProductProviderHttpClient.cs
+++ src/Plugins/Nop.Plugin.Test.ProductProvider/Api/ProductProviderHttpClient.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using Nop.Core.Configuration;
+using Nop.Plugin.Test.ProductProvider.Exceptions;
 using Nop.Plugin.Test.ProductProvider.Models;
 using Nop.Services.Configuration;
 
@@ -14,43 +16,65 @@ namespace Nop.Plugin.Test.ProductProvider.Api
     {
         protected readonly HttpClient _httpClient;
         private readonly ISettingService _settingService;
+        private readonly ILogger<ProductProviderHttpClient> _logger;
 
-        public ProductProviderHttpClient(HttpClient httpClient, ISettingService settingService)
+        public ProductProviderHttpClient(HttpClient httpClient, ISettingService settingService, ILogger<ProductProviderHttpClient> logger)
         {
             _httpClient = httpClient;
             _settingService = settingService;
+            _logger = logger;
         }
 
         public async Task<IEnumerable<int>> GetProductsAsync()
         {
-            var settings = await _settingService.LoadSettingAsync<ProductProviderSettings>();
-            
-            _httpClient.Timeout = TimeSpan.FromSeconds(10);
-            _httpClient.DefaultRequestHeaders.Authorization =
-                new AuthenticationHeaderValue(settings.ApiKeyType, settings.ApiKey);
+            try
+            {
+                var settings = await _settingService.LoadSettingAsync<ProductProviderSettings>();
 
-            var url = $"{settings.BaseUrl}/{settings.ProductListEndpoint}";
-            
-            var httpResponse = await _httpClient.GetAsync(url);
-            var responseString = await httpResponse.Content.ReadAsStringAsync();
+                _httpClient.Timeout = TimeSpan.FromSeconds(10);
+                _httpClient.DefaultRequestHeaders.Authorization =
+                    new AuthenticationHeaderValue(settings.ApiKeyType, settings.ApiKey);
 
-            return JsonConvert.DeserializeObject<IEnumerable<int>>(responseString);
+                var url = $"{settings.BaseUrl}/{settings.ProductListEndpoint}";
+
+                var httpResponse = await _httpClient.GetAsync(url);
+                var responseString = await httpResponse.Content.ReadAsStringAsync();
+
+                return JsonConvert.DeserializeObject<IEnumerable<int>>(responseString);
+            }
+            catch (Exception e)
+            {
+                _logger.LogWarning("Couldn't get external products");
+                throw;
+            }
         }
 
         public async Task<ExternalProductModel> GetProductDetailsAsync(int id)
         {
-            var settings = await _settingService.LoadSettingAsync<ProductProviderSettings>();
+            try
+            {
+                var settings = await _settingService.LoadSettingAsync<ProductProviderSettings>();
 
-            _httpClient.Timeout = TimeSpan.FromSeconds(10);
-            _httpClient.DefaultRequestHeaders.Authorization =
-                new AuthenticationHeaderValue(settings.ApiKeyType, settings.ApiKey);
+                _httpClient.Timeout = TimeSpan.FromSeconds(10);
+                _httpClient.DefaultRequestHeaders.Authorization =
+                    new AuthenticationHeaderValue(settings.ApiKeyType, settings.ApiKey);
 
-            var url = $"{settings.BaseUrl}/{settings.ProductDetailEndpoint}/{id}";
+                var url = $"{settings.BaseUrl}/{settings.ProductDetailEndpoint}/{id}";
 
-            var httpResponse = await _httpClient.GetAsync(url);
-            var responseString = await httpResponse.Content.ReadAsStringAsync();
+                var httpResponse = await _httpClient.GetAsync(url);
 
-            return JsonConvert.DeserializeObject<ExternalProductModel>(responseString);
+                if (httpResponse == null)
+                    throw new ExternalProductNotFoundException();
+                    
+                var responseString = await httpResponse.Content.ReadAsStringAsync();
+
+                return JsonConvert.DeserializeObject<ExternalProductModel>(responseString);
+            }
+            catch(Exception e)
+            {
+                _logger.LogWarning("Couldn't get external product by id");
+                throw;
+            }
         }
     }
 }
