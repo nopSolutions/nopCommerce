6d5a762d3dd4e70413aeb71a5c110846511c3ebb
Virgiliu Plesca <virgiliu.plesca@iis.utm.md>
2023-06-14 10:39:33 +0300

    add new empty widget with viewComponent

---
+++ src/Plugins/Nop.Plugin.Widgets.Deals/DealsPlugin.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Nop.Plugin.Widgets.Deals.Components;
+using Nop.Services.Cms;
+using Nop.Services.Plugins;
+using Nop.Web.Framework.Infrastructure;
+
+namespace Nop.Plugin.Widgets.Deals;
+
+public class DealsPlugin : BasePlugin, IWidgetPlugin
+{
+    public Task<IList<string>> GetWidgetZonesAsync()
+    {
+        return Task.FromResult<IList<string>>(new List<string> { PublicWidgetZones.HomepageBeforeNews });
+    }
+
+    public Type GetWidgetViewComponent(string widgetZone)
+    {
+        return typeof(DealsViewComponent);
+    }
+
+    public bool HideInWidgetList { get; }
+}
