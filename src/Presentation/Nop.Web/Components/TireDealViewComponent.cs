d075276019a2ef83038fd22b3b1c1e0dd4ec0347
Virgiliu Plesca <virgiliu.plesca@iis.utm.md>
2023-06-20 18:15:59 +0300

    add component view

--- src/Presentation/Nop.Web/Components/TireDealViewComponent.cs
+++ src/Presentation/Nop.Web/Components/TireDealViewComponent.cs
@@ -1,37 +1,41 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Nop.Services.Media;
 using Nop.Services.TireDeals;
 using Nop.Web.Areas.Admin.Models.TireDeals;
 using Nop.Web.Framework.Components;
 
 namespace Nop.Web.Components;
 
-public class TireDealsViewComponent : NopViewComponent
+public class TireDealViewComponent : NopViewComponent
 {
     private readonly ITireDealService _tireDealService;
+    private readonly IPictureService _pictureService;
 
-    public TireDealsViewComponent(ITireDealService tireDealService)
+    public TireDealViewComponent(ITireDealService tireDealService, IPictureService pictureService)
     {
         _tireDealService = tireDealService;
+        _pictureService = pictureService;
     }
 
     public async Task<IViewComponentResult> InvokeAsync(string widgetZone, object additionalData)
     {
         var entities = await _tireDealService.GetAllActiveAsync();
-        List<TireDealModel> models = new();
+        List<PublicInfoModel> models = new();
         
         foreach (var item in entities)
         {
-            models.Add(new TireDealModel()
+            models.Add(new PublicInfoModel()
             {
                 Id = item.Id,
                 Title = item.Title,
                 ShortDescription = item.ShortDescription,
                 LongDescription = item.LongDescription,
-                BackgroundPictureId = item.BackgroundPictureId,
-                BrandPictureId = item.BrandPictureId
+                BackgroundPictureUrl = await _pictureService.GetPictureUrlAsync(item.BackgroundPictureId),
+                BrandPictureUrl = await _pictureService.GetPictureUrlAsync(item.BrandPictureId),
+                IsActive = item.IsActive
             });
         }
         
-        return View(models);
+        return View("Default.cshtml", models);
     }
 }
