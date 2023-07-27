660629e6ed84bc4dba5693dda05e6a282ee770fe
RomanovM <maxim.r@nopcommerce.com>
2023-07-27 15:03:50 +0200

    #6794 Fix incorrect results in the sales summary report when applying some filters

--- src/Libraries/Nop.Services/Orders/OrderReportService.cs
+++ src/Libraries/Nop.Services/Orders/OrderReportService.cs
@@ -510,6 +510,7 @@ namespace Nop.Services.Orders
 
             //filter by category
             if (categoryId > 0)
+            {
                 query = from o in query
                     join oi in _orderItemRepository.Table on o.Id equals oi.OrderId
                     join p in _productRepository.Table on oi.ProductId equals p.Id
@@ -517,8 +518,12 @@ namespace Nop.Services.Orders
                     where pc.CategoryId == categoryId
                     select o;
 
+                query = query.Distinct();
+            }
+
             //filter by manufacturer
             if (manufacturerId > 0)
+            {
                 query = from o in query
                     join oi in _orderItemRepository.Table on o.Id equals oi.OrderId
                     join p in _productRepository.Table on oi.ProductId equals p.Id
@@ -526,20 +531,31 @@ namespace Nop.Services.Orders
                     where pm.ManufacturerId == manufacturerId
                     select o;
 
+                query = query.Distinct();
+            }
+
             //filter by country
             if (billingCountryId > 0)
+            {
                 query = from o in query
                     join oba in _addressRepository.Table on o.BillingAddressId equals oba.Id
                     where
                         billingCountryId <= 0 || oba.CountryId == billingCountryId
                     select o;
 
+                query = query.Distinct();
+            }
+
             //filter by product
             if (productId > 0)
+            {
                 query = from o in query
                         join oi in _orderItemRepository.Table on o.Id equals oi.OrderId
                         where oi.ProductId == productId
                         select o;
+
+                query = query.Distinct();
+            }
 
             //filter by store
             if (storeId > 0)
@@ -552,6 +568,8 @@ namespace Nop.Services.Orders
                         join p in _productRepository.Table on oi.ProductId equals p.Id
                         where p.VendorId == vendorId
                         select o;
+
+                query = query.Distinct();
             }
 
             var primaryStoreCurrency = await _currencyService.GetCurrencyByIdAsync(_currencySettings.PrimaryStoreCurrencyId);
