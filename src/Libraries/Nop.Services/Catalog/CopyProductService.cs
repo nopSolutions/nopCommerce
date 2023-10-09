66e1ab9026c2f80588f325101d75b7ddbc5d108f
Alexey Anokhin <exile.developer@gmail.com>
2023-10-09 16:31:35 +0300

    #6784 Removed redundant product updates during copying

--- src/Libraries/Nop.Services/Catalog/CopyProductService.cs
+++ src/Libraries/Nop.Services/Catalog/CopyProductService.cs
@@ -87,7 +87,6 @@ namespace Nop.Services.Catalog
             foreach (var discountMapping in await _productService.GetAllDiscountsAppliedToProductAsync(product.Id))
             {
                 await _productService.InsertDiscountProductMappingAsync(new DiscountProductMapping { EntityId = productCopy.Id, DiscountId = discountMapping.DiscountId });
-                await _productService.UpdateProductAsync(productCopy);
             }
         }
 
@@ -511,8 +510,6 @@ namespace Nop.Services.Catalog
                 var message = $"{await _localizationService.GetResourceAsync("Admin.StockQuantityHistory.Messages.MultipleWarehouses")} {string.Format(await _localizationService.GetResourceAsync("Admin.StockQuantityHistory.Messages.CopyProduct"), product.Id)}";
                 await _productService.AddStockQuantityHistoryEntryAsync(productCopy, pwi.StockQuantity, pwi.StockQuantity, pwi.WarehouseId, message);
             }
-
-            await _productService.UpdateProductAsync(productCopy);
         }
 
         /// <summary>
@@ -825,8 +822,6 @@ namespace Nop.Services.Catalog
             //copy product tags
             foreach (var productTag in await _productTagService.GetAllProductTagsByProductIdAsync(product.Id))
                 await _productTagService.InsertProductProductTagMappingAsync(new ProductProductTagMapping { ProductTagId = productTag.Id, ProductId = productCopy.Id });
-
-            await _productService.UpdateProductAsync(productCopy);
 
             //copy product pictures
             var originalNewPictureIdentifiers = await CopyProductPicturesAsync(product, newName, copyMultimedia, productCopy);
