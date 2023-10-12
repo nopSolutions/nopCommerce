f0a9f51a16f79b7a67528614122dcb5a89bd0969
Sergey Koshelev <sergey.k@nopcommerce.com>
2023-10-12 17:29:18 +0300

    Increased minor version number

--- src/Libraries/Nop.Core/NopVersion.cs
+++ src/Libraries/Nop.Core/NopVersion.cs
@@ -13,7 +13,7 @@
         /// <summary>
         /// Gets the minor store version
         /// </summary>
-        public const string MINOR_VERSION = "4";
+        public const string MINOR_VERSION = "5";
         
         /// <summary>
         /// Gets the full store version
