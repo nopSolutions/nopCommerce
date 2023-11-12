68257b0ba929075f685387f60fe6689c49aabeb6
Hasan Elsherbiny <HassanElsherbiny55@Gmail.com>
2023-11-12 16:05:59 +0200

    Publish event on changing the encryption key #6740

---
+++ src/Libraries/Nop.Core/Domain/Security/Events.cs
@@ -0,0 +1,23 @@
+﻿using Nop.Core.Configuration;
+
+namespace Nop.Core.Domain.Security
+{
+    /// <summary>
+    /// Security settings
+    /// </summary>
+    public partial class SecuritySettingsChangedEvent
+    {
+        public SecuritySettings SecuritySettings { get; set; }
+        public string OldEncryptionPrivateKey { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="SecuritySettings"></param>
+        /// <param name="oldEncryptionPrivateKey"></param>
+        public SecuritySettingsChangedEvent(SecuritySettings SecuritySettings, string oldEncryptionPrivateKey)
+        {
+            this.SecuritySettings = SecuritySettings;
+            this.OldEncryptionPrivateKey = oldEncryptionPrivateKey;
+        }
+    }
+}
