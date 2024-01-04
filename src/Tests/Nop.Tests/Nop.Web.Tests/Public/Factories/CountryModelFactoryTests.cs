bc923c3fe664f30bd44ab394c000ac4a9a481ee1
Sergey Koshelev <sergey.k@nopcommerce.com>
2024-01-04 13:04:12 +0300

    #405 Added a couple more tests for ICountryModelFactory

--- src/Tests/Nop.Tests/Nop.Web.Tests/Public/Factories/CountryModelFactoryTests.cs
+++ src/Tests/Nop.Tests/Nop.Web.Tests/Public/Factories/CountryModelFactoryTests.cs
@@ -3,14 +3,15 @@ using Nop.Services.Localization;
 using NUnit.Framework;
 using FluentAssertions;
 
-
 namespace Nop.Tests.Nop.Web.Tests.Public.Factories;
+
 public class CountryModelFactoryTests : WebTest
 {
     private ICountryModelFactory _countryModelFactory;
     private ILocalizationService _localizationService;
     private string _countryId;
     private string _invalidCountryId;
+    private string _countryWithoutStatesId;
 
     [OneTimeSetUp]
     public void SetUp()
@@ -18,7 +19,8 @@ public class CountryModelFactoryTests : WebTest
         _countryModelFactory = GetService<ICountryModelFactory>();
         _localizationService = GetService<ILocalizationService>();
 
-        _countryId = "41";
+        _countryId = "41"; //Canada
+        _countryWithoutStatesId = "3"; //Albania
         _invalidCountryId = "0";
     }
 
@@ -27,13 +29,35 @@ public class CountryModelFactoryTests : WebTest
     {
         var models = await _countryModelFactory.GetStatesByCountryIdAsync(_countryId, false);
         models.Should().NotBeNullOrEmpty();
+        models.Count.Should().BeGreaterThan(1);
+
+        models = await _countryModelFactory.GetStatesByCountryIdAsync(_countryWithoutStatesId, false);
+        models.Should().NotBeNullOrEmpty();
+        models.Count.Should().Be(1);
     }
 
     [Test]
-    public async Task CanGetStatesByInvalidCountryId()
+    public async Task CanGetStatesByInvalidCountryIdWithoutSelectState()
     {
         var models = await _countryModelFactory.GetStatesByCountryIdAsync(_invalidCountryId, false);
         models.Should().NotBeNullOrEmpty();
+
+        var result = await models.AnyAwaitAsync(
+            async m => m.name == await _localizationService.GetResourceAsync("Address.Other")
+        );
+        result.Should().BeTrue();
+    }
+
+    [Test]
+    public async Task CanGetStatesByInvalidCountryIdWithSelectState()
+    {
+        var models = await _countryModelFactory.GetStatesByCountryIdAsync(_invalidCountryId, true);
+        models.Should().NotBeNullOrEmpty();
+
+        var result = await models.AnyAwaitAsync(
+            async m => m.name == await _localizationService.GetResourceAsync("Address.SelectState")
+        );
+        result.Should().BeTrue();
     }
 
     [Test]
