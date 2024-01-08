749b93d4c62bebaf74ed9e617fb3c5c3b0ab884a
zaidanashraf17 <it.1703244.a@gmail.com>
2024-01-08 07:00:27 +0000

    Added tags implementation for Topics

---
+++ src/Libraries/Nop.Core/Domain/Catalog/TopicTag.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nop.Core.Domain.Localization;
+using Nop.Core.Domain.Seo;
+
+namespace Nop.Core.Domain.Catalog;
+public class TopicTag: BaseEntity, ILocalizedEntity, ISlugSupported
+{
+    /// <summary>
+    /// Gets or sets the name
+    /// </summary>
+    public string Name { get; set; }
+}
