b9092f581d1f856d25bc26fc47db2f7e8d129d75
SateeshMunagala <sateesh.munagala@gmail.com>
2024-01-25 21:00:55 +0530

    Scripts updated

--- DocumentsAndScripts/JobSupport_Application_Setup_Scripts/DML_Scripts/7.Store_DML_Scripts.sql
+++ DocumentsAndScripts/JobSupport_Application_Setup_Scripts/DML_Scripts/7.Store_DML_Scripts.sql
@@ -8,3 +8,15 @@ UPDATE [dbo].[Store] SET DefaultMetaDescription='On Job support Help |Job suppor
 UPDATE [dbo].[Store] SET DefaultMetaKeywords='On Job support Help |Job support | Proxy Support | Interview Support | USA | INDIA | MS Students | Masters in USA | OPT |CPT |' WHERE Id = 1
 UPDATE [dbo].[Store] SET HomepageDescription='On Job support Help |Job support | Proxy Support | Interview Support | USA | INDIA | MS Students | Masters in USA | OPT |CPT |' WHERE Id = 1
 UPDATE [dbo].[Store] SET HomepageTitle='On Job support Help |Job support | Proxy Support | Interview Support | USA | INDIA | MS Students | Masters in USA | OPT |CPT |' WHERE Id = 1
+
+Select * From Store
+
+UPDATE [dbo].[Store] 
+    SET 
+        Name='on job support' ,
+        Url='https://onjobsupport.in/' ,
+        Hosts='onjobsupport.in,www.onjobsupport.in',
+        CompanyName='JALA Technologies',
+        CompanyAddress='Hyderabad',
+        CompanyPhoneNumber='00000000'
+WHERE Id = 1
