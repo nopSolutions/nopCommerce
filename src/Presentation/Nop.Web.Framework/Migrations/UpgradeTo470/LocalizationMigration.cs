294da01b82f676f018d48dde785b68ba548087cd
Alexey Anokhin <exile.developer@gmail.com>
2024-03-21 11:57:08 +0300

    #7031 Changed some locales

--- src/Presentation/Nop.Web.Framework/Migrations/UpgradeTo470/LocalizationMigration.cs
+++ src/Presentation/Nop.Web.Framework/Migrations/UpgradeTo470/LocalizationMigration.cs
@@ -236,7 +236,7 @@ public class LocalizationMigration : MigrationBase
             ["Enums.Nop.Core.Domain.Messages.EmailAuthenticationMethod.Login"] = "Login/Password",
             ["Enums.Nop.Core.Domain.Messages.EmailAuthenticationMethod.GmailOAuth2"] = "Google (OAuth2)",
             ["Enums.Nop.Core.Domain.Messages.EmailAuthenticationMethod.MicrosoftOAuth2"] = "Microsoft (OAuth2)",
-            ["Admin.Configuration.EmailAccounts.RedirectUrl.Info"] = "{0} - enter this redirect URI when creating your credentials in the Google Cloud console. You will be redirected to this path after authenticating with Google.",
+            ["Admin.Configuration.EmailAccounts.RedirectUrl.Info"] = "<b>{0}</b> - enter this \"Authorized redirect URI\" when creating your credentials in the Google Cloud console. You will be redirected to this path after authenticating with Google.",
 
         }, languageId);
 
