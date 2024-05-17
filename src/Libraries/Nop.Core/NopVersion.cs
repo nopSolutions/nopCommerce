061729f74cad476483023ca11aad1feeeadbd7cf
Sergey Koshelev <sergey.k@nopcommerce.com>
2024-05-17 15:27:41 +0300

    Changed nopCommerce version to 4.80

--- src/Libraries/Nop.Core/NopVersion.cs
+++ src/Libraries/Nop.Core/NopVersion.cs
@@ -8,12 +8,12 @@ public static class NopVersion
     /// <summary>
     /// Gets the major store version
     /// </summary>
-    public const string CURRENT_VERSION = "4.70";
+    public const string CURRENT_VERSION = "4.80";
 
     /// <summary>
     /// Gets the minor store version
     /// </summary>
-    public const string MINOR_VERSION = "1";
+    public const string MINOR_VERSION = "0";
 
     /// <summary>
     /// Gets the full store version
