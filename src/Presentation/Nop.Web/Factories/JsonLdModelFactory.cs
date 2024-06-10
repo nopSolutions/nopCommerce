13f3d2f6cc4ccf8ea6e09d49198abfcdfbb7e435
Sergey Koshelev <sergey.k@nopcommerce.com>
2024-06-10 09:46:13 +0300

    #7221 #6568 Added some data encoding into JSON-LD

--- src/Presentation/Nop.Web/Factories/JsonLdModelFactory.cs
+++ src/Presentation/Nop.Web/Factories/JsonLdModelFactory.cs
@@ -1,4 +1,5 @@
 ﻿using System.Globalization;
+using System.Text.Encodings.Web;
 using Nop.Core;
 using Nop.Core.Domain.Catalog;
 using Nop.Core.Events;
@@ -161,13 +162,13 @@ public partial class JsonLdModelFactory : IJsonLdModelFactory
 
             product.Review = model.ProductReviews.Items?.Select(review => new JsonLdReviewModel
             {
-                Name = review.Title,
-                ReviewBody = review.ReviewText,
+                Name = JavaScriptEncoder.Default.Encode(review.Title),
+                ReviewBody = JavaScriptEncoder.Default.Encode(review.ReviewText),
                 ReviewRating = new JsonLdRatingModel
                 {
                     RatingValue = review.Rating
                 },
-                Author = new JsonLdPersonModel { Name = review.CustomerName },
+                Author = new JsonLdPersonModel { Name = JavaScriptEncoder.Default.Encode(review.CustomerName) },
                 DatePublished = review.WrittenOnStr
             }).ToList();
         }
