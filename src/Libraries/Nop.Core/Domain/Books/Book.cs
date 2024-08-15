adc2002c790e8a28dc22ca24462c070c910ccd22
officeedgesoft <129208910+officeedgesoft@users.noreply.github.com>
2024-08-15 18:25:03 +0530

    Book Entity Model

---
+++ src/Libraries/Nop.Core/Domain/Books/Book.cs
@@ -0,0 +1,18 @@
+﻿namespace Nop.Core.Domain.Books;
+
+/// <summary>
+/// Represents a blog post tag
+/// </summary>
+public partial class Book : BaseEntity
+{
+    /// <summary>
+    /// Gets or sets the name
+    /// </summary>
+    public string Name { get; set; }
+
+
+    /// <summary>
+    /// Gets or sets the created date
+    /// </summary>
+    public DateTime CreatedOn { get; set; }
+}
