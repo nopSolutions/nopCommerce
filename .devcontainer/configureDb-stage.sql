625b9ef8bd3f2a435f761c0694e774cbda09cf9a
Dave Farinelli <d@dfar.io>
2024-08-29 12:34:06 -0400

    Change staging DB script (#35)
    * Add EmailAccount config to stage DB script
    
    * Update script

--- .devcontainer/configureDb-stage.sql
+++ .devcontainer/configureDb-stage.sql
@@ -1,29 +1,16 @@
 USE [NOPCommerce]
 UPDATE Store
-SET Url = 'http://localhost:5000/'
+SET Url = 'https://stage.abcwarehouse.com'
 WHERE Url = 'https://www.abcwarehouse.com/'
-OR Url = 'https://stage.abcwarehouse.com/'
-
-UPDATE Store
-SET SslEnabled = 0
 
 UPDATE Setting
 SET Value = 'WithoutWww'
 WHERE Name = 'seosettings.wwwrequirement'
 
 UPDATE Setting
-SET Value = 'False'
-WHERE Name = 'commonsettings.enablehtmlminification'
-    OR Name = 'commonsettings.minificationenabled'
-
-UPDATE Setting
-SET Value = 'True'
-WHERE Name = 'coresettings.areexternalcallsskipped'
-
-UPDATE Setting
 SET Value = ''
 WHERE Name = 'synchronypaymentsettings.merchantpassword'
     OR Name = 'storelocatorsettings.googleapikey'
 
-UPDATE ScheduleTask
-SET Enabled = 0
+UPDATE EmailAccount
+SET Host = 'email.abcwarehouse.com'
