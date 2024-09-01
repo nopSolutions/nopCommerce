ec032274bef5ff9bfac793330b25938adf1d9931
Dave Farinelli <d@dfar.io>
2024-08-31 20:56:26 -0400

    Update configureDb-stage.sql

--- .devcontainer/configureDb-stage.sql
+++ .devcontainer/configureDb-stage.sql
@@ -3,6 +3,10 @@ UPDATE Store
 SET Url = 'https://stage.abcwarehouse.com'
 WHERE Url = 'https://www.abcwarehouse.com/'
 
+UPDATE Store
+SET Url = 'https://stagehawthorne.abcwarehouse.com'
+WHERE Url = 'https://hawthorne.abcwarehouse.com/'
+
 UPDATE Setting
 SET Value = 'WithoutWww'
 WHERE Name = 'seosettings.wwwrequirement'
