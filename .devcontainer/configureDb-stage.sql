c11599a3ce6e58d33f241806aa129d4adb90dcdc
Dave Farinelli <d@dfar.io>
2024-08-31 21:41:06 -0400

    Update configureDb-stage.sql

--- .devcontainer/configureDb-stage.sql
+++ .devcontainer/configureDb-stage.sql
@@ -1,11 +1,18 @@
-USE [NOPCommerce]
 UPDATE Store
 SET Url = 'https://stage.abcwarehouse.com'
 WHERE Url = 'https://www.abcwarehouse.com/'
 
 UPDATE Store
+SET Hosts = 'stage.abcwarehouse.com'
+WHERE Hosts = 'www.abcwarehouse.com'
+
+UPDATE Store
 SET Url = 'https://stagehawthorne.abcwarehouse.com'
 WHERE Url = 'https://hawthorne.abcwarehouse.com/'
+
+UPDATE Store
+SET Hosts = 'stagehawthorne.abcwarehouse.com'
+WHERE Hosts = 'hawthorne.abcwarehouse.com'
 
 UPDATE Setting
 SET Value = 'WithoutWww'
