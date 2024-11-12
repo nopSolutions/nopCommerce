f37057a48b678edb85ae635f960afc4882ce47aa
Sergey Koshelev <sergey.k@nopcommerce.com>
2024-11-12 16:57:59 +0300

    #7256 Update omnibus plugin to use new way of creating admin menu

---
+++ src/Plugins/Nop.Plugin.Misc.OmnibusDirective/Infrastructure/EventConsumer.cs
@@ -0,0 +1,26 @@
+﻿using Nop.Services.Events;
+using Nop.Web.Framework.Events;
+using Nop.Web.Framework.Menu;
+
+namespace Nop.Plugin.Misc.OmnibusDirective.Infrastructure;
+
+/// <summary>
+/// Represents plugin event consumer
+/// </summary>
+public class EventConsumer: IConsumer<AdminMenuCreatedEvent>
+{
+    public Task HandleEventAsync(AdminMenuCreatedEvent eventMessage)
+    {
+        eventMessage.RootMenuItem.InsertBefore("Local plugins",
+            new AdminMenuItem
+            {
+                SystemName = "EU Omnibus Directive",
+                Title = "Omnibus Directive plugin",
+                Url = eventMessage.GetMenuItemUrl("OmnibusDirective", "Configure"),
+                IconClass = "far fa-dot-circle",
+                Visible = true,
+            });
+
+        return Task.CompletedTask;
+    }
+}
--- src/Plugins/Nop.Plugin.Misc.OmnibusDirective/OmnibusDirectivePlugin.cs
+++ src/Plugins/Nop.Plugin.Misc.OmnibusDirective/OmnibusDirectivePlugin.cs
@@ -11,7 +11,7 @@ namespace Nop.Plugin.Misc.OmnibusDirective;
 /// <summary>
 /// Represents EU Omnibus Directive plugin
 /// </summary>
-public class OmnibusDirectivePlugin : BasePlugin, IAdminMenuPlugin, IMiscPlugin
+public class OmnibusDirectivePlugin : BasePlugin, IMiscPlugin
 {
     #region Fields
 
@@ -39,37 +39,6 @@ public class OmnibusDirectivePlugin : BasePlugin, IAdminMenuPlugin, IMiscPlugin
     public override string GetConfigurationPageUrl()
     {
         return $"{_webHelper.GetStoreLocation()}Admin/OmnibusDirective/Configure";
-    }
-
-    public async Task ManageSiteMapAsync(SiteMapNode rootNode)
-    {
-        if (!await _permissionService.AuthorizeAsync(StandardPermission.Configuration.MANAGE_PLUGINS))
-            return;
-
-        var config = rootNode.ChildNodes.FirstOrDefault(node => node.SystemName.Equals("Configuration"));
-        if (config == null)
-            return;
-
-        var plugins = config.ChildNodes.FirstOrDefault(node => node.SystemName.Equals("Local plugins"));
-
-        if (plugins == null)
-            return;
-
-        var index = config.ChildNodes.IndexOf(plugins);
-
-        if (index < 0)
-            return;
-
-        config.ChildNodes.Insert(index, new SiteMapNode
-        {
-            SystemName = "EU Omnibus Directive",
-            Title = "Omnibus Directive plugin",
-            ControllerName = "OmnibusDirective",
-            ActionName = "Configure",
-            IconClass = "far fa-dot-circle",
-            Visible = true,
-            RouteValues = new RouteValueDictionary { { "area", AreaNames.ADMIN } }
-        });
     }
 
     /// <summary>
