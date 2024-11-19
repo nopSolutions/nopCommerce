e36533d3b08a16355a389219339fc4e23fb88185
Mike0081 <mikeschwartz81@gmail.com>
2024-11-19 09:42:41 -0500

    Update to CC and to email

--- src/Plugins/Nop.Plugin.Widgets.AbcContactUs/Controllers/AbcContactUsController.cs
+++ src/Plugins/Nop.Plugin.Widgets.AbcContactUs/Controllers/AbcContactUsController.cs
@@ -144,11 +144,11 @@ namespace Nop.Plugin.Widgets.AbcHomeDeliveryStatus.Controllers
                     await _logger.WarningAsync($"AbcContactUs: shopAbc not found for shop {shop.Id}:{shop.Name}, sending to base contact email {_settings.ContactUsEmail}.");
                 }
 
-                toAddress = shopAbc != null ? shopAbc.AbcEmail : _settings.ContactUsEmail;
+                toAddress = shopAbc != null ? shopAbc.ManagerEmail : _settings.ContactUsEmail;
                 ccEmails.Add(_settings.ContactUsEmail);
                 ccEmails.Add("bjohnson@abcwarehouse.com");
                 ccEmails.Add("ms22418@abcwarehouse.com");
-                ccEmails.Add(shopAbc.ManagerEmail);
+                ccEmails.Add(shopAbc.AbcEmail);
             }
 
             var subject = "Customer " + model.Reason + "- " + model.SelectedStore + "/" + model.Name + "  " + model.PhoneNumber;
@@ -175,5 +175,3 @@ namespace Nop.Plugin.Widgets.AbcHomeDeliveryStatus.Controllers
         }
     }
 }
-
-
