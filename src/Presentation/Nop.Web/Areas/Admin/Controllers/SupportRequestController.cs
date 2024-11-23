20289256f3898fa8e851737e35958be5edefbf26
davidbyrne <david.byrne222@gmail.com>
2024-11-23 22:27:44 +0000

    added MVC for admin requsts list

---
+++ src/Presentation/Nop.Web/Areas/Admin/Controllers/SupportRequestController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Nop.Core;
+using Nop.Core.Domain.Support;
+using Nop.Web.Areas.Admin.Models.Support;
+using Nop.Services.Support;
+
+namespace Nop.Web.Areas.Admin.Controllers;
+
+[Area("Admin")]
+public class SupportRequestController : BaseAdminController
+{
+    protected readonly ISupportRequestService _supportRequestService;
+    protected readonly IWorkContext _workContext;
+
+    public SupportRequestController(ISupportRequestService supportRequestService, IWorkContext workContext)
+    {
+        _supportRequestService = supportRequestService;
+        _workContext = workContext;
+    }
+    
+    // List action to display notes for a specific customer
+    public async Task<IActionResult> RequestsList()
+    {
+        var requestList = _supportRequestService.GetSupportRequests();
+        
+        var requests = requestList.Select(request => new SupportRequestModel
+        {
+            Id = request.Id,
+            CustomerId = request.CustomerId,
+            Status = request.Status,
+            Subject = request.Subject,
+            Read = request.Read,
+            CreatedOnUtc = request.CreatedOnUtc,
+            UpdatedOnUtc = request.UpdatedOnUtc
+        }).ToList();
+        
+        return View(requests);
+    }
+    
+}
