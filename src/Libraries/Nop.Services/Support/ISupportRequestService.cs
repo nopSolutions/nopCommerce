536573d8d92ebca7391407312ac2f70a2ec4551f
davidbyrne <david.byrne222@gmail.com>
2024-11-23 20:15:50 +0000

    created initial service implementation. Untested

---
+++ src/Libraries/Nop.Services/Support/ISupportRequestService.cs
@@ -0,0 +1,22 @@
+﻿using Nop.Core.Domain.Support;
+
+namespace Nop.Services.Support;
+
+public partial interface ISupportRequestService
+{
+    // Create
+    void CreateSupportRequest(SupportRequest request);
+    void CreateSupportMessage(SupportMessage message);
+    
+    // Read
+    SupportRequest GetSupportRequestById(int id);
+    IList<SupportRequest> GetSupportRequests();
+    IList<SupportMessage> GetSupportRequestMessages(int SupportRequestId);
+    
+    // Update
+    void UpdateSupportRequest(SupportRequest request);
+    
+    // Delete
+    void DeleteSupportRequest(SupportRequest request);
+    void DeleteSupportRequestMessages(int SupportRequestId);
+}
---
+++ src/Libraries/Nop.Services/Support/SupportRequestService.cs
@@ -0,0 +1,66 @@
+﻿using LinqToDB;
+using Nop.Core.Domain.Customers;
+using Nop.Core.Domain.Support;
+using Nop.Data;
+
+namespace Nop.Services.Support;
+
+public partial class SupportRequestService : ISupportRequestService
+{
+    protected readonly IRepository<SupportRequest> _supportRequestRepository;
+    protected readonly IRepository<SupportMessage> _supportMessageRepository;
+
+    public SupportRequestService(IRepository<SupportRequest> supportRequestRepository, IRepository<SupportMessage> supportMessageRepository)
+    {
+        _supportRequestRepository = supportRequestRepository;
+        _supportMessageRepository = supportMessageRepository;
+    }
+    
+    #region Requests
+    
+    public void CreateSupportRequest(SupportRequest request)
+    {
+        _supportRequestRepository.Insert(request);
+    }
+    
+    public SupportRequest GetSupportRequestById(int id)
+    {
+        return _supportRequestRepository.Table.FirstOrDefault(x => x.Id == id);
+    }
+
+    public IList<SupportRequest> GetSupportRequests()
+    {
+        return _supportRequestRepository.Table.ToList();
+    }
+
+    public void UpdateSupportRequest(SupportRequest request)
+    {
+        _supportRequestRepository.Update(request);
+    }
+
+    public void DeleteSupportRequest(SupportRequest request)
+    {
+        _supportRequestRepository.Delete(request);
+    }
+    
+    #endregion
+    
+    #region Messages
+    
+    public void CreateSupportMessage(SupportMessage message)
+    {
+        _supportMessageRepository.Insert(message);
+    }
+    
+    public IList<SupportMessage> GetSupportRequestMessages(int SupportRequestId)
+    {
+        return _supportMessageRepository.Table.Where(sm => sm.Id == SupportRequestId).ToList();
+    }
+    
+    public void DeleteSupportRequestMessages(int SupportRequestId)
+    {
+        _supportMessageRepository.Table.Where(sm => sm.Id == SupportRequestId).Delete();
+    }
+    
+    #endregion
+}
