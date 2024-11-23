9347ea330b11130fe2ef72da60b7adf2a980ae58
davidbyrne <david.byrne222@gmail.com>
2024-11-23 19:05:42 +0000

    added entities, builders, and migration for support requests

---
+++ src/Libraries/Nop.Core/Domain/Support/SupportMessage.cs
@@ -0,0 +1,10 @@
+﻿namespace Nop.Core.Domain.Support;
+
+public class SupportMessage : BaseEntity
+{
+    public int RequestId { get; set; }
+    public int AuthorId { get; set; }
+    public bool IsAdmin { get; set; }
+    public DateTime CreatedOnUtc { get; set; }
+    public string Message { get; set; }
+}
---
+++ src/Libraries/Nop.Data/Mapping/Builders/Support/SupportMessageBuilder.cs
@@ -0,0 +1,19 @@
+﻿using FluentMigrator.Builders.Create.Table;
+using Nop.Core.Domain.Customers;
+using Nop.Core.Domain.Support;
+using Nop.Data.Extensions;
+
+namespace Nop.Data.Mapping.Builders.Support;
+
+public partial class SupportMessageBuilder : NopEntityBuilder<SupportMessage>
+{
+    public override void MapEntity(CreateTableExpressionBuilder table)
+    {
+        table
+            .WithColumn(nameof(SupportMessage.RequestId)).AsInt32().ForeignKey<SupportRequest>().NotNullable()
+            .WithColumn(nameof(SupportMessage.AuthorId)).AsInt32().ForeignKey<Customer>().OnDelete(0).NotNullable()
+            .WithColumn(nameof(SupportMessage.IsAdmin)).AsBoolean().NotNullable()
+            .WithColumn(nameof(SupportMessage.CreatedOnUtc)).AsDateTime().NotNullable()
+            .WithColumn(nameof(SupportMessage.Message)).AsString(1000).NotNullable();
+    }
+}
