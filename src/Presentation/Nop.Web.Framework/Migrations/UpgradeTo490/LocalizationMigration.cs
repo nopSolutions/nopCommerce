3bd06617ba4d4df75b8c7e85f7f957c0dc93d7eb
Alexey Anokhin <exile.developer@gmail.com>
2024-12-24 16:58:19 +0300

    Changed nopCommerce version to 4.90

---
+++ src/Presentation/Nop.Web.Framework/Migrations/UpgradeTo490/LocalizationMigration.cs
@@ -0,0 +1,42 @@
+﻿using FluentMigrator;
+using Nop.Core.Infrastructure;
+using Nop.Data;
+using Nop.Data.Migrations;
+using Nop.Services.Localization;
+using Nop.Web.Framework.Extensions;
+
+namespace Nop.Web.Framework.Migrations.UpgradeTo490;
+
+[NopUpdateMigration("2024-12-01 00:00:00", "4.90", UpdateMigrationType.Localization)]
+public class LocalizationMigration : MigrationBase
+{
+    /// <summary>Collect the UP migration expressions</summary>
+    public override void Up()
+    {
+        if (!DataSettingsManager.IsDatabaseInstalled())
+            return;
+
+        //do not use DI, because it produces exception on the installation process
+        var localizationService = EngineContext.Current.Resolve<ILocalizationService>();
+
+        var (languageId, _) = this.GetLanguageData();
+
+        #region Delete locales
+
+        #endregion
+
+        #region Rename locales
+
+        #endregion
+
+        #region Add or update locales
+
+        #endregion
+    }
+
+    /// <summary>Collects the DOWN migration expressions</summary>
+    public override void Down()
+    {
+        //add the downgrade logic if necessary 
+    }
+}
---
+++ src/Presentation/Nop.Web.Framework/Migrations/UpgradeTo490/SettingMigration.cs
@@ -0,0 +1,26 @@
+﻿using FluentMigrator;
+using Nop.Core.Infrastructure;
+using Nop.Data;
+using Nop.Data.Migrations;
+using Nop.Services.Configuration;
+
+namespace Nop.Web.Framework.Migrations.UpgradeTo490;
+
+[NopUpdateMigration("2024-12-01 00:00:00", "4.90", UpdateMigrationType.Settings)]
+public class SettingMigration : MigrationBase
+{
+    /// <summary>Collect the UP migration expressions</summary>
+    public override void Up()
+    {
+        if (!DataSettingsManager.IsDatabaseInstalled())
+            return;
+
+        //do not use DI, because it produces exception on the installation process
+        var settingService = EngineContext.Current.Resolve<ISettingService>();
+    }
+
+    public override void Down()
+    {
+        //add the downgrade logic if necessary 
+    }
+}
