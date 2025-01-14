f63fbf87a7191ab92c3e0b9fc99083608118aa02
Alexey Anokhin <exile.developer@gmail.com>
2025-01-14 16:09:45 +0300

    #5345 Add availability period (start/end dates) to topics

---
+++ src/Libraries/Nop.Data/Migrations/UpgradeTo490/SchemaMigration.cs
@@ -0,0 +1,35 @@
+﻿using FluentMigrator;
+using Nop.Core.Domain.Topics;
+
+namespace Nop.Data.Migrations.UpgradeTo490;
+
+[NopSchemaMigration("2025-01-27 00:00:00", "SchemaMigration for 4.90.0")]
+public class SchemaMigration : ForwardOnlyMigration
+{
+    /// <summary>
+    /// Collect the UP migration expressions
+    /// </summary>
+    public override void Up()
+    {
+        //#7294
+        var topicTableName = nameof(Topic);
+        var topicAvailableEndDateColumnName = nameof(Topic.AvailableEndDateTimeUtc);
+        var topicAvailableStartDateColumnName = nameof(Topic.AvailableStartDateTimeUtc);
+
+        if (!Schema.Table(topicTableName).Column(topicAvailableEndDateColumnName).Exists())
+        {
+            Alter.Table(topicTableName)
+                .AddColumn(topicAvailableEndDateColumnName)
+                .AsDateTime()
+                .Nullable();
+        }
+
+        if (!Schema.Table(topicTableName).Column(topicAvailableStartDateColumnName).Exists())
+        {
+            Alter.Table(topicTableName)
+                .AddColumn(topicAvailableStartDateColumnName)
+                .AsDateTime()
+                .Nullable();
+        }
+    }
+}
