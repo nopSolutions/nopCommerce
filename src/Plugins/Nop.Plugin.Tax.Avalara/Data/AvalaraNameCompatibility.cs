baf530498af97c285b32ea7ec6d3452be873cd50
Sergey Koshelev <sergey.k@nopcommerce.com>
2025-01-15 16:38:05 +0300

    Update AvalaraNameCompatibility class

--- src/Plugins/Nop.Plugin.Tax.Avalara/Data/AvalaraNameCompatibility.cs
+++ src/Plugins/Nop.Plugin.Tax.Avalara/Data/AvalaraNameCompatibility.cs
@@ -1,11 +1,12 @@
-﻿using Nop.Plugin.Tax.Avalara.Domain;
+﻿using Nop.Data.Mapping;
+using Nop.Plugin.Tax.Avalara.Domain;
 
-namespace Nop.Data.Mapping;
+namespace Nop.Plugin.Tax.Avalara.Data;
 
 /// <summary>
 /// Plugin table naming compatibility
 /// </summary>
-public partial class BaseNameCompatibility : INameCompatibility
+public class AvalaraNameCompatibility : INameCompatibility
 {
     public Dictionary<Type, string> TableNames => new() { { typeof(ItemClassification), "AvalaraItemClassification" } };
     public Dictionary<(Type, string), string> ColumnName => new();
