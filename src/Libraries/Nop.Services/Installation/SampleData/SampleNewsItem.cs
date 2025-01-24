4020f2943b6e5f75cebe7f3bf72e76af5813b89e
Sergey Koshelev <sergey.k@nopcommerce.com>
2025-01-24 16:05:14 +0300

    #776 Store sample data in JSON (installation)

---
+++ src/Libraries/Nop.Services/Installation/SampleData/SampleNewsItem.cs
@@ -0,0 +1,62 @@
+﻿namespace Nop.Services.Installation.SampleData;
+
+/// <summary>
+/// Represents a sample news item
+/// </summary>
+public partial class SampleNewsItem
+{
+    /// <summary>
+    /// Gets or sets the news title
+    /// </summary>
+    public string Title { get; set; }
+
+    /// <summary>
+    /// Gets or sets the short text
+    /// </summary>
+    public string Short { get; set; }
+
+    /// <summary>
+    /// Gets or sets the full text
+    /// </summary>
+    public string Full { get; set; }
+
+    /// <summary>
+    /// Gets or sets a value indicating whether the news item is published
+    /// </summary>
+    public bool Published { get; set; }
+
+    /// <summary>
+    /// Gets or sets a value indicating whether the news post comments are allowed 
+    /// </summary>
+    public bool AllowComments { get; set; }
+
+    /// <summary>
+    /// Gets or sets the news comments
+    /// </summary>
+    public List<SampleNewsComment> NewsComments { get; set; } = new();
+
+    #region Nested class
+
+    /// <summary>
+    /// Represents a sample news comment
+    /// </summary>
+    public partial class SampleNewsComment
+    {
+        /// <summary>
+        /// Gets or sets the comment title
+        /// </summary>
+        public string CommentTitle { get; set; }
+
+        /// <summary>
+        /// Gets or sets the comment text
+        /// </summary>
+        public string CommentText { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the comment is approved
+        /// </summary>
+        public bool IsApproved { get; set; }
+    }
+
+    #endregion
+}
---
+++ src/Libraries/Nop.Services/Installation/SampleData/SamplePoll.cs
@@ -0,0 +1,57 @@
+﻿namespace Nop.Services.Installation.SampleData;
+
+/// <summary>
+/// Represents a sample poll
+/// </summary>
+public partial class SamplePoll
+{
+    /// <summary>
+    /// Gets or sets the name
+    /// </summary>
+    public string Name { get; set; }
+
+    /// <summary>
+    /// Gets or sets the system keyword
+    /// </summary>
+    public string SystemKeyword { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Gets or sets a value indicating whether the entity is published
+    /// </summary>
+    public bool Published { get; set; }
+
+    /// <summary>
+    /// Gets or sets a value indicating whether the entity should be shown on home page
+    /// </summary>
+    public bool ShowOnHomepage { get; set; }
+
+    /// <summary>
+    /// Gets or sets the display order
+    /// </summary>
+    public int DisplayOrder { get; set; }
+
+    /// <summary>
+    /// Gets or sets the answers
+    /// </summary>
+    public List<SamplePollAnswer> Answers { get; set; } = new();
+
+    #region Nested class
+
+    /// <summary>
+    /// Represents a poll answer
+    /// </summary>
+    public partial class SamplePollAnswer
+    {
+        /// <summary>
+        /// Gets or sets the poll answer name
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the display order
+        /// </summary>
+        public int DisplayOrder { get; set; }
+    }
+
+    #endregion
+}
