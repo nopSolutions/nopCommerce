2fd37e76a0721ea38f840cd37c43d752bb1ec551
qasas145 <mohamed.sayed14527@yahoo.com>
2025-02-03 16:34:13 +0300

    Remving searchGroupName

--- src/Presentation/Nop.Web/Areas/Admin/Models/Catalog/SpecificationAttributeGroupSearchModel.cs
+++ src/Presentation/Nop.Web/Areas/Admin/Models/Catalog/SpecificationAttributeGroupSearchModel.cs
@@ -8,10 +8,6 @@ namespace Nop.Web.Areas.Admin.Models.Catalog;
 /// </summary>
 public partial record SpecificationAttributeGroupSearchModel : BaseSearchModel
 {
-    // this is was empty 
-    [NopResourceDisplayName("Admin.Catalog.Attributes.SpecificationAttributes.SpecificationAttribute.Fields.SearchGroupName")]
-    public string SpecificationAttributeSearchGroupName { get; set; }
-
     [NopResourceDisplayName("Admin.Catalog.Attributes.SpecificationAttributes.SpecificationAttribute.Fields.SearchName")]
     public string SpecificationAttributeName { get; set; }
 
