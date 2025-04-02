627f7c0c951bdb9c0c6fc600c9d63420e504b8a7
DmitriyKulagin <kulagin87@gmail.com>
2025-04-02 14:37:36 +0300

    Add "layout" property to DataTablesModel

---
+++ src/Presentation/Nop.Web.Framework/Models/DataTables/LayoutOption.cs
@@ -0,0 +1,22 @@
+﻿namespace Nop.Web.Framework.Models.DataTables;
+
+/// <summary>
+/// Represent DataTables layout option
+/// </summary>
+public partial class LayoutOption
+{
+    /// <summary>
+    /// Option name
+    /// </summary>
+    public string Name { get; set; }
+
+    /// <summary>
+    /// Option type
+    /// </summary>
+    public LayoutOptionType OptionType { get; set; }
+
+    /// <summary>
+    /// Option value
+    /// </summary>
+    public object Value { get; set; }
+}
