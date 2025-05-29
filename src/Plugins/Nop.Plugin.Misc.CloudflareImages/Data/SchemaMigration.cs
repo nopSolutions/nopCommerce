5cecc48b09943e9899a9a2df0e1046eabd97d478
Sergey Koshelev <sergey.k@nopcommerce.com>
2025-05-29 21:56:52 +0300

    #6226 Use CloudFlare Images (image hosting)

---
+++ src/Plugins/Nop.Plugin.Misc.CloudflareImages/Data/SchemaMigration.cs
@@ -0,0 +1,21 @@
+﻿using FluentMigrator;
+using Nop.Data.Extensions;
+using Nop.Data.Migrations;
+
+namespace Nop.Plugin.Misc.CloudflareImages.Data;
+
+[NopMigration("2025-04-25 08:00:00", "Misc.CloudflareImages base schema", MigrationProcessType.Installation)]
+public class SchemaMigration : AutoReversingMigration
+{
+    #region Methods
+
+    /// <summary>
+    /// Collect the UP migration expressions
+    /// </summary>
+    public override void Up()
+    {
+        Create.TableFor<Domain.CloudflareImages>();
+    }
+
+    #endregion
+}
---
+++ src/Plugins/Nop.Plugin.Misc.CloudflareImages/Models/ConfigurationModel.cs
@@ -0,0 +1,25 @@
+﻿using Nop.Web.Framework.Models;
+using Nop.Web.Framework.Mvc.ModelBinding;
+
+namespace Nop.Plugin.Misc.CloudflareImages.Models;
+
+/// <summary>
+/// Represents configuration model
+/// </summary>
+public record ConfigurationModel : BaseNopModel
+{
+    [NopResourceDisplayName("Nop.Plugin.Misc.CloudflareImages.Enabled")]
+    public bool Enabled { get; set; } = false;
+
+    [NopResourceDisplayName("Nop.Plugin.Misc.CloudflareImages.RequestTimeout")]
+    public int? RequestTimeout { get; set; } = CloudflareImagesDefaults.RequestTimeout;
+
+    [NopResourceDisplayName("Nop.Plugin.Misc.CloudflareImages.AccountId")]
+    public string AccountId { get; set; } = string.Empty;
+
+    [NopResourceDisplayName("Nop.Plugin.Misc.CloudflareImages.AccessToken")]
+    public string AccessToken { get; set; } = string.Empty;
+
+    [NopResourceDisplayName("Nop.Plugin.Misc.CloudflareImages.DeliveryUrl")]
+    public string DeliveryUrl { get; set; } = string.Empty;
+}
