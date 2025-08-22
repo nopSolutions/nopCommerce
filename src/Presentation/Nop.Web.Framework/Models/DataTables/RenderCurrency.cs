cf91e28f01d6068f53df132c6af6d0cef03daafc
Dan Fehrenbach <dan@fbach.us>
2025-08-22 09:31:44 -0500

    implement RenderCurrency

---
+++ src/Presentation/Nop.Web.Framework/Models/DataTables/RenderCurrency.cs
@@ -0,0 +1,24 @@
+﻿namespace Nop.Web.Framework.Models.DataTables;
+
+public partial class RenderCurrency : IRender
+{
+    private const Align ALIGN_DEFAULT = Align.Right;
+
+    private const string CURRENCY_DEFAULT = "USD";
+
+    public string Currency { get; set; }
+    public Align TextAlign { get; set; }
+
+    public RenderCurrency()
+    {
+        TextAlign = ALIGN_DEFAULT;
+        Currency = CURRENCY_DEFAULT;
+    }
+
+    public enum Align
+    {
+        Left,
+        Right,
+        Center
+    }
+}
