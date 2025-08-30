27f1111238f7604e864e003208ac2efe9645a837
fredrick amoako <fredrick.amoako@amalitech.com>
2025-08-30 14:10:02 +0000

    fix di issue

--- src/Plugins/Nop.Plugin.Payments.Momo/Infrastructure/NopStartup.cs
+++ src/Plugins/Nop.Plugin.Payments.Momo/Infrastructure/NopStartup.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Nop.Core.Infrastructure;
+using Nop.Plugin.Payments.Momo.Services;
 using Nop.Web.Framework.Infrastructure.Extensions;
 
 namespace Nop.Plugin.Payments.Momo.Infrastructure;
@@ -15,6 +16,14 @@ public partial class NopStartup : INopStartup
 
     public void ConfigureServices(IServiceCollection services, IConfiguration configuration)
     {
+        //http clients
         services.AddHttpClient<MtnMomoConfigHttpClient>().WithProxy();
+        services.AddHttpClient<MomoPaymentClient>().WithProxy();
+
+        //settings
+        services.AddScoped<MomoPaymentSettings>();
+
+        //services
+        services.AddScoped<IMomoTransactionService, MomoTransactionService>();
     }
 }
