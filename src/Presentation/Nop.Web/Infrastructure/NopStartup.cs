TriVectorX/maiba-jewelry-shop

7743 Reminders (Abandoned cart, pending registration, pending orders)

src/Presentation/Nop.Web/Infrastructure/NopStartup.cs
@@ -85,6 +85,7 @@ public partial class NopStartup : INopStartup
         services.AddScoped<Areas.Admin.Factories.IWidgetModelFactory, Areas.Admin.Factories.WidgetModelFactory>();
         services.AddScoped<ITranslationModelFactory, TranslationModelFactory>();
         services.AddScoped<IMenuModelFactory, MenuModelFactory>();
+        services.AddScoped<IReminderModelFactory, RemindersModelFactory>();
 
         //factories
         services.AddScoped<Factories.IAddressModelFactory, Factories.AddressModelFactory>();

nopSolutions/nopCommerce

src/Presentation/Nop.Web/Infrastructure/NopStartup.cs
@@ -86,6 +86,7 @@ public partial class NopStartup : INopStartup
         services.AddScoped<Areas.Admin.Factories.IWidgetModelFactory, Areas.Admin.Factories.WidgetModelFactory>();
         services.AddScoped<ITranslationModelFactory, TranslationModelFactory>();
         services.AddScoped<IMenuModelFactory, MenuModelFactory>();
+        services.AddScoped<IReminderModelFactory, ReminderModelFactory>();
 
         //factories
         services.AddScoped<Factories.IAddressModelFactory, Factories.AddressModelFactory>();