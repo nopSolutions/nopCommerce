37d1cf5c225c1de1493794fa1a2184602ea71a82
Sergey Koshelev <sergey.k@nopcommerce.com>
2025-11-11 14:27:49 +0300

    #7951 Misc.RFQ plugin cannot be uninstalled

--- src/Plugins/Nop.Plugin.Misc.RFQ/Data/Migrations/SchemaMigration.cs
+++ src/Plugins/Nop.Plugin.Misc.RFQ/Data/Migrations/SchemaMigration.cs
@@ -6,7 +6,7 @@ using Nop.Plugin.Misc.RFQ.Domains;
 namespace Nop.Plugin.Misc.RFQ.Data.Migrations;
 
 [NopMigration("2024/07/03 10:30:08:1687554", "Nop.Plugin.Misc.RFQ schema", MigrationProcessType.Installation)]
-public class SchemaMigration : Migration
+public class SchemaMigration : AutoReversingMigration
 {
     /// <summary>
     /// Collect the UP migration expressions
@@ -18,17 +18,5 @@ public class SchemaMigration : Migration
 
         Create.TableFor<Quote>();
         Create.TableFor<QuoteItem>();
-    }
-
-    /// <summary>
-    /// Collects the DOWN migration expressions
-    /// </summary>
-    public override void Down()
-    {
-        Delete.Table(nameof(QuoteItem));
-        Delete.Table(nameof(Quote));
-
-        Delete.Table(nameof(RequestQuoteItem));
-        Delete.Table(nameof(RequestQuote));
     }
 }
