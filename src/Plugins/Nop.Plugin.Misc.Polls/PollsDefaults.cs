362dd2af295869deaed36ff4a7eec3d0503a7dc6
severinfv <severin.filonov@pm.me>
2025-12-17 12:29:46 +0100

    add polls plugin structure

---
+++ src/Plugins/Nop.Plugin.Misc.Polls/PollsDefaults.cs
@@ -0,0 +1,12 @@
+﻿namespace Nop.Plugin.Misc.Polls;
+
+/// <summary>
+/// Represents plugin constants
+/// </summary>
+public class PollsDefaults
+{
+    /// <summary>
+    /// Gets a plugin system name
+    /// </summary>
+    public static string SystemName => "Misc.Polls";
+}
---
+++ src/Plugins/Nop.Plugin.Misc.Polls/PollsPlugin.cs
@@ -0,0 +1,49 @@
+﻿using Nop.Core;
+using Nop.Services.Common;
+using Nop.Services.Plugins;
+
+namespace Nop.Plugin.Misc.Polls;
+
+/// <summary>
+/// Represents the nopCommerce mobile application helper plugin
+/// </summary>
+public class PollsPlugin : BasePlugin, IMiscPlugin
+{
+    #region Fields
+
+    private readonly IWebHelper _webHelper;
+
+    #endregion
+
+    #region Ctor
+
+    public PollsPlugin(IWebHelper webHelper)
+    {
+        _webHelper = webHelper;
+    }
+
+    #endregion
+
+    #region Methods
+
+    /// <summary>
+    /// Install the plugin
+    /// </summary>
+    /// <returns>A task that represents the asynchronous operation</returns>
+    public override async Task InstallAsync()
+    {
+        await base.InstallAsync();
+    }
+
+    /// <summary>
+    /// Uninstall the plugin
+    /// </summary>
+    /// <returns>A task that represents the asynchronous operation</returns>
+    public override async Task UninstallAsync()
+    {
+        await base.UninstallAsync();
+    }
+
+    #endregion
+
+}
