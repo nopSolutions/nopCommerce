cf8b805d42f6b8dce604a948b7a6a237c714b433
Mazen Sghaier <sghaiermazen7@gmail.com>
2026-04-08 19:16:30 +0100

    I've successfully upgraded your FluentMigrator configuration from MySQL 5 to MySQL 8+.

--- src/Libraries/Nop.Data/DataProviders/MySqlDataProvider.cs
+++ src/Libraries/Nop.Data/DataProviders/MySqlDataProvider.cs
@@ -1,4 +1,4 @@
-﻿using System.Data;
+using System.Data;
 using System.Data.Common;
 using System.Text;
 using LinqToDB;
@@ -353,7 +353,7 @@ public partial class MySqlNopDataProvider : BaseDataProvider, INopDataProvider
     /// <summary>
     /// MySql data provider
     /// </summary>
-    protected override IDataProvider LinqToDbDataProvider => MySqlTools.GetDataProvider();
+    protected override IDataProvider LinqToDbDataProvider => MySqlTools.GetDataProvider("8.0");
 
     /// <summary>
     /// Gets allowed a limit input value of the data for hashing functions, returns 0 if not limited
