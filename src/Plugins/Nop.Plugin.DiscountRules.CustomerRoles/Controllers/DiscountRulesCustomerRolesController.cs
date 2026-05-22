523afd826a7d5701c1986137cf5a1391dc96993b
Saidul Islam Rajib <saidul.rajib@brainstation-23.com>
2026-05-22 14:45:09 +0600

    added testing pipeline

--- src/Plugins/Nop.Plugin.DiscountRules.CustomerRoles/Controllers/DiscountRulesCustomerRolesController.cs
+++ src/Plugins/Nop.Plugin.DiscountRules.CustomerRoles/Controllers/DiscountRulesCustomerRolesController.cs
@@ -95,12 +95,7 @@ public class DiscountRulesCustomerRolesController : BasePluginController
             Value = "0"
         });
 
-        //model.AvailableCustomerRoles.Insert(0, new SelectListItem
-        //{
-        //    Text = await _localizationService.GetResourceAsync("Plugins.DiscountRules.CustomerRoles.Fields.CustomerRole.Select"),
-        //    Value = "0"
-        //});
-
+        //set the HTML field prefix
         //set the HTML field prefix
         ViewData.TemplateInfo.HtmlFieldPrefix = string.Format(DiscountRequirementDefaults.HtmlFieldPrefix, discountRequirementId ?? 0);
 
